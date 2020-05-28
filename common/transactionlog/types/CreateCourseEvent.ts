618641a1bb434ae5f7042d9c42cff463da4bed53
Ben Swierzy <swierzy@uni-bonn.de>
2020-05-28 21:34:21 +0200

    Renamed to CreateCourseEvent

--- common/transactionlog/types/CreateCourseEvent.ts
+++ common/transactionlog/types/CreateCourseEvent.ts
@@ -3,7 +3,7 @@ import LogType from "./LogType";
 import { Student } from "../../entity/Student";
 import { Course } from "../../entity/Course";
 
-export default class CreatedCourseEvent extends LogUserEvent {
+export default class CreateCourseEvent extends LogUserEvent {
     constructor(student: Student, course: Course) {
         super(LogType.CREATED_COURSE, student, {id: course.id});
     }
