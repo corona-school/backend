2ebb3fa75d9d72b39738457887946c51c6e54e6b
Gero <gero.embser@gmail.com>
2020-06-08 05:20:59 +0200

    Rename function that just sends screening invite mail
    Rename that because with the suffix "Mail" it becomes clear, that this function is just sending a mail to a student, without any logic, in particular without storing information about the sent mail in the database (which requires some kind of application logic and is therefore written under common/admin in a previous commit).

--- common/administration/screening/initial-invitations/index.ts
+++ common/administration/screening/initial-invitations/index.ts
@@ -1,11 +1,11 @@
 import { EntityManager } from "typeorm";
 import { Student } from "../../../entity/Student";
-import { sendFirstScreeningInvitation } from "../../../mails/screening";
+import { sendFirstScreeningInvitationMail } from "../../../mails/screening";
 
 
 export async function sendFirstScreeningInvitationToStudent(manager: EntityManager, student: Student) {
     //send actual mail...
-    await sendFirstScreeningInvitation(student);
+    await sendFirstScreeningInvitationMail(student);
 
     //... store that in the database
     student.lastSentScreeningInvitationDate = new Date();
--- common/mails/screening/index.ts
+++ common/mails/screening/index.ts
@@ -2,7 +2,7 @@ import { Student } from "../../entity/Student";
 import { mailjetTemplates, sendTemplateMail } from "../index";
 
 /// Use this function to send the first screening invitation mail to the student
-export async function sendFirstScreeningInvitation(student: Student) {
+export async function sendFirstScreeningInvitationMail(student: Student) {
     const mail = mailjetTemplates.STUDENTFIRSTSCREENINGINVITATION({
         personFirstname: student.firstname,
         confirmationURL: student.screeningURL(),
