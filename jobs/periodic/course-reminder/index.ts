79f7cfed16e98f115959d2b520bb276aaad74649
Daniel <Ni42@users.noreply.github.com>
2020-06-16 00:29:53 +0200

    Add initial functions

---
+++ jobs/periodic/course-reminder/index.ts
@@ -0,0 +1,48 @@
+import { EntityManager } from "typeorm";
+import { Course, CourseState } from "../../../common/entity/Course";
+import { Subcourse } from "../../../common/entity/Subcourse";
+import moment from "moment";
+import { getLogger } from "log4js";
+import { Lecture } from "../../../common/entity/Lecture";
+
+
+const logger = getLogger();
+
+export default async function execute(manager: EntityManager) {
+    logger.info("CourseReminder job: looking for subcourses with first lecture in two days...");
+    await sendUpcomingCourseReminders(manager);
+}
+
+async function sendUpcomingCourseReminders(manager: EntityManager) {
+    // find courses that have a lecture the day after tomorrow
+    const courses = await manager.find(Course, { courseState: CourseState.ALLOWED });
+
+    for(let course of courses) {
+        // find any subcourses
+        const subcourses = await manager.find(Subcourse, { course: course, published: true });
+
+        for(let subcourse of subcourses) {
+            // get first lecture, eager loaded
+            
+            let firstLecture = subcourse.lectures[0];
+            for(let i = 1; i < subcourse.lectures.length; i++) {
+                if(subcourse.lectures[i].start < firstLecture.start) {
+                    firstLecture = subcourse.lectures[i];
+                }
+            }
+
+            const dayAfterTomorrow = moment().add(2, 'days');
+            if(moment(firstLecture.start).isSame(dayAfterTomorrow, 'day')) {
+                // first lecture is in two days
+                logger.info("Found lecture on (sub)course " + course.name + " with start date in 2 days: " + firstLecture.start.toDateString());
+
+                // TODO: notify instructor
+                // firstLecture.instructor
+
+                // TODO: notify all participants
+                // subcourse.participants
+            }
+        }
+
+    }
+}
