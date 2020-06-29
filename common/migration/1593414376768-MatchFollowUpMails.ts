72bb4ef05ce6329f4983f143dab7b7cff92c23fa
imkgerC <imrund08@gmail.com>
2020-06-29 21:48:56 +0200

    fix missing semicolon in migration
    auto-generated migration does not place semicolon after name

--- common/migration/1593414376768-MatchFollowUpMails.ts
+++ common/migration/1593414376768-MatchFollowUpMails.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class MatchFollowUpMails1593414376768 implements MigrationInterface {
-    name = 'MatchFollowUpMails1593414376768'
+    name = 'MatchFollowUpMails1593414376768';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "match" ADD "followUpToPupilMail" boolean NOT NULL DEFAULT false`, undefined);
