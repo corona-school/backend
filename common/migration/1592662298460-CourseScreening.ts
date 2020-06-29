cf62890eece5dd2abd8b0f54d49c287d77bf595d
Daniel <Ni42@users.noreply.github.com>
2020-06-29 21:36:15 +0200

    Bugfix migration for courseId foreign key

--- common/migration/1592662298460-CourseScreening.ts
+++ common/migration/1592662298460-CourseScreening.ts
@@ -5,10 +5,13 @@ export class CourseScreening1592662298460 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "course" ADD "screeningComment" character varying`, undefined);
+        await queryRunner.query(`ALTER TABLE subcourse ADD COLUMN "courseId" integer`, undefined);
+        await queryRunner.query(`ALTER TABLE subcourse ADD CONSTRAINT "FK_274b57f6af62ffadb80afcbae85" FOREIGN KEY ("courseId") REFERENCES course(id)`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE subcourse DROP CONSTRAINT "FK_274b57f6af62ffadb80afcbae85"`, undefined);
+        await queryRunner.query(`ALTER TABLE subcourse DROP COLUMN "courseId"`, undefined);
         await queryRunner.query(`ALTER TABLE "course" DROP COLUMN "screeningComment"`, undefined);
     }
-
 }
