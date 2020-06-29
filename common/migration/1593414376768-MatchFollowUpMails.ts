a897e8ad334d6d404ed2adff9a05fbeebf799614
imkgerC <imrund08@gmail.com>
2020-06-29 09:31:45 +0200

    implement basic version of match follow up mail
    This will implement a job to send the follow up mails for matches
    created 7 days ago. Some follow-up questions for clarification were
    asked, as I do not think it is sensible to send this mail a week after
    every match but once after the first match would be sensible

---
+++ common/migration/1593414376768-MatchFollowUpMails.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class MatchFollowUpMails1593414376768 implements MigrationInterface {
+    name = 'MatchFollowUpMails1593414376768'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "match" ADD "followUpToPupilMail" boolean NOT NULL DEFAULT false`, undefined);
+        await queryRunner.query(`ALTER TABLE "match" ADD "followUpToStudentMail" boolean NOT NULL DEFAULT false`, undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "match" DROP COLUMN "followUpToStudentMail"`, undefined);
+        await queryRunner.query(`ALTER TABLE "match" DROP COLUMN "followUpToPupilMail"`, undefined);
+    }
+
+}
--- jobs/list.ts
+++ jobs/list.ts
@@ -5,6 +5,7 @@ import fetchJob from "./periodic/fetch";
 import screeningReminderJob from "./periodic/screening-reminder";
 import courseReminderJob from "./periodic/course-reminder";
 import feedbackRequestJob from "./periodic/feedback-request";
+import matchFollowUpJob from "./periodic/match-follow-up";
 
 // A list of all jobs that should be scheduled at the moment
 export const allJobs: CSCronJob[] = [
@@ -15,5 +16,6 @@ export const allJobs: CSCronJob[] = [
     // every morning, but a little later
     { cronTime: "00 15 09 * * *", jobFunction: courseReminderJob},
     // every morning, but a little bit later
-    { cronTime: "00 15 10 * * *", jobFunction: feedbackRequestJob}
+    { cronTime: "00 15 10 * * *", jobFunction: feedbackRequestJob},
+    { cronTime: "00 15 11 * * *", jobFunction: matchFollowUpJob}
 ];
