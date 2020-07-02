97dc741037655549d557d34e65d2cd53656b0cf9
Jonas Wilms <jonaswilms2000@gmail.com>
2020-07-02 09:35:57 +0200

    Always update students, as there are more props than isStudent

--- web/controllers/screeningController/index.ts
+++ web/controllers/screeningController/index.ts
@@ -518,10 +518,9 @@ export async function updateInstructor(req: Request, res: Response) {
 
         await instructor.addScreeningResult(screeningResult);
 
-        if(isStudent !== instructor.isStudent) {
-            instructor.isStudent = isStudent;
-            await getManager().save(Student, instructor);
-        }
+        instructor.isStudent = isStudent;
+        
+        await getManager().save(Student, instructor);
 
         const screening = await instructor.screening;
 
