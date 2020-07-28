111f843ca3b9569658d47207dbdcc03dd8cc3232
Daniel Peters <daniel.peters01@sap.com>
2020-07-28 23:08:11 +0200

    Upgrade node module 'pg' to v8.3.0

--- tests/integration/screeningInvitation.int.test.ts
+++ tests/integration/screeningInvitation.int.test.ts
@@ -7,9 +7,8 @@ import {createTestingConnection, closeTestingConnection} from "../utils/typeorm"
 import TestStudents from "../utils/TestStudents";
 import {verifyToken} from "../../web/controllers/tokenController";
 
-// Skipped for CI because there is no DB setup. To run locally remove 'skip' from 'describe.skip'
 describe("Screening Invitation", function() {
-    this.timeout(30000);
+    this.timeout(20000);
 
     // The connection that should be used for tests
     let connection: Connection;
