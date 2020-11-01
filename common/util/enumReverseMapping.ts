4a595dcd43238aa94359bf80a2420163967aba81
Gero <gero.embser@gmail.com>
2020-11-01 13:07:11 +0100

    Allow case insensitive values for enum reverse maps
    Some string enums should definitely support case insensitivity, which improves interoparability with frontend code as well. This is, in parts, at the moment also required by frontend, because it uses the API sometimes without respect to the enum case values

--- common/util/enumReverseMapping.ts
+++ common/util/enumReverseMapping.ts
@@ -7,19 +7,25 @@ import { TeacherModule } from "../entity/Student";
 
 
 const EnumReverseMappings = {
-    State: reverseMappingForStringEnum(State),
-    SchoolType: reverseMappingForStringEnum(SchoolType),
-    MentoringCategory: reverseMappingForStringEnum(MentoringCategory),
+    State: caseInsensitive(reverseMappingForStringEnum(State)),
+    SchoolType: caseInsensitive(reverseMappingForStringEnum(SchoolType)),
+    MentoringCategory: caseInsensitive(reverseMappingForStringEnum(MentoringCategory)),
     ProjectField: reverseMappingForStringEnum(ProjectField),
-    TutorJufoParticipationIndication: reverseMappingForStringEnum(TutorJufoParticipationIndication),
-    TuteeJufoParticipationIndication: reverseMappingForStringEnum(TuteeJufoParticipationIndication),
-    TeacherModule: reverseMappingForStringEnum(TeacherModule)
+    TutorJufoParticipationIndication: caseInsensitive(reverseMappingForStringEnum(TutorJufoParticipationIndication)),
+    TuteeJufoParticipationIndication: caseInsensitive(reverseMappingForStringEnum(TuteeJufoParticipationIndication)),
+    TeacherModule: caseInsensitive(reverseMappingForStringEnum(TeacherModule))
 };
 
 function reverseMappingForStringEnum<E>(e: E): ((s: string) => E[keyof E]) {
     //this would be equivalent to check whether `Object.values(ENUM)` contains `s` and then return s
     return (s: string) => {
         return Object.entries(e).find( ([, value]) => typeof value === "string" && value === s)?.[1];
+    };
+}
+
+function caseInsensitive<E>(reverseMappingFunction: (s: string) => E) {
+    return (s: string) => {
+        return reverseMappingFunction(s.toUpperCase()) || reverseMappingFunction(s.toLowerCase());
     };
 }
 
