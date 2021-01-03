a304821f0497e0bb93f6eb5d1c47314b1f142d33
Gero <gero.embser@gmail.com>
2021-01-03 08:05:49 +0100

    Change S3 endpoint handling to make tests work
    Test won't work if no endpoint information is provided. This commit provides a quick fix for this problem by using the default AWS S3 endpoint if no one was provided manually. There may be better solutions and the entire aws-sdk handling and how it is tested and used during development on your local machine needs to be revised in the future.

--- common/file-bucket/s3.ts
+++ common/file-bucket/s3.ts
@@ -1,20 +1,22 @@
 import * as aws from "aws-sdk";
+import { assert } from "console";
 
 export const AWS_BUCKET_ENDPOINT = process.env.AWS_BUCKET_ENDPOINT;
 export const DEFAULT_BUCKET = process.env.AWS_DEFAULT_BUCKET;
 
 /// The domain that can be used to access the files in the bucket
-export const ACCESS_DOMAIN_NAME = `${DEFAULT_BUCKET}.${AWS_BUCKET_ENDPOINT}`;
+export const ACCESS_DOMAIN_NAME = DEFAULT_BUCKET && AWS_BUCKET_ENDPOINT ? `${DEFAULT_BUCKET}.${AWS_BUCKET_ENDPOINT}` : undefined;
 
 /// The default s3 endpoint
-const s3Endpoint = new aws.Endpoint(AWS_BUCKET_ENDPOINT);
+const s3Endpoint = AWS_BUCKET_ENDPOINT ? new aws.Endpoint(AWS_BUCKET_ENDPOINT) : undefined;
 
 /// Returns the default s3 instance
 export const s3 = new aws.S3({
-    endpoint: s3Endpoint
+    ...(s3Endpoint && {endpoint: s3Endpoint})
 });
 
 /// Returns a URL for a given key (it is not ensured whether the file for that key exists or is even publicly accessible)
 export function accessURLForKey(key: string) {
+    assert(ACCESS_DOMAIN_NAME, `Cannot create access URL for key ${key} because the ACCESS_DOMAIN_NAME is undefined. Please consider setting the appropriate environment variables to make the file bucket work.`);
     return new URL(key, `https://${ACCESS_DOMAIN_NAME}`).href;
 }
