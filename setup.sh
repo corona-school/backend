87aea59f4135da876c06f094fcee56d4c12820ad
Jonas Wilms <JonasWilms2000@gmail.com>
2021-02-09 18:29:36 +0100

    Add a shellscript to experiment with a customized build in Heroku

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# This script will run after Typescript was compiled into the ./built folder
+
+echo ' -------------- Setup.sh started           -------------- '
+
+# If a private repo is available to get the secret assets from, replace the /built/assets folder
+if  ! [[ -z "${SECRET_ASSETS_REPOSITORY}" ]]; then
+  cd ./built/assets
+  rm ./*
+  ssh-agent bash -c 'ssh-add - <<< "${SECRET_ASSETS_KEY}"; git clone "${SECRET_ASSETS_REPOSITORY}"'
+fi
+
+echo ' -------------- Setup.sh ended successfully -------------- '
