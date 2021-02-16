ca77486291149d7cc833ecfe60f43d851afc53c5
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2021-02-16 19:07:58 +0100

    Proper OR short-circuiting

--- web/controllers/screeningController/index.ts
+++ web/controllers/screeningController/index.ts
@@ -459,7 +459,7 @@ export async function getCourses(req: Request, res: Response) {
         if (page && (Number.isNaN(+page) || !Number.isInteger(+page)))
             return res.status(400).send("Invalid value for parameter 'page', must be integer.");
 
-        const where = {
+        const where = (!!courseState || !!search) && {
             OR: [
                 {
                     courseState,
