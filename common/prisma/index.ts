46dfec9d097e0b8b5f3c42da2f99c989163ac090
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2021-02-16 19:24:32 +0100

    Add logging in development

--- common/prisma/index.ts
+++ common/prisma/index.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+/* In the development environment, also log 'query'
+   which is the SQL query sent to the database.
+   Through that, queries can be optimized for performance */
+export const prisma = new PrismaClient(
+    process.env.NODE_ENV === "production"
+        ? undefined
+        : { log: ['query', 'info', `warn`, `error`] }
+);
