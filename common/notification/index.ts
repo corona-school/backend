d5df31ac5fd6702f6ce8cf6622a4271ba0f4d985
Jonas <JonasWilms2000@gmail.com>
2021-06-22 20:58:33 +0200

    Various cleanups and enhancements

--- common/notification/index.ts
+++ common/notification/index.ts
@@ -1,64 +1,42 @@
 import { PrismaClientInitializationError } from '@prisma/client/runtime';
 import { Person } from '../entity/Person';
 import { mailjetChannel } from './channels/mailjet';
-import { NotificationID, NotificationContext, Context, sentNotification, notification } from './types';
-import { prisma } from '../prisma'
-import { sentNotificationState } from '@prisma/client';
-import { Pupil } from 'common/entity/Pupil';
-import { Student } from 'common/entity/Student';
-import { idText } from 'typescript';
+import { NotificationID, NotificationContext, Context, Notification, ConcreteNotification, ConcreteNotificationState } from './types';
+import { prisma } from '../prisma';
+import { Pupil } from '../entity/Pupil';
+import { Student } from '../entity/Student';
+import { debug, info, warn } from 'console';
+import { getNotification, getNotifications } from './notification';
+import { getManager } from 'typeorm';
 
+// This is the main extension point of notifications: Implement the Channel interface, then add the channel here
+const channels = [mailjetChannel];
 
+/* -------------------------------- Public API exposed to other components ----------------------------------------------------------- */
 
-const channels = [mailjetChannel]
-// This method can be used to send one specific notification with a very specific notification context to the user
-// e.g. the login email which contains a login token
-async function sendNotification(id: NotificationID, user: Person, notificationContext: NotificationContext): Promise<any> {
-    //get Notificationchannel for Notification
-    const notification = await prisma.notification.findUnique({ where: { id }, rejectOnNotFound: true })
-    await _sendNotification(notification, user, notificationContext)
-}
+/* sends one specific notification with a very specific notification context to the user.
+   Using this directly is an intermediate solution, prefer actions ("actionTaken") instead */
+export async function sendNotification(id: NotificationID, user: Person, notificationContext: NotificationContext): Promise<void> {
+    const notification = await getNotification(id);
 
-async function _sendNotification(notification: notification, user: Person, notificationContext: NotificationContext): Promise<any> {
-    // TODO Fire and Forget
-    const channel = channels.find(it => it.canSend(notification.id))
-    if (!channel) {
-        throw new Error("Channel not found for notification id.");
-    }
     const context: Context = {
         ...notificationContext,
-        user: user,
-        title: ""
+        user: user
     };
-    try {
-        await channel.send(notification.id, context);
-        await prisma.sentNotification.create({
-            data: {
-                sentAt: new Date(),
-                state: sentNotificationState.SENT
-            }
-        });
-    } catch (error) {
-        console.log(error);
-        await prisma.sentNotification.create({
-            data: {
-                sentAt: new Date(),
-                state: sentNotificationState.ERROR
-            }
-        });
-    }
+
+    // We allow duplicates here, as the caller of sendNotification is supposed to make sure that this method is only called once for the user
+    const concreteNotification = await createConcreteNotification(notification, user, context, /* allowDuplicates: */ true);
+
+    await deliverNotification(concreteNotification, notification, user, context);
 }
 
-// for campaigns, no Notification needs to be maintained in backend, just Template ID is enough
-// category can be specified to provide user opt-out possibility
-// TODO: User specific campaigns ("all students names 'Jonas'") ?
-function sendMailCampaign(mailjetId: string, category?: string) {
-    // TODO
-}
 
-// taking an action might kick off some notifications and cancel others
-// thinking of e.g. 'login', which will cancel a lot of reminders ("check out XY!")
-async function actionTaken(user: Person, actionId: string, notificationContext: NotificationContext) {
+/* Triggers all notification sending, scheduling and unscheduling of the emails for a certain action
+   Call actionTaken whenever some user action gets performed where in the future, a notification might be useful
+   If 'allowDuplicates' is set, the same action may be sent multiple times by the same user
+*/
+export async function actionTaken(user: Person, actionId: string, notificationContext: NotificationContext, allowDuplicates = false) {
+    const context = { ...notificationContext, user };
 
     const notifications = await getNotifications();
     const relevantNotifications = notifications.get(actionId);
@@ -67,58 +45,164 @@ async function actionTaken(user: Person, actionId: string, notificationContext: 
         return;
     }
 
-    const pupilId = user instanceof Pupil ? user.id : undefined;
-    const studentId = user instanceof Student ? user.id : undefined;
-    if (!pupilId && !studentId) {
-        throw new Error("No ID provided");
-    }
 
     // prevent sending of now unnecessary notifications
-    await prisma.sentNotification.updateMany({
+    await prisma.concrete_notification.updateMany({
         data: {
-            state: sentNotificationState.ACTION_TAKEN
+            state: ConcreteNotificationState.ACTION_TAKEN,
+            sentAt: new Date()
         },
         where: {
             notificationID: {
                 in: relevantNotifications.toCancel.map(it => it.id)
             },
-            state: sentNotificationState.DELAYED,
-            pupilId,
-            studentId
+            state: ConcreteNotificationState.DELAYED,
+            ...getUserRelation(user)
         }
     });
 
     const reminders = relevantNotifications.toSend.filter(it => it.delay);
     const directSends = relevantNotifications.toSend.filter(it => !it.delay);
 
+    // Trigger notifications that are supposed to be directly sent on this action
     for (const directSend of directSends) {
-        await _sendNotification(directSend, user, notificationContext);
+        const concreteNotification = await createConcreteNotification(directSend, user, context, allowDuplicates);
+        await deliverNotification(concreteNotification, directSend, user, context);
     }
 
-    // add new notifications depending on action
+    // Insert reminders into concrete_notification table so that a cron job can deliver them in the future
     if (reminders.length) {
-        await prisma.sentNotification.create({
+        await prisma.concrete_notification.createMany({
             data: reminders.map(it => ({
-                notificationID:it.id,
-                state: sentNotificationState.DELAYED,
-                pupilId,
-                studentId
+                notificationID: it.id,
+                state: ConcreteNotificationState.DELAYED,
+                sentAt: new Date(Date.now() + it.delay),
+                ...getUserRelation(user),
+                contextID: notificationContext.uniqueId,
+                context: notificationContext
             }))
         });
     }
 }
 
-async function getNotifications(): Promise<Map<String, { toSend: notification[], toCancel: notification[] }>> {
-    // TODO: get relevant notifications and use caching
-    return new Map();
-}
 
-async function checkReminders() {
-    prisma.sentNotification.findMany({
+export async function checkReminders() {
+    debug(`Checking for pending notification reminders`);
+    const start = Date.now();
+
+    const remindersToSend = await prisma.concrete_notification.findMany({
         where: {
-            state: sentNotificationState.DELAYED,
-            sentAt: {lte:new Date}
+            state: ConcreteNotificationState.DELAYED,
+            sentAt: { lte: new Date }
         }
-    })
+    });
+
+    for (const reminder of remindersToSend) {
+        if (!reminder.context)
+            throw new Error(`Notification(${reminder.id}) was supposed to contain a context when sending out reminders`);
+        const notification = await getNotification(reminder.notificationID);
+        const user = await getPersonForConcreteNotification(reminder);
+        await deliverNotification(reminder, notification, user, reminder.context as Context);
+    }
+
+    info(`Sent ${remindersToSend.length} reminders in ${Date.now() - start}ms`);
+
 }
 // TODO: function for user preferences
+
+/* --------------------------- Concrete Notification "Queue" ----------------------------------- */
+
+/* Creates an entry in the concrete_notifications table, to track the notification */
+async function createConcreteNotification(notification: Notification, user: Person, context: Context, allowDuplicates: boolean): Promise<ConcreteNotification> {
+    if (!allowDuplicates) {
+        const existingNotification = await prisma.concrete_notification.findFirst({
+            where: {
+                notificationID: notification.id,
+                ...getUserRelation(user),
+                contextID: context.uniqueId
+            }
+        });
+
+        if (existingNotification)
+            throw new Error(`Duplicate Notification(${notification.id}) for User(${user.id}) with Context(${context.uniqueId})`);
+    }
+
+    // First of all we commit the notification to the database, which allows us to recover if the backend crashes
+    const concreteNotification = await prisma.concrete_notification.create({
+        data: {
+            // the unique id is automatically created by the database
+            notificationID: notification.id,
+            state: ConcreteNotificationState.PENDING,
+            ...getUserRelation(user),
+            sentAt: new Date(),
+            contextID: context.uniqueId,
+            context
+        }
+    });
+
+    return concreteNotification;
+}
+
+async function deliverNotification(concreteNotification: ConcreteNotification, notification: Notification, user: Person, context: Context): Promise<void> {
+    debug(`Sending ConcreteNotification(${concreteNotification.id}) of Notification(${notification.id}) to User(${user.id})`);
+
+    try {
+        const channel = channels.find(it => it.canSend(notification.id));
+        if (!channel) {
+            throw new Error(`No fitting channel found for Notification(${notification.id})`);
+        }
+
+        await channel.send(notification.id, context);
+        await prisma.concrete_notification.update({
+            data: {
+                state: ConcreteNotificationState.SENT,
+                sentAt: new Date(),
+                // drop the context, as it is irrelevant from now on, and only eats up memory
+                context: null
+            },
+            where: {
+                id: concreteNotification.id
+            }
+        });
+
+        info(`Succesfully sent ConcreteNotification(${concreteNotification.id}) of Notification(${notification.id}) to User(${user.id})`);
+
+    } catch (error) {
+        warn(`Failed to send ConcreteNotification(${concreteNotification.id}) of Notification(${notification.id}) to User(${user.id})`, error);
+
+        await prisma.concrete_notification.update({
+            data: {
+                sentAt: new Date(),
+                state: ConcreteNotificationState.ERROR
+            },
+            where: {
+                id: concreteNotification.id
+            }
+        });
+
+
+    }
+}
+
+
+/* --------------------------------------- Utils ----------------------------------------------- */
+
+function getUserRelation(user: Person) {
+    const pupilId = user instanceof Pupil ? user.id : undefined;
+    const studentId = user instanceof Student ? user.id : undefined;
+
+    if (!pupilId && !studentId) {
+        throw new Error("A Person was neither a Student nor a Pupil");
+    }
+
+    return { studentId, pupilId };
+}
+
+async function getPersonForConcreteNotification(concreteNotification: ConcreteNotification) {
+    if (concreteNotification.pupilId)
+        return await getManager().findOneOrFail(Pupil, { where: { id: concreteNotification.pupilId }});
+
+    if (concreteNotification.studentId)
+        return await getManager().findOneOrFail(Student, { where: { id: concreteNotification.studentId }});
+}
+
--- common/notification/types.ts
+++ common/notification/types.ts
@@ -1,4 +1,4 @@
-import { Person } from "common/entity/Person";
+import { Person } from "../entity/Person";
 import { Pupil } from "../entity/Pupil";
 import { Student } from "../entity/Student";
 
@@ -6,51 +6,37 @@ export type NotificationID = string; // either our own or we reuse them from Mai
 export type CategoryID = string; // categories as means to opt out from a certain category of mails
 // An action is something the user does. One action might trigger / cancel multiple notifications
 export type ActionID = string;
-export type Email = `${string}@${string}.${string}`; 
+export type Email = `${string}@${string}.${string}`;
+
+// Prisma exports lowercase types, but we want capitalized types
+export {
+    concrete_notification as ConcreteNotification,
+    notification as Notification,
+    concrete_notification_state as ConcreteNotificationState
+} from '.prisma/client';
 
 
 
-
-export interface Reminder {
-    cancelledOnAction: ActionID[];
-    delay: number;
-    interval: number;
-}
-
-export{ sentNotification, notification } from '.prisma/client'; 
-
-
-
-// Currently the templates have a lot of repeating fields, such as "userFirstName"
-// Maybe we can generalize into a context that is partially available for each Notification
-// Mailjet does support objects in variables, so just dumping the Context to Mailjet will work  
-// TODO: How to migrate there from current templates? Copy & Paste in Mailjet?
+// Previously the templates had a lot of repeating fields, such as "userFirstName"
+// by generalizing into a context that is partially available for each Notification, this was cleaned up
 export interface NotificationContext {
+    uniqueId: string;
     student?: Student; // set if the pupil is notified, and a certain student is relevant, this property is set
     pupil?: Pupil; // if the pupil is notified and a certain student is somehow relevant, this property is set
-    replyToAddress? : Email; 
-    // I'm not sure if it is useful to maintain the variable shape in the backend
-    //  as a missmatch with the Mailjet template won't be detected anyways. I guess we can just allow any further props:
+    replyToAddress? : Email;
+    // As it is not quite useful to maintain the variable shape in the backend as a missmatch with the Mailjet template won't be detected anyways,
+    // further props can be set at will
     [key: string]: any;
 }
 
 // The user is always known, also for notifications sent by Actions / Reminders
 export interface Context extends NotificationContext {
     user: Person;
-    title: string;
-    
 }
 
+// Abstract away from the core: Channels are our Ports to external notification systems (Mailjet, SMS, ...)
 export interface Channel {
-    type: "mailjet";
+    type: "mailjet" /* | ... */;
     send(id: string, context: Context): Promise<any>;
-    canSend(id: NotificationID): boolean; 
-}
-
-export namespace Notification { // supports both SMS & E-Mail
-    // notifications: NotificationTemplate[];
-    // reminders: Reminder[];
-    // channels: Channel[];
-
-    
+    canSend(id: NotificationID): boolean;
 }
