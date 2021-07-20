164214b8e69770a7a6a38726fbd0238ac7ea523a
Jonas <JonasWilms2000@gmail.com>
2021-07-20 14:16:08 +0200

    Add further logging

--- graphql/logging.ts
+++ graphql/logging.ts
@@ -7,14 +7,41 @@ const isDev = process.env.NODE_ENV === "dev";
 
 export const GraphQLLogger: ApolloServerPlugin = {
     async requestDidStart(requestContext) {
-        logger.debug(`Request did start`);
+        logger.debug(`Request started`);
 
-        return {
-            async didResolveOperation(requestContext) {
-                logger.debug(`Starting Operation ${requestContext.operationName}`);
+        const handler: GraphQLRequestListener = {
+            async parsingDidStart() {
+                logger.debug(`Parsing started`);
+                return async (error) => {
+                    if (error) {
+                        logger.warn(`Parsing failed with:`, error);
+                    } else {
+                        logger.debug(`Parsing succeeded`);
+                    }
+                };
+            },
+            async validationDidStart() {
+                logger.debug(`Validation started`);
+                return async (errors) => {
+                    if (errors.length) {
+                        logger.warn(`Validation failed with:`, errors);
+                    } else {
+                        logger.debug(`Validation succeeded`);
+                    }
+                };
+            },
+            async executionDidStart() {
+                logger.debug(`Execution started`);
+                return { async executionDidEnd(error) {
+                    if (error) {
+                        logger.warn(`Execution failed with:`, error);
+                    } else {
+                        logger.debug(`Execution succeeded`);
+                    }
+                } };
             },
             async didEncounterErrors(requestContext) {
-                logger.warn(`An error  occured:`, requestContext.errors);
+                logger.warn(`An error  occurred:`, requestContext.errors);
             },
             async willSendResponse(requestContext) {
                 logger.debug(`Processed the query:\n${requestContext.request.query}`);
@@ -28,5 +55,7 @@ export const GraphQLLogger: ApolloServerPlugin = {
                 }
             }
         };
+
+        return handler;
     }
 };
