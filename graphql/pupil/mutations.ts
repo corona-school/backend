4c2a5535f31d830d378b4384d950ff8aa3fe2ae9
Jonas <JonasWilms2000@gmail.com>
2021-07-20 19:10:59 +0200

    fixup

--- graphql/pupil/mutations.ts
+++ graphql/pupil/mutations.ts
@@ -1,22 +1,22 @@
 import { prisma } from "../../common/prisma";
-import { Resolver, Mutation, Root } from "type-graphql";
+import { Resolver, Mutation, Root, Arg } from "type-graphql";
 import * as GraphQLModel from "../generated/models";
 import { activatePupil, deactivatePupil } from "../../common/pupil/activation";
 
-const getPupil = (pupil: GraphQLModel.Pupil) => prisma.pupil.findUnique({ where: { id: pupil.id } });
+const getPupil = (pupilId: number) => prisma.pupil.findUnique({ where: { id: pupilId } });
 
 @Resolver(of => GraphQLModel.Pupil)
 export class MutatePupilResolver {
     @Mutation(returns => Boolean)
-    async pupilActivate(@Root() _pupil: GraphQLModel.Pupil): Promise<boolean> {
-        const pupil = await getPupil(_pupil);
+    async pupilActivate(@Arg("pupilId") pupilId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
         await activatePupil(pupil);
         return true;
     }
 
     @Mutation(returns => Boolean)
-    async pupilDeactivate(@Root() _pupil: GraphQLModel.Pupil): Promise<boolean> {
-        const pupil = await getPupil(_pupil);
+    async pupilDeactivate(@Arg("pupilId") pupilId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
         await deactivatePupil(pupil);
         return true;
     }
