170f1a30c200a56d2625d963dccc40dc82958892
Jonas <JonasWilms2000@gmail.com>
2021-07-27 18:33:25 +0200

    Cleanup

--- graphql/match/mutations.ts
+++ graphql/match/mutations.ts
@@ -2,31 +2,25 @@ import { prisma } from "../../common/prisma";
 import { Resolver, Mutation, Root, Arg, Authorized } from "type-graphql";
 import * as GraphQLModel from "../generated/models";
 import { Role } from "../authorizations";
+import { getMatch, getPupil, getStudent } from "../util";
 
-const getPupil = (pupilId: number) => prisma.pupil.findUnique({ where: { id: pupilId }, rejectOnNotFound: true });
-
-@Resolver(of => GraphQLModel.Pupil)
-export class MutatePupilResolver {
+@Resolver(of => GraphQLModel.Match)
+export class MutateMatchResolver {
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async addPupilTutoringMatch(/*to do*/ ): Promise<boolean>{
-        /*to do*/
+    async matchAdd(@Arg("pupilId") pupilId: number, @Arg("studentId") studentId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
+        const student = await getStudent(studentId);
+        /* TODO */
         return true;
     }
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async dissolveTutoringMatch(/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        return true;
-    }
-
-    @Mutation(returns => Boolean)
-    @Authorized(Role.ADMIN)
-    async editTutoringMatch(/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        /*Edit subjects,status and priority*/
+    async matchDissolve(@Arg("matchId") matchId: number): Promise<boolean> {
+        const match = await getMatch(matchId);
+        /* TODO */
         return true;
     }
 }
--- graphql/pupil/mutations.ts
+++ graphql/pupil/mutations.ts
@@ -1,10 +1,8 @@
-import { prisma } from "../../common/prisma";
 import { Resolver, Mutation, Root, Arg, Authorized } from "type-graphql";
 import * as GraphQLModel from "../generated/models";
 import { activatePupil, deactivatePupil } from "../../common/pupil/activation";
 import { Role } from "../authorizations";
-
-const getPupil = (pupilId: number) => prisma.pupil.findUnique({ where: { id: pupilId }, rejectOnNotFound: true });
+import { getPupil, getSubcourse } from "../util";
 
 @Resolver(of => GraphQLModel.Pupil)
 export class MutatePupilResolver {
@@ -26,58 +24,34 @@ export class MutatePupilResolver {
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async resendVerificationMail(@Arg("pupilId") pupilId: number/*to do*/): Promise<boolean> {
-        /*to do*/
+    async pupilResendVerificationMail(@Arg("pupilId") pupilId: number): Promise<boolean> {
+        /* TODO */
         return true;
     }
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async addTutoringRequest(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
+    async pupilChangeStatus(@Arg("pupilId") pupilId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
+        /* TODO: choose between applied(not selectable),accepted,rejection,deregistration */
         return true;
     }
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async editTutoringRequest(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        /*Edit subjects,status and priority*/
+    async pupilJoinSubcourse(@Arg("pupilId") pupilId: number, @Arg("subcourseId") subcourseId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
+        const subcourse = await getSubcourse(subcourseId);
+
+        /* TODO */
         return true;
     }
 
     @Mutation(returns => Boolean)
     @Authorized(Role.ADMIN)
-    async takeBackTutoringRequest(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        return true;
-    }
-
-    @Mutation(returns => Boolean)
-    @Authorized(Role.ADMIN)
-    async changePupilStatus(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        /*choose between applied(not selectable),accepted,rejection,deregistration*/
-        return true
-    }
-
-    @Mutation(returns => Boolean)
-    @Authorized(Role.ADMIN)
-    async addCourse(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        return true;
-    }
-
-    @Mutation(returns => Boolean)
-    @Authorized(Role.ADMIN)
-    async deregisterPupil(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
-        /*to do*/
-        return true;
-    }
-
-    @Mutation(returns => Boolean)
-    @Authorized(Role.ADMIN)
-    async seePupilActions(@Arg("pupilId") pupilId: number/*to do*/ ): Promise<boolean>{
+    async pupilUnjoinSubcourse(@Arg("pupilId") pupilId: number, @Arg("subcourseId") subcourseId: number): Promise<boolean> {
+        const pupil = await getPupil(pupilId);
+        const subcourse = await getSubcourse(subcourseId);
         /*to do*/
         return true;
     }
