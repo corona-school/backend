e7763ba52bcf2d10d1c621de0ae32c98867c985e
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2021-08-01 22:45:18 +0200

    User transaction logs

--- graphql/pupil/fields.ts
+++ graphql/pupil/fields.ts
@@ -1,4 +1,4 @@
-import { Subcourse, Pupil, Concrete_notification } from "../generated";
+import { Subcourse, Pupil, Concrete_notification, Log } from "../generated";
 import { Authorized, Field, FieldResolver, Resolver, Root } from "type-graphql";
 import { prisma } from "../../common/prisma";
 import { Role } from "../authorizations";
@@ -27,5 +27,14 @@ export class ExtendFieldsPupilResolver {
     @Authorized(Role.ADMIN)
     async concreteNotifications(@Root() pupil: Required<Pupil>) {
         return await prisma.concrete_notification.findMany({ where: { userId: getUserId(pupil) }});
+    }
+
+    @FieldResolver(type => [Log])
+    @Authorized(Role.ADMIN)
+    async logs(@Root() pupil: Required<Pupil>) {
+        return await prisma.log.findMany({
+            where: { user: pupil.wix_id },
+            orderBy: { createdAt: "asc" }
+        });
     }
 }
