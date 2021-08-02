4cec7dc7fcbd3b925e7b52390b0692252d081b9d
Jonas <JonasWilms2000@gmail.com>
2021-08-02 12:15:23 +0200

    Use Basic Auth for User authentication

--- graphql/context.ts
+++ graphql/context.ts
@@ -2,6 +2,24 @@ import type { PrismaClient } from "@prisma/client";
 import { Role } from "./authorizations";
 import { prisma } from "../common/prisma";
 import { getLogger } from "log4js";
+import * as basicAuth from "basic-auth";
+import * as crypto from "crypto";
+
+/* time safe comparison adapted from
+    https://github.com/LionC/express-basic-auth/blob/master/index.js
+    https://github.com/Bruce17/safe-compare/blob/master/index.js
+*/
+function timingSafeCompare(a: string, b: string) {
+    const aLen = Buffer.byteLength(a);
+    const bLen = Buffer.byteLength(b);
+
+    const bufA = Buffer.alloc(aLen, 0, 'utf8');
+    bufA.write(a);
+    const bufB = Buffer.alloc(aLen, 0, 'utf8');
+    bufB.write(b);
+
+    return crypto.timingSafeEqual(bufA, bufB) && aLen === bLen;
+}
 
 export interface GraphQLContext {
     user: { roles: Role[] };
@@ -15,12 +33,17 @@ if (!process.env.ADMIN_AUTH_TOKEN) {
 }
 
 export default function injectContext({ req }) {
-    const token = req.headers.authorization || '';
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const auth = basicAuth.parse(req);
 
     let roles: Role[] = [];
 
-    if (process.env.ADMIN_AUTH_TOKEN && token === process.env.ADMIN_AUTH_TOKEN) {
+    if (process.env.ADMIN_AUTH_TOKEN && auth && auth.name === "admin") {
+        if (!timingSafeCompare(process.env.ADMIN_AUTH_TOKEN, auth.pass)) {
+            authLogger.warn(`Admin failed to authenticate from ${ip}`);
+            throw new Error("Invalid Admin Password");
+        }
+
         roles.push(Role.ADMIN);
         authLogger.info(`Admin authenticated from ${ip}`);
     } else {
