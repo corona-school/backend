3c2bcfdefb93dc1ab7a7b42e866f7b61180f8272
Jonas <JonasWilms2000@gmail.com>
2021-08-14 19:11:57 +0200

    Add rule node visitors

--- linter/graphql-authorized.js
+++ linter/graphql-authorized.js
@@ -1,5 +1,38 @@
 module.exports = {
+    meta: {
+        type: "problem",
+
+        docs: {
+            description: "",
+            category: "Lernfair Rule",
+            recommended: true,
+            url: "https://github.com/corona-school/backend/tree/master/linter"
+        },
+        fixable: "code"
+    },
     create(context) {
-        throw new Error("It worked!");
+        let inResolver = false;
+
+        return {
+            "ClassDeclaration": function(classNode) {
+                const isResolver = classNode.decorators.some(it => it.expression.expression.escapedText === "Resolver");
+                console.log("Visiting Class", classNode, isResolver);
+                if (isResolver) {
+                    inResolver = true;
+                }
+            },
+            "ClassDeclaration:exit": function(classNode) {
+                inResolver = false;
+            },
+            "MethodDeclaration": function(methodNode) {
+                if (!inResolver) {
+                    return;
+                }
+
+                const authorizationCheck = methodNode.decorators.some(it => it.expression.expression.escapedText === "Authorized");
+
+                console.log("Method in Resolver", methodNode, authorizationCheck);
+            }
+        };
     }
 };
