40f76b4af39c04fe1c241c938ed98ea6cb960511
Jonas <JonasWilms2000@gmail.com>
2021-08-14 16:54:57 +0200

    fix import

--- graphql/project_match/fields.ts
+++ graphql/project_match/fields.ts
@@ -2,7 +2,7 @@ import { Role } from "../authorizations";
 import { Authorized, Field, FieldResolver, Resolver, Root } from "type-graphql";
 import { prisma } from "../../common/prisma";
 import { Subcourse, Pupil, Project_match, Student } from "../generated";
-import { LimitEstimated } from "graphql/complexity";
+import { LimitEstimated } from "../complexity";
 
 @Resolver(of => Project_match)
 export class ExtendedFieldsProjectMatchResolver {
