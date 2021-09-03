e32167e777c5eceee134c7bf8dbeb66bdc726938
Jonas <JonasWilms2000@gmail.com>
2021-09-03 12:24:29 +0200

    Add subjects to Match as virtual field

--- graphql/match/fields.ts
+++ graphql/match/fields.ts
@@ -1,10 +1,28 @@
 import { Role } from "../authorizations";
-import { Authorized, Field, FieldResolver, Resolver, Root } from "type-graphql";
+import { Authorized, Field, FieldResolver, Int, ObjectType, Resolver, Root } from "type-graphql";
 import { prisma } from "../../common/prisma";
 import { Subcourse, Pupil, Match, Student } from "../generated";
 import { LimitEstimated } from "../complexity";
 import { getStudent, getPupil } from "../util";
 import { getOverlappingSubjects } from "../../common/match/util";
+
+@ObjectType()
+class Range { // GraphQL Type for common/entity/Student -> Subject.range
+    @Field(type => Int)
+    min: number;
+    @Field(type => Int)
+    field: number;
+}
+
+@ObjectType() 
+class Subject { // GraphQL Type for common/entity/Student -> Subject
+    @Field()
+    name: string;
+
+    @Field(type => Range)
+    grade: Range; 
+}
+
 
 @Resolver(of => Match)
 export class ExtendedFieldsMatchResolver {
@@ -26,7 +44,8 @@ export class ExtendedFieldsMatchResolver {
         });
     }
 
-    @FieldResolver(returns => [String])
+    @FieldResolver(returns => [Subject])
+    @Authorized(Role.ADMIN)
     async subjects(@Root() match: Match) {
         const student = await getStudent(match.studentId);
         const pupil = await getPupil(match.pupilId);
