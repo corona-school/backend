058be62547edff12253d94514846a0a3f2f0dad7
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2021-10-25 13:51:08 +0200

    Create certificate of conduct in both cases, when `criminalRecords`is true or false

--- common/certificate-of-conduct/certificateOfConduct.ts
+++ common/certificate-of-conduct/certificateOfConduct.ts
@@ -1,4 +1,3 @@
-import {Prisma} from "@prisma/client";
 import { getLogger } from "log4js";
 import {prisma} from "../prisma";
 import {deactivateStudent} from "../student/activation";
@@ -6,19 +5,18 @@ import {getStudent} from "../../graphql/util";
 const logger = getLogger("Certificate of Conduct");
 
 export async function create(dateOfInspection, dateOfIssue, criminalRecords, inspectingScreenerId, studentId) {
-    if (!criminalRecords) {
-        const result = await prisma.certificate_of_conduct.create({
-            data: {
-                dateOfInspection: dateOfInspection,
-                dateOfIssue: dateOfIssue,
-                criminalRecords: criminalRecords,
-                inspectingScreenerId: inspectingScreenerId,
-                studentId: studentId
-            }
-        });
+    const result = await prisma.certificate_of_conduct.create({
+        data: {
+            dateOfInspection: dateOfInspection,
+            dateOfIssue: dateOfIssue,
+            criminalRecords: criminalRecords,
+            inspectingScreenerId: inspectingScreenerId,
+            studentId: studentId
+        }
+    });
+    logger.info(`Certificate of Conduct (${result.id}) created\n`);
 
-        logger.info(`Certificate of Conduct (${result.id}) created\n`);
-    } else {
+    if (criminalRecords) {
         const student = await getStudent(studentId);
         await deactivateStudent(student);
         //TODO: Get the student and dissolve the matches, delete courses if sole instructor, and remove from course if there are other instructors.
