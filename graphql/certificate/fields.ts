d7842ddfcd7ca89d99a9d69c2b6b1a0260c3c221
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2021-11-07 20:43:38 +0100

    Add subject formatting

--- graphql/certificate/fields.ts
+++ graphql/certificate/fields.ts
@@ -1,7 +1,7 @@
 import * as GraphQLModel from "../generated/models";
 import { Role } from "../authorizations";
 import { Arg, Authorized, Ctx, FieldResolver, Resolver, Root } from "type-graphql";
-import { participation_certificate } from "@prisma/client";
+import { participation_certificate as ParticipationCertificate } from "@prisma/client";
 import { getCertificatePDF, Language, LANGUAGES } from "../../common/certificate";
 import { getSessionStudent } from "../authentication";
 
@@ -9,12 +9,18 @@ import { getSessionStudent } from "../authentication";
 export class ExtendedFieldsParticipationCertificateResolver {
     @FieldResolver(returns => String)
     @Authorized(Role.STUDENT)
-    async pdf(@Ctx() context, @Root() certificate: participation_certificate, @Arg("language") language: string) {
+    async pdf(@Ctx() context, @Root() certificate: ParticipationCertificate, @Arg("language") language: string) {
         if (!LANGUAGES.includes(language as Language)) {
             throw new Error(`Unknown language '${language}'`);
         }
 
         const student = await getSessionStudent(context);
         return await getCertificatePDF(certificate.uuid, student, language as Language);
+    }
+
+    @FieldResolver(returns => [String])
+    @Authorized(Role.ADMIN, Role.STUDENT)
+    subjectsFormatted(@Root() certificate: ParticipationCertificate) {
+        return certificate.subjects.split(",");
     }
 }
