37fe33382f7ccdc04a345df535b2b3ba0c07e575
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2021-11-11 21:12:11 +0100

    Linter happiness

--- graphql/index.ts
+++ graphql/index.ts
@@ -69,7 +69,7 @@ const schema = buildSchemaSync({
 
         /* ParticipationCertificate */
         ExtendedFieldsParticipationCertificateResolver,
-        MutateParticipationCertificateResolver,
+        MutateParticipationCertificateResolver
     ],
     authChecker
 });
