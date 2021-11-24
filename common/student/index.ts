6e928934a0b6a1f4f080976b90bb7ee39e66b150
Ayush Pandey <8898523+ayushpandey8439@users.noreply.github.com>
2021-11-24 21:27:57 +0530

    Update index.ts
    Fix Typo

--- common/student/index.ts
+++ common/student/index.ts
@@ -3,7 +3,7 @@ import {prisma} from "../prisma";
 
 export async function updateCertificateOfConduct(student : Student, criminalRecord: boolean, dateOfIssue: Date, dateOfInspection: Date) {
     if (!student.active) {
-        throw new Error("Student was deactivated. Tyring to add certificate.");
+        throw new Error("Student was deactivated. Trying to add certificate.");
     }
 
     await prisma.certificate_of_conduct.update({
