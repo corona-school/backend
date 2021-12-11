4c65dbe6d5bc49eb1b41880618bb0db64a3cddab
Marius <38134006+realmayus@users.noreply.github.com>
2021-12-11 18:54:44 +0100

    Update put.ts

--- common/file-bucket/put.ts
+++ common/file-bucket/put.ts
@@ -4,7 +4,7 @@ import { s3 } from "./s3";
 const logger = getLogger();
 
 
-/// Stores the given buffer in the default bucket associated with the given key
+/// Stores the given buffer in the specified bucket associated with the given key
 /// NOTE: May throw in case the upload failed
 export async function putFile(f: Buffer, key: string, bucket: string) {
     const request = s3.putObject({
