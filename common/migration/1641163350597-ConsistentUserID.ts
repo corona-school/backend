7532c3d607a7a2bb7e4fbe555344597d7d96301c
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-01-03 00:11:45 +0100

    Adapt migration to hopefully not crash

--- common/migration/1641163350597-ConsistentUserID.ts
+++ common/migration/1641163350597-ConsistentUserID.ts
@@ -4,7 +4,13 @@ export class ConsistentUserID1641163350597 implements MigrationInterface {
     name = 'ConsistentUserID1641163350597';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "screener" ADD "userID" character varying NOT NULL`);
+        // Create new column
+        await queryRunner.query(`ALTER TABLE "screener" ADD "userID" character varying`);
+        // Fill with new UUIDs
+        await queryRunner.query(`UPDATE "screener" SET "userID" = gen_random_uuid ()`);
+
+        // As now all screeners should have a userID, uniqueness constraints can be enforced
+        await queryRunner.query(`ALTER TABLE "screener" ALTER COLUMN "userID" SET NOT NULL`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_157bc8d04bf357f137cdec4423" ON "screener" ("userID") `);
     }
 
