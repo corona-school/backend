909a001782099857c13da6c836cc53ac9f2e2576
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-01-06 19:20:17 +0100

    Also rewrite concrete_notifications

--- common/migration/1641163350597-ConsistentUserID.ts
+++ common/migration/1641163350597-ConsistentUserID.ts
@@ -12,6 +12,27 @@ export class ConsistentUserID1641163350597 implements MigrationInterface {
         // As now all screeners should have a userID, uniqueness constraints can be enforced
         await queryRunner.query(`ALTER TABLE "screener" ALTER COLUMN "userID" SET NOT NULL`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_157bc8d04bf357f137cdec4423" ON "screener" ("userID") `);
+
+        // Rewrite foreign keys of concrete_notification table
+        await queryRunner.query(
+            `UPDATE SET
+              "concrete_notification"."userId" = "student"."wix_id"
+             FROM "concrete_notification"
+             INNER JOIN "student" ON "student"."id" = CAST(SUBSTRING("userId" FROM 9) AS INT)
+             WHERE
+               SUBSTRING("userId" FROM 0 FOR 9) = 'student/';`
+        );
+
+        await queryRunner.query(
+            `UPDATE SET
+              "concrete_notification"."userId" = "pupil"."wix_id"
+             FROM "concrete_notification"
+             INNER JOIN "pupil" ON "pupil"."id" = CAST(SUBSTRING("userId" FROM 7) AS INT)
+             WHERE
+               SUBSTRING("userId" FROM 0 FOR 7) = 'pupil/';`
+        );
+
+        // For completeness one would also need to support Mentors and Screeners here, though we do not currently send mails to them via the notification system
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
