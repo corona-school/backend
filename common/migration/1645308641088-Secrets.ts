e144acd24a50fbcf339fe3d5750996857f24b5cf
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-02-20 21:43:18 +0100

    linter hapiness

--- common/migration/1645308641088-Secrets.ts
+++ common/migration/1645308641088-Secrets.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class Secrets1645308641088 implements MigrationInterface {
-    name = 'Secrets1645308641088'
+    name = 'Secrets1645308641088';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "secret" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" character varying NOT NULL, "type" integer NOT NULL, "secret" character varying NOT NULL, "expiresAt" TIMESTAMP NOT NULL, "lastUsed" TIMESTAMP NOT NULL, CONSTRAINT "PK_6afa4961954e17ec2d6401afc3d" PRIMARY KEY ("id"))`);
