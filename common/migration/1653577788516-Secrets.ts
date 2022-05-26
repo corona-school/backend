51e1b7c8a01239078a6cf1ae2a66a88c9f5ee6e4
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-05-26 19:29:52 +0200

    linter hapiness

--- common/migration/1653577788516-Secrets.ts
+++ common/migration/1653577788516-Secrets.ts
@@ -5,7 +5,7 @@ export class Secrets1653577788516 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "secret_type_enum" AS ENUM('PASSWORD', 'TOKEN', 'EMAIL_TOKEN')`);
-        await queryRunner.query(`CREATE TABLE "secret" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" character varying NOT NULL, "type" "secret_type_enum" NOT NULL, "secret" character varying NOT NULL, "expiresAt" TIMESTAMP, "lastUsed" TIMESTAMP, CONSTRAINT "PK_6afa4961954e17ec2d6401afc3d" PRIMARY KEY ("id"))`);   
+        await queryRunner.query(`CREATE TABLE "secret" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" character varying NOT NULL, "type" "secret_type_enum" NOT NULL, "secret" character varying NOT NULL, "expiresAt" TIMESTAMP, "lastUsed" TIMESTAMP, CONSTRAINT "PK_6afa4961954e17ec2d6401afc3d" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
