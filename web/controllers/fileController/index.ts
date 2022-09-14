07794dc1d999e2ba431f12f3ffc7c083824616c6
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-09-14 22:41:32 +0200

    Logging and Error handling

--- web/controllers/fileController/index.ts
+++ web/controllers/fileController/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { getLogger } from "log4js";
 import multer from "multer";
 import { File, FileID, getFile, addFile } from "../../../graphql/files";
+
+const log = getLogger("File API");
 
 const fileUpload = multer({
     limits: {
@@ -16,9 +19,14 @@ fileRouter.post("/upload", fileUpload.single("file"), (req, res) => {
         return res.status(400).send("Missing file");
     }
 
-    const fileID = addFile(req.file);
-
-    return res.status(200).send(fileID);
+    try {
+        const fileID = addFile(req.file);
+        log.info(`File was uploaded from ${req.ip} with id ${fileID}`);
+        return res.status(200).send(fileID);
+    } catch (error) {
+        log.error(`${req.ip} Failed to upload file`, error);
+        return res.status(503).send("Could not upload file. Please wait some time for the service to recover before retrying");
+    }
 });
 
 fileRouter.get("/download/:fileID", (req, res) => {
@@ -33,6 +41,7 @@ fileRouter.get("/download/:fileID", (req, res) => {
         res.type(file.mimetype);
         res.send(file.buffer);
     } catch (error) {
+        log.error(`${req.ip} failed to download file `, error);
         return res.status(404).send("File not Found");
     }
 
