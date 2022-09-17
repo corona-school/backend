79da856f4a4707ee1ffba5f3199a4cb723664478
Leo310 <leodev310@gmail.com>
2022-09-17 12:43:15 +0200

    Fix: inaccuracies

--- graphql/lecture/fields.ts
+++ graphql/lecture/fields.ts
@@ -1,16 +1,15 @@
 import { Role } from '../authorizations';
-import { Arg, Authorized, Ctx, FieldResolver, Query, Resolver, Root } from 'type-graphql';
-import { Course, Lecture, Subcourse, Pupil, Bbb_meeting as BBBMeeting } from '../generated';
+import { Arg, Authorized, Ctx, FieldResolver, Resolver, Root } from 'type-graphql';
+import { Lecture } from '../generated';
 import { GraphQLContext } from '../context';
 import { getSessionStudent } from '../authentication';
-import { prisma } from '../../common/prisma';
 
 @Resolver((of) => Lecture)
 export class ExtendedFieldsLectureResolver {
     @FieldResolver((returns) => Boolean)
     @Authorized(Role.ADMIN, Role.STUDENT)
-    async isInstructor(@Ctx() context: GraphQLContext, @Root() subcourse: Subcourse, @Arg('studentId', { nullable: true }) studentId: number) {
+    async isInstructor(@Ctx() context: GraphQLContext, @Root() lecture: Lecture, @Arg('studentId', { nullable: true }) studentId: number) {
         const student = await getSessionStudent(context, studentId);
-        return (await prisma.lecture.count({ where: { instructorId: student.id } })) > 0;
+        return lecture.instructorId === student.id;
     }
 }
