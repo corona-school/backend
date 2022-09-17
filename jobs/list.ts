c4877bdeec051d6a4666bc49ceba2c774dc13924
Jonas Wilms <jonaswilms2000@gmail.com>
2022-09-17 16:19:09 +0200

    New interest confirmations (#328)
    * Draft new interest confirmations
    
    * fixups
    
    * Cleanup unconfirmed interests
    
    * Remove interest confirmation on match creation
    
    * Expose pupils that will be reminded (for debugging) and switch back to old implementation for now

--- jobs/list.ts
+++ jobs/list.ts
@@ -13,11 +13,13 @@ import interestConfirmationRequestReminders from "./periodic/interest-confirmati
 import * as Notification from "../common/notification";
 import deactivateMissingCoc from "./periodic/deactivate-missing-coc";
 import { cleanupSecrets } from "../common/secret";
+import { runInterestConfirmations } from "../common/match/pool";
 
 // A list of all jobs that should be scheduled at the moment
 export const allJobs: CSCronJob[] = [
     // every morning, quite early (but only on Monday and Thursday)
     { cronTime: "00 55 07 * * 1,4", jobFunction: initialInterestConfirmationRequests},
+    // { cronTime: "00 55 07 * * 1,4", jobFunction: runInterestConfirmations },
     // every morning
     { cronTime: "00 00 09 * * *", jobFunction: screeningReminderJob},
     { cronTime: "00 34 08 * * *", jobFunction: projectMatchMaking},
