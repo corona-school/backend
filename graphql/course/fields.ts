edd000fc9ec125cc2746229ef8ea024fea79a7e2
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-09-24 15:12:22 +0200

    Expose course tags

--- graphql/course/fields.ts
+++ graphql/course/fields.ts
@@ -1,4 +1,4 @@
-import { Course, Subcourse } from '../generated';
+import { Course, Subcourse, Course_tag as CourseTag } from '../generated';
 import { Authorized, FieldResolver, Resolver, Root } from 'type-graphql';
 import { prisma } from '../../common/prisma';
 import { Role } from '../authorizations';
@@ -7,7 +7,7 @@ import { accessURLForKey } from '../../common/file-bucket/s3';
 @Resolver((of) => Course)
 export class ExtendedFieldsCourseResolver {
     @FieldResolver((returns) => [Subcourse])
-    @Authorized(Role.ADMIN)
+    @Authorized(Role.ADMIN, Role.OWNER)
     async subcourses(@Root() course: Course) {
         return await prisma.subcourse.findMany({
             where: {
@@ -17,7 +17,7 @@ export class ExtendedFieldsCourseResolver {
     }
 
     @FieldResolver((returns) => String, { nullable: true })
-    @Authorized(Role.ADMIN)
+    @Authorized(Role.UNAUTHENTICATED)
     image(@Root() course: Course) {
         if (!course.imageKey) {
             return null;
@@ -25,4 +25,19 @@ export class ExtendedFieldsCourseResolver {
 
         return accessURLForKey(course.imageKey);
     }
+
+    @FieldResolver((returns) => [CourseTag])
+    @Authorized(Role.UNAUTHENTICATED)
+    async tags(@Root() course: Course) {
+        return await prisma.course_tag.findMany({
+            where: {
+                course_tags_course_tag: {
+                    some: {
+                        courseId: course.id
+                    }
+                }
+            }
+        });
+    }
+
 }
