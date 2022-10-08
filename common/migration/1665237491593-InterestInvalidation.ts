c1ec16a40579af800923a591033b16a8c232afd0
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-10-08 16:18:02 +0200

    linter hapiness

--- common/migration/1665237491593-InterestInvalidation.ts
+++ common/migration/1665237491593-InterestInvalidation.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class InterestInvalidation1665237491593 implements MigrationInterface {
-    name = 'InterestInvalidation1665237491593'
+    name = 'InterestInvalidation1665237491593';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "pupil_tutoring_interest_confirmation_request" ADD "invalidated" boolean NOT NULL DEFAULT false`);
