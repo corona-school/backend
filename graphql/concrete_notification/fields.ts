be72ee8e893b739f1664bc3193177ff1a17684ae
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-10-15 23:21:32 +0200

    Campaign Reporting

--- graphql/concrete_notification/fields.ts
+++ graphql/concrete_notification/fields.ts
@@ -1,7 +1,26 @@
 import { Concrete_notification as ConcreteNotification, Notification } from '../generated';
-import { Authorized, FieldResolver, Resolver, Root } from 'type-graphql';
+import { Authorized, Field, FieldResolver, Int, ObjectType, Query, Resolver, Root } from 'type-graphql';
 import { prisma } from '../../common/prisma';
 import { Role } from '../authorizations';
+import { JSONResolver } from 'graphql-scalars';
+import { ConcreteNotificationState } from 'common/entity/ConcreteNotification';
+
+@ObjectType()
+class Campaign {
+    @Field((type) => Int)
+    notificationID: number;
+    @Field((type) => JSONResolver)
+    context: {};
+
+    @Field((type) => Int)
+    drafted: number;
+    @Field((type) => Int)
+    scheduled: number;
+    @Field((type) => Int)
+    sent: number;
+    @Field((type) => Int)
+    error: number;
+}
 
 @Resolver((of) => ConcreteNotification)
 export class ExtendedFieldsConcreteNotificationResolver {
@@ -9,5 +28,51 @@ export class ExtendedFieldsConcreteNotificationResolver {
     @Authorized(Role.UNAUTHENTICATED)
     async notification(@Root() concreteNotification: ConcreteNotification) {
         return await prisma.notification.findUnique({ where: { id: concreteNotification.notificationID } });
+    }
+
+    @Query((returns) => [Campaign])
+    @Authorized(Role.ADMIN)
+    async concreteNotificationCampaign() {
+        const aggregated = await prisma.concrete_notification.groupBy({
+            _count: true,
+            by: ['notificationID', 'context', 'state'],
+        });
+
+        const byCampaign: { [key: string]: Campaign } = {};
+
+        for (const { _count, context, notificationID, state } of aggregated) {
+            const key = JSON.stringify([notificationID, context]);
+            if (!byCampaign[key]) {
+                byCampaign[key] = {
+                    context,
+                    notificationID,
+                    drafted: 0,
+                    error: 0,
+                    scheduled: 0,
+                    sent: 0,
+                };
+            }
+
+            switch (state) {
+                case ConcreteNotificationState.DELAYED:
+                    byCampaign[key].scheduled = _count;
+                    break;
+                case ConcreteNotificationState.DRAFTED:
+                    byCampaign[key].drafted = _count;
+                    break;
+                case ConcreteNotificationState.ERROR:
+                    byCampaign[key].error = _count;
+                    break;
+
+                case ConcreteNotificationState.SENT:
+                    byCampaign[key].sent = _count;
+                    break;
+
+                default:
+                    throw new Error(`Unexpected state ${state}`);
+            }
+        }
+
+        return Object.values(byCampaign);
     }
 }
