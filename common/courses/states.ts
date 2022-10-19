2d79c84d8755cb76811ee4d8f6ebff06271dc699
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-10-19 20:06:55 +0200

    Allow creation of Subcourses for not yet approved Courses
    Instead publishing is blocked. Also provide Subcourse.canPublish to check whether all requirements are met

--- common/courses/states.ts
+++ common/courses/states.ts
@@ -1,4 +1,5 @@
 import { subcourse as Subcourse } from '@prisma/client';
+import { Decision } from '../util/decision';
 import { prisma } from '../prisma';
 
 export async function hasStarted(subcourse: Subcourse) {
@@ -9,4 +10,24 @@ export async function hasStarted(subcourse: Subcourse) {
         },
     });
     return startedLectures === 0;
+}
+
+export async function canPublish(subcourse: Subcourse): Promise<Decision> {
+    const course = await prisma.course.findUnique({ where: { id: subcourse.courseId } });
+    if (course.courseState !== 'allowed') {
+        return { allowed: false, reason: 'course-not-allowed' };
+    }
+
+    const lectures = await prisma.lecture.findMany({ where: { subcourseId: subcourse.id } });
+    if (lectures.length == 0) {
+        return { allowed: false, reason: 'no-lectures' };
+    }
+
+    let currentDate = new Date();
+    const pastLectures = lectures.filter((lecture) => +lecture.start < +currentDate);
+    if (pastLectures.length !== 0) {
+        return { allowed: false, reason: 'past-lectured' };
+    }
+
+    return { allowed: true };
 }
