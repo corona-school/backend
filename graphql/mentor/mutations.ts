78830d5374ad963a849902d5ad8ad97c02c562a6
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-10-26 15:55:22 +0200

    Allow all users to call mentoringContact

--- graphql/mentor/mutations.ts
+++ graphql/mentor/mutations.ts
@@ -3,7 +3,7 @@ import { Mentor } from '../generated';
 import { Arg, Authorized, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
 import { Role } from '../authorizations';
 import { contactEmailAddress, MentoringCategory } from '../../common/mentoring/categories';
-import { getSessionStudent } from '../authentication';
+import { getSessionPupil, getSessionStudent, isSessionPupil, isSessionStudent } from '../authentication';
 import { DEFAULTSENDERS } from '../../common/mails/config';
 import mailjet from '../../common/mails/mailjet';
 import { logTransaction } from '../../common/transactionlog/log';
@@ -26,20 +26,29 @@ class MentorContactInput {
 @Resolver((of) => Mentor)
 export class MutateMentorResolver {
     @Mutation((returns) => Boolean)
-    @Authorized(Role.STUDENT)
+    @Authorized(Role.USER)
     async mentoringContact(@Ctx() context: GraphQLContext, @Arg('data') data: MentorContactInput) {
-        const student = await getSessionStudent(context);
-        const replyToAddress = student.email;
-        const replyToName = `${student.firstname} ${student.lastname}`;
+        const replyToAddress = context.user!.email;
+        const replyToName = `${context.user!.firstname} ${context.user!.lastname}`;
         const receiverAddress = contactEmailAddress(data.category);
 
         await mailjet.sendPure(data.subject, data.message, DEFAULTSENDERS.noreply, receiverAddress, replyToName, undefined, replyToAddress, replyToName);
 
-        await logTransaction('contactMentor', student, {
-            category: data.category,
-            subject: data.subject,
-            text: data.message,
-        });
+        if (isSessionStudent(context)) {
+            const student = await getSessionStudent(context);
+            await logTransaction('contactMentor', student, {
+                category: data.category,
+                subject: data.subject,
+                text: data.message,
+            });
+        } else if (isSessionPupil(context)) {
+            const pupil = await getSessionPupil(context);
+            await logTransaction('contactMentor', pupil, {
+                category: data.category,
+                subject: data.subject,
+                text: data.message,
+            });
+        }
 
         return true;
     }
