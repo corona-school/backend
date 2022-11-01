56b5de7c7ec1de7f48b8d22b5afdd5f47797d989
Jonas <JonasWilms2000@gmail.com>
2022-11-01 16:02:22 +0100

    linter hapiness

--- common/migration/1665239916440-SecretDescription.ts
+++ common/migration/1665239916440-SecretDescription.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class SecretDescription1665239916440 implements MigrationInterface {
-    name = 'SecretDescription1665239916440'
+    name = 'SecretDescription1665239916440';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "secret" ADD "description" character varying`);
