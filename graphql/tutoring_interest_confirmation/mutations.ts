970096d461c0e5e9216a28621b402711dd861ae8
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-11-03 19:24:03 +0100

    Manually request interests

--- graphql/tutoring_interest_confirmation/mutations.ts
+++ graphql/tutoring_interest_confirmation/mutations.ts
@@ -8,6 +8,10 @@ import { logTransaction } from '../../common/transactionlog/log';
 import { RedundantError } from '../../common/util/error';
 import { UserInputError } from '../error';
 import { generateToken } from '../../jobs/periodic/fetch/utils/verification';
+import { requestInterestConfirmation } from '../../common/match/interest';
+import { getLogger } from 'log4js';
+
+const logger = getLogger('MutateTutoringInterest');
 
 async function changeStatus(token: string, status: InterestConfirmationStatus) {
     const confirmation = await prisma.pupil_tutoring_interest_confirmation_request.findUnique({
@@ -74,6 +78,17 @@ export class MutateTutoringInterestConfirmationResolver {
             previousStatus: InterestConfirmationStatus.PENDING,
             newStatus: status,
         });
+
+        return true;
+    }
+
+    @Mutation((returns) => Boolean)
+    @Authorized(Role.ADMIN)
+    async tutoringInterestRequest(@Arg('pupilId') pupilId: number) {
+        const pupil = await getPupil(pupilId);
+        await requestInterestConfirmation(pupil);
+
+        logger.info(`Admin requested interest of Pupil(${pupilId})`);
 
         return true;
     }
