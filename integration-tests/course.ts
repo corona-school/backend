a62c5ca7cb861bf7764ecb5ca6f3068d28b5a0f6
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-11-08 20:55:49 +0100

    fix tests

--- integration-tests/course.ts
+++ integration-tests/course.ts
@@ -21,18 +21,18 @@ test("Admin set subcourse meetingURL and join", async () => {
 test("Search further instructors", async() => {
     const { client } = await studentOne;
 
-    const emailSearch = await client.request(`query { otherInstructors(search: "@Lern-Fair.de") { id }}`);
+    const emailSearch = await client.request(`query { otherInstructors(search: "@Lern-Fair.de", take: 100, skip: 0) { id }}`);
     assert.equal(emailSearch.otherInstructors.length, 2);
 
-    const firstnameSearch = await client.request(`query { otherInstructors(search: "melanie") { firstname }}`);
+    const firstnameSearch = await client.request(`query { otherInstructors(search: "melanie, take: 100, skip: 0) { firstname }}`);
     assert.equal(firstnameSearch.otherInstructors.length, 1);
     assert.equal(firstnameSearch.otherInstructors[0].firstname, "Melanie");
 
-    const lastnameSearch = await client.request(`query { otherInstructors(search: "meiers") { lastname }}`);
+    const lastnameSearch = await client.request(`query { otherInstructors(search: "meiers", take: 100, skip: 0) { lastname }}`);
     assert.equal(lastnameSearch.otherInstructors.length, 1);
     assert.equal(lastnameSearch.otherInstructors[0].lastname, "Meiers");
 
-    const nameSearch = await client.request(`query { otherInstructors(search: "melanie meiers") { firstname lastname }}`);
+    const nameSearch = await client.request(`query { otherInstructors(search: "melanie meiers", take: 100, skip: 0) { firstname lastname }}`);
     assert.equal(nameSearch.otherInstructors.length, 1);
     assert.equal(nameSearch.otherInstructors[0].firstname, "Melanie");
 
