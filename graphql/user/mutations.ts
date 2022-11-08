d9d1e7d840f5c6dbd71f3f34727bb2d398ba31e2
Jonas Wilms <jonaswilms2000@gmail.com>
2022-11-08 20:59:17 +0100

    Determine Login Options of User (#390)

--- graphql/user/mutations.ts
+++ graphql/user/mutations.ts
@@ -3,6 +3,8 @@ import { RateLimit } from '../rate-limit';
 import { Mutation, Resolver, Arg, Authorized } from 'type-graphql';
 import { UserType } from '../types/user';
 import { isEmailAvailable } from '../../common/user/email';
+import { determinePreferredLoginOption, LoginOption } from '../../common/secret';
+import { getUserByEmail } from '../../common/user';
 
 @Resolver((of) => UserType)
 export class MutateUserResolver {
@@ -11,5 +13,18 @@ export class MutateUserResolver {
     @RateLimit('Email Availability', 50 /* requests per */, 5 * 60 * 60 * 1000 /* 5 hours */)
     async isEmailAvailable(@Arg('email') email: string) {
         return await isEmailAvailable(email);
+    }
+
+    @Mutation((returns) => String)
+    @Authorized(Role.UNAUTHENTICATED)
+    @RateLimit('Determine Login Options', 50 /* requests per */, 5 * 60 * 60 * 1000 /* 5 hours */)
+    async userDetermineLoginOptions(@Arg('email') email: string) {
+        try {
+            const user = await getUserByEmail(email);
+            return await determinePreferredLoginOption(user);
+        } catch (error) {
+            // Invalid email
+            return LoginOption.none;
+        }
     }
 }
