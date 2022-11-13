01fb95a3b2a48ac7a3e22347378f7a9d84badba9
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-11-13 21:57:07 +0100

    Add USER_APP_DOMAIN to config and notification context

--- common/util/environment.ts
+++ common/util/environment.ts
@@ -1,1 +1,4 @@
 export const isDev = process.env.ENV === 'dev';
+
+// Domain of the User App, where we link users to in emails etc.
+export const USER_APP_DOMAIN = process.env.USER_APP_DOMAIN ?? 'user-app-dev.herokuapp.com';
