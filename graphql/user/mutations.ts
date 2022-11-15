5b83ce87f89060b43e5b1c922a75952cc62fc432
Jonas Wilms <jonaswilms2000@gmail.com>
2022-11-15 16:55:51 +0100

    Error Reporting from Frontend (#401)
    * Error Reporting from Frontend
    
    * Add logging
    
    * Fix typing
    
    * Fix null handling

--- graphql/user/mutations.ts
+++ graphql/user/mutations.ts
@@ -1,10 +1,17 @@
 import { Role } from '../authorizations';
 import { RateLimit } from '../rate-limit';
-import { Mutation, Resolver, Arg, Authorized } from 'type-graphql';
+import { Mutation, Resolver, Arg, Authorized, Ctx } from 'type-graphql';
 import { UserType } from '../types/user';
 import { isEmailAvailable } from '../../common/user/email';
 import { determinePreferredLoginOption, LoginOption } from '../../common/secret';
 import { getUserByEmail } from '../../common/user';
+import { GraphQLContext } from '../context';
+import { toPublicToken } from '../authentication';
+import mailjet from '../../common/mails/mailjet';
+import { DEFAULTSENDERS } from '../../common/mails/config';
+import { getLogger } from 'log4js';
+
+const logger = getLogger('MutateUser');
 
 @Resolver((of) => UserType)
 export class MutateUserResolver {
@@ -26,5 +33,45 @@ export class MutateUserResolver {
             // Invalid email
             return LoginOption.none;
         }
+    }
+
+    @Mutation((returns) => Boolean)
+    @Authorized(Role.UNAUTHENTICATED)
+    async userReportIssue(
+        @Ctx() context: GraphQLContext,
+        @Arg('issueTag') issueTag: string,
+        @Arg('errorMessage') errorMessage: string,
+        @Arg('errorStack') errorStack: string,
+        @Arg('logs', (type) => [String]) logs: string[],
+        @Arg('userAgent') userAgent: string
+    ) {
+        let result = '';
+        const section = (name: string, level: number) => (result += '\n\n' + ('-'.repeat(5 - level) + ' ' + name + ' ').padEnd(30 - level * 2, '-') + '\n');
+        const info = (name: string, value: string) => (result += (name + ':').padEnd(10, ' ') + value + '\n');
+
+        section('USER', 0);
+        info('IssueTag', issueTag);
+        info('UserID', context.user.userID);
+        info('E-Mail', context.user.email);
+
+        section('BACKEND', 0);
+        info('SessionID', context.sessionToken ? toPublicToken(context.sessionToken) : '-');
+        info('Roles', context.user.roles.join(', '));
+        info('Time', new Date().toISOString());
+
+        section('FRONTEND', 0);
+        info('UserAgent', userAgent);
+        info('ErrorMessage', errorMessage);
+
+        section('STACK', 1);
+        result += errorStack;
+
+        section('LOGS', 1);
+        result += logs.join('\n');
+
+        logger.info(`An issue was reported from the frontend: \n` + result);
+
+        await mailjet.sendPure(`Frontend Issue: ${errorMessage}`, result, DEFAULTSENDERS.noreply, 'backend@lern-fair.de', 'Backend', 'Tech-Team');
+        return true;
     }
 }
