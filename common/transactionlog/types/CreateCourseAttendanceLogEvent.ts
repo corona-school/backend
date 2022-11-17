4278c4aa289ae9148f15b0633b705fb56b793b9f
realmayus <realmayus@gmail.com>
2022-11-17 12:16:31 +0100

    hide IP address

--- common/transactionlog/types/CreateCourseAttendanceLogEvent.ts
+++ common/transactionlog/types/CreateCourseAttendanceLogEvent.ts
@@ -1,10 +1,12 @@
-import LogUserEvent from "./LogUserEvent";
-import LogType from "./LogType";
-import {CourseAttendanceLog} from "../../entity/CourseAttendanceLog";
-import {Pupil} from "../../entity/Pupil";
+import LogUserEvent from './LogUserEvent';
+import LogType from './LogType';
+import { CourseAttendanceLog } from '../../entity/CourseAttendanceLog';
+import { Pupil } from '../../entity/Pupil';
 
 export default class CreateCourseAttendanceLogEvent extends LogUserEvent {
     constructor(pupil: Pupil, courseAttendanceLog: CourseAttendanceLog) {
-        super(LogType.CREATED_COURSE_ATTENDANCE_LOG, pupil, {courseAttendanceLog: courseAttendanceLog});
+        let log = Object.assign({}, courseAttendanceLog);
+        log.ip = undefined;
+        super(LogType.CREATED_COURSE_ATTENDANCE_LOG, pupil, { courseAttendanceLog: courseAttendanceLog });
     }
 }
