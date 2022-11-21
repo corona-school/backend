e25914e8e083f06981629e371c16e09ddc9ab215
Andreas (AK) <andreas@typedigital.de>
2022-11-21 23:53:29 +0100

    add auth and support for multiple connections

--- common/websocket/index.ts
+++ common/websocket/index.ts
@@ -1,12 +1,13 @@
 import { Server } from 'http';
 import { getLogger } from 'log4js';
 import { WebSocketServer, WebSocket } from 'ws';
+import { v4 as createUuid } from 'uuid';
+import { getUserForSession, GraphQLUser } from '../../graphql/authentication';
 
-// can also be any kind of authkey
 type UserId = string;
 
 type ConnectedClients = {
-    [k: UserId]: WebSocket;
+    [k: UserId]: WebSocket[];
 };
 
 type Message = {
@@ -41,52 +42,79 @@ class WebSocketService {
         return this.connectedClients;
     }
 
-    private addClient(id: string, connection: WebSocket) {
-        this.connectedClients[id] = connection;
+    private addClient(userId: string, connection: WebSocket) {
+        (this.connectedClients[userId] = this.connectedClients[userId] || []).push(connection);
     }
 
     private removeClient(id: string) {
+        Object.keys(this.connectedClients).forEach((key) => {
+            const idx = this.connectedClients[key].findIndex((ws: WebSocket & { clientId: string }) => ws.clientId === id);
+            if (idx !== -1) {
+                this.connectedClients[key].splice(idx, 1);
+            }
+            if (this.connectedClients[key].length === 0) {
+                delete this.connectedClients[key];
+            }
+        });
         delete this.connectedClients[id];
     }
 
-    private getClientByConnectionId(id: string): WebSocket {
-        return this.connectedClients[id];
+    private getClientsByUserId(userId: string): WebSocket[] {
+        return this.connectedClients[userId];
     }
 
     /**
      * Sends a message to an websocket client
-     * @param id - client id
+     * @param userId - user id
      * @param message - websocket message with notification id
      */
-    sendMessageToClient(id: string, message: Message): void {
-        const client = this.getClientByConnectionId(id);
-        if (!client) {
+    sendMessageToClients(userId: string, message: Message): void {
+        const clients = this.getClientsByUserId(userId);
+        if (!clients) {
             throw new Error('Client not connected.');
         }
-        client.send(message, (err) => log.error(`Error while sending websocket message: ${err.message}`));
+        clients.forEach((client) => {
+            client.send(message, (err) => log.error(`Error while sending websocket message: ${err.message}`));
+        });
     }
 
     /**
      * This could be used in the channel for canSend
-     * @param id - userId and identifier of the connection
+     * @param userId - userId and identifier of the connection
      * @returns {boolean} online status indicating whether the user has an active websocket connection to get notified
      */
-    isClientOnline(id: string): boolean {
-        return !!this.connectedClients[id];
+    isUserOnline(userId: string): boolean {
+        return !!this.connectedClients[userId];
     }
 
     /**
      * initializes the events and handlers for the websocket server
      */
     configure(): void {
-        this.websocketServer.on('connection', (ws: WebSocket & { id: string }, request) => {
+        this.websocketServer.on('connection', async (ws: WebSocket & { clientId: string }, request) => {
             try {
-                // We should do some kind of auth check here, so that the token in the header matches the param
-                // otherwise some user could get the notifications of otherones just by sending the request with another userIds
-                const id = getUserIdFromConnectionRequest(request.url);
-                ws['id'] = id;
-                this.addClient(id, ws);
-                log.info(`Connected websocket client with id: ${id}`);
+                let sessionUser: GraphQLUser;
+
+                if (request.headers['authorization'] && request.headers['authorization'].startsWith('Bearer ')) {
+                    const sessionToken = request.headers['authorization'].slice('Bearer '.length);
+                    log.debug(`Session token: ${sessionToken}`);
+
+                    if (sessionToken.length < 20) {
+                        throw new Error('Session Tokens must have at least 20 characters');
+                    }
+
+                    sessionUser = await getUserForSession(sessionToken);
+                    log.debug(`Session user: ${JSON.stringify(sessionUser)}`);
+                }
+
+                const userId = getUserIdFromConnectionRequest(request.url);
+                if (userId !== sessionUser.userID) {
+                    throw new Error('Session user does not match user id from connection request parameter');
+                }
+                // create client
+                ws['clientId'] = createUuid();
+                this.addClient(userId, ws);
+                log.info(`Connected websocket client with userId: ${userId}`);
                 log.debug(`Websocket client ids: ${Object.keys(this.getCients())}`);
             } catch (err) {
                 if (!!err?.message) {
@@ -96,19 +124,20 @@ class WebSocketService {
                 ws.terminate();
             }
             ws.on('close', () => {
-                this.removeClient(ws.id);
-                log.info(`Closing connection with id: ${ws.id}`);
-                log.debug(`Connected client ids: ${Object.keys(this.getCients())}`);
+                this.removeClient(ws.clientId);
+                log.info(`Closing connection with id: ${ws.clientId}`);
+                log.debug(`Connected user ids: ${Object.keys(this.getCients())}`);
             });
         });
     }
 }
 
 function getUserIdFromConnectionRequest(requestUrl: string | undefined) {
-    if (!requestUrl || requestUrl?.split('/').length !== 2) {
+    const idQueryParam = '?id=';
+    if (!requestUrl || requestUrl?.split(idQueryParam).length !== 2) {
         throw new Error(`Inavlid websocket connection request url: ${requestUrl}`);
     }
-    const urlParams = requestUrl.split('/');
+    const urlParams = requestUrl.split(idQueryParam);
     return urlParams[1];
 }
 
