44476bb142ce42f3238a985f2b90523055fa0586
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-07 16:30:57 +0100

    fix object type

--- graphql/types/notificationMessage.ts
+++ graphql/types/notificationMessage.ts
@@ -1,12 +1,18 @@
 import { MessageType } from '../../notifications/messageTypes';
 import { ObjectType, Field, Int } from 'type-graphql';
 
-@ObjectType()
+@ObjectType('NotificationMessage')
 export abstract class NotificationMessage {
+    @Field((_type) => String)
     headline: string;
+    @Field((_type) => String)
     body: string;
+    @Field((_type) => String)
     messageType: MessageType;
+    @Field((_type) => String, { nullable: true })
     navigateTo?: string;
+    @Field((_type) => Boolean, { nullable: true })
     isUrlExternal?: boolean;
+    @Field((_type) => String, { nullable: true })
     error?: string;
 }
