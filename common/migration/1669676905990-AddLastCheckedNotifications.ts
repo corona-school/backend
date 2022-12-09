06498724046f4d1b00ce16d7fad0af8f57d17ac5
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-09 10:19:22 +0100

    update migration

--- common/migration/1669676905990-AddLastCheckedNotifications.ts
+++ common/migration/1669676905990-AddLastCheckedNotifications.ts
@@ -1,13 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddLastCheckedNotifications1669676905990 implements MigrationInterface {
-    name = 'AddLastCheckedNotifications1669676905990'
+    name = 'AddLastCheckedNotifications1669676905990';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "pupil" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"2022-11-28T23:08:26.067Z"'`);
-        await queryRunner.query(`ALTER TABLE "screener" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"2022-11-28T23:08:26.067Z"'`);
-        await queryRunner.query(`ALTER TABLE "student" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"2022-11-28T23:08:26.067Z"'`);
-        await queryRunner.query(`ALTER TABLE "mentor" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"2022-11-28T23:08:26.067Z"'`);
+        await queryRunner.query(`ALTER TABLE "pupil" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"1970-01-01T00:00:00.000Z"'`);
+        await queryRunner.query(`ALTER TABLE "screener" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"1970-01-01T00:00:00.000Z"'`);
+        await queryRunner.query(`ALTER TABLE "student" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"1970-01-01T00:00:00.000Z"'`);
+        await queryRunner.query(`ALTER TABLE "mentor" ADD "lastTimeCheckedNotifications" TIMESTAMP DEFAULT '"1970-01-01T00:00:00.000Z"'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -16,5 +16,4 @@ export class AddLastCheckedNotifications1669676905990 implements MigrationInterf
         await queryRunner.query(`ALTER TABLE "screener" DROP COLUMN "lastTimeCheckedNotifications"`);
         await queryRunner.query(`ALTER TABLE "pupil" DROP COLUMN "lastTimeCheckedNotifications"`);
     }
-
 }
