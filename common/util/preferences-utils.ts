5334de2f928edef54e5e838b12fa6bc84ee557a0
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-09 11:01:58 +0100

    remove unused

--- common/util/preferences-utils.ts
+++
@@ -1,9 +0,0 @@
-type Channels = {
-    email: boolean;
-};
-
-type MessageTypes = 'chat' | 'match' | 'course' | 'appointment' | 'survey' | 'news';
-
-export type NotificationPreferences = {
-    [MessageType in MessageTypes]: Channels;
-};
