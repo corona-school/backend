55d84beeee73d2cb5f65baf095af85eff8b5ce5f
Jonas Wilms <23138527+Jonasdoubleyou@users.noreply.github.com>
2022-12-10 10:40:54 +0100

    Add ValidateEmail Decorator

--- graphql/me/mutation.ts
+++ graphql/me/mutation.ts
@@ -53,6 +53,7 @@ import { UserType } from '../types/user';
 import { ProjectFieldWithGradeInput, StudentUpdateInput, updateStudent } from '../student/mutations';
 import { PupilUpdateInput, updatePupil } from '../pupil/mutations';
 import { deactivateStudent } from '../../common/student/activation';
+import { ValidateEmail } from '../validators';
 
 @InputType()
 class RegisterStudentInput implements RegisterStudentData {
@@ -95,7 +96,7 @@ class RegisterPupilInput implements RegisterPupilData {
     lastname: string;
 
     @Field((type) => String)
-    @MaxLength(100)
+    @ValidateEmail()
     email: string;
 
     @Field((type) => Boolean)
@@ -265,7 +266,7 @@ export class MutateMeResolver {
     @RateLimit('RegisterPupil', 10 /* requests per */, 5 * 60 * 60 * 1000 /* 5 hours */)
     async meRegisterPupil(@Ctx() context: GraphQLContext, @Arg('data') data: RegisterPupilInput, @Arg('noEmail', { nullable: true }) noEmail: boolean = false) {
         const byAdmin = context.user!.roles.includes(Role.ADMIN);
-
+        console.log('Email in Mutation', data.email);
         if (data.registrationSource === RegistrationSource.plus && !byAdmin) {
             throw new UserInputError('Lern-Fair Plus pupils may only be registered by admins');
         }
