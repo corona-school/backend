76aa5572e34c65cb9230973db4d61fa99150a8a9
Leo310 <leodev310@gmail.com>
2022-12-11 20:28:30 +0100

    Fix: date format

--- jobs/periodic/anonymise-attendance-log/index.ts
+++ jobs/periodic/anonymise-attendance-log/index.ts
@@ -5,6 +5,6 @@ const DELETE_AFTER = 7; // in days
 const logger = getLogger();
 
 export default async function execute() {
-    await prisma.course_attendance_log.updateMany({ where: { createdAt: { lt: new Date(Date.now() - DELETE_AFTER) } }, data: { ip: "" } });
+    await prisma.course_attendance_log.updateMany({ where: { createdAt: { lt: new Date(Date.now() - DELETE_AFTER * 24 * 60 * 60 * 1000) } }, data: { ip: "" } });
     logger.info(`Removed IP Address and PupilID from attendance logs, which are older then 7 days.`);
 }
