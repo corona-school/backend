8d2757ca363d8905aa9b37c873691a2d79302a05
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-15 11:43:56 +0100

    make preference fields optional

--- graphql/types/preferences.ts
+++ graphql/types/preferences.ts
@@ -9,32 +9,32 @@ class Channels {
 @InputType('PreferencesInput')
 @ObjectType()
 export class NotificationPreferences {
-    @Field()
-    chat: Channels;
-    @Field()
-    match: Channels;
-    @Field()
-    course: Channels;
-    @Field()
-    appointment: Channels;
-    @Field()
-    survey: Channels;
-    @Field()
-    news: Channels;
-    @Field()
-    newsletter: Channels;
-    @Field()
-    training: Channels;
-    @Field()
-    events: Channels;
-    @Field()
-    newsoffer: Channels;
-    @Field()
-    request: Channels;
-    @Field()
-    learnoffer: Channels;
-    @Field()
-    alternativeoffer: Channels;
-    @Field()
-    feedback: Channels;
+    @Field({ nullable: true })
+    chat?: Channels;
+    @Field({ nullable: true })
+    match?: Channels;
+    @Field({ nullable: true })
+    course?: Channels;
+    @Field({ nullable: true })
+    appointment?: Channels;
+    @Field({ nullable: true })
+    survey?: Channels;
+    @Field({ nullable: true })
+    news?: Channels;
+    @Field({ nullable: true })
+    newsletter?: Channels;
+    @Field({ nullable: true })
+    training?: Channels;
+    @Field({ nullable: true })
+    events?: Channels;
+    @Field({ nullable: true })
+    newsoffer?: Channels;
+    @Field({ nullable: true })
+    request?: Channels;
+    @Field({ nullable: true })
+    learnoffer?: Channels;
+    @Field({ nullable: true })
+    alternativeoffer?: Channels;
+    @Field({ nullable: true })
+    feedback?: Channels;
 }
