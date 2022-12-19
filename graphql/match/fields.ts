5e1ba5a15bc17ce8b3cc46627b7847e185a8c08a
Jonas Wilms <jonaswilms2000@gmail.com>
2022-12-19 22:59:40 +0100

    Release email of match partners to each other (#445)

--- graphql/match/fields.ts
+++ graphql/match/fields.ts
@@ -1,5 +1,5 @@
 import { Role } from '../authorizations';
-import { Authorized, Field, FieldResolver, Int, ObjectType, Resolver, Root } from 'type-graphql';
+import { Authorized, Ctx, Field, FieldResolver, Int, ObjectType, Resolver, Root } from 'type-graphql';
 import { prisma } from '../../common/prisma';
 import { Subcourse, Pupil, Match, Student } from '../generated';
 import { LimitEstimated } from '../complexity';
@@ -35,5 +35,24 @@ export class ExtendedFieldsMatchResolver {
         const pupil = await getPupil(match.pupilId);
 
         return getOverlappingSubjects(pupil, student);
+    }
+
+    // Unfortunately it is difficult to define conditional access on a user's email without raising field level access restrictions,
+    //  which we don't really want as the email is very sensitive data.
+    // Thus in hope that a proper Chat will soon replace email communication, manually release the fields through this indirection:
+    @FieldResolver((returns) => String)
+    @Authorized(Role.OWNER)
+    async pupilEmail(@Root() match: Match) {
+        return await (
+            await prisma.pupil.findUniqueOrThrow({ where: { id: match.pupilId }, select: { email: true } })
+        ).email;
+    }
+
+    @FieldResolver((returns) => String)
+    @Authorized(Role.OWNER)
+    async studentEmail(@Root() match: Match) {
+        return await (
+            await prisma.student.findUniqueOrThrow({ where: { id: match.studentId }, select: { email: true } })
+        ).email;
     }
 }
