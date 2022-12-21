63c2f4ebf39d82b24647268621427a57273ba03e
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-21 16:50:18 +0100

    revert inapp channel name

--- common/notification/types.ts
+++ common/notification/types.ts
@@ -66,7 +66,7 @@ export interface Context extends NotificationContext {
 
 // Abstract away from the core: Channels are our Ports to external notification systems (Mailjet, SMS, ...)
 export interface Channel {
-    type: 'email' | 'webApp';
+    type: 'email' | 'inapp';
     send(notification: Notification, to: User, context: Context, concreteID: number, attachments?: AttachmentGroup): Promise<any>;
     canSend(notification: Notification, user: User): boolean;
 }
