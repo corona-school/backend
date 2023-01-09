64aabb52a1dbdd14c1d76db34c802a13d6056b4b
realmayus <realmayus@gmail.com>
2023-01-09 11:09:34 +0100

    Revert change

--- common/transactionlog/types/LogUserEvent.ts
+++ common/transactionlog/types/LogUserEvent.ts
@@ -6,7 +6,7 @@ import { Pupil } from '../../entity/Pupil';
 import { Mentor } from '../../entity/Mentor';
 
 export default abstract class LogUserEvent extends LogEvent {
-    protected constructor(logType: LogType, user: { wix_id }, data: EventData) {
+    protected constructor(logType: LogType, user: { wix_id: string }, data: EventData) {
         super(logType, user.wix_id, data);
     }
 }
