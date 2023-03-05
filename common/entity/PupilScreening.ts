a9ddea0dc13b385caa7a8ea3cc78d375fb697e2d
realmayus <realmayus@gmail.com>
2023-03-05 18:57:55 +0100

    Added pupil screening DB entity

---
+++ common/entity/PupilScreening.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Pupil } from './Pupil';
+
+export enum PupilScreeningStatus {
+    PENDING, // default
+    SUCCESS,
+    REJECTION,
+    DISPUTE,
+}
+
+@Entity()
+export class PupilScreening {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+
+    @ManyToOne(() => Pupil, (pupil) => pupil.screenings)
+    pupil: Pupil;
+
+    @Column({
+        type: 'enum',
+        enum: PupilScreeningStatus,
+        default: PupilScreeningStatus.PENDING,
+    })
+    status: PupilScreeningStatus;
+
+    @Column()
+    invalidated: boolean;
+
+    @Column({ nullable: true })
+    comment?: string;
+}
