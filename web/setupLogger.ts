5bba66a25f1c714ee1a5e546aa277eea1b7a433a
Daniel Henkel <daniel@henkel.tech>
2023-03-11 20:42:51 +0100

    remove try/catch from logger setup

--- web/setupLogger.ts
+++ web/setupLogger.ts
@@ -1,19 +1,14 @@
 import { isCommandArg } from '../common/util/basic';
 import { configure } from 'log4js';
 
-try {
-    configure({
-        appenders: {
-            out: { type: 'stdout', layout: { type: 'coloured' } },
+configure({
+    appenders: {
+        out: { type: 'stdout', layout: { type: 'coloured' } },
+    },
+    categories: {
+        default: {
+            appenders: ['out'],
+            level: isCommandArg('--debug') ? 'debug' : 'info',
         },
-        categories: {
-            default: {
-                appenders: ['out'],
-                level: isCommandArg('--debug') ? 'debug' : 'info',
-            },
-        },
-    });
-} catch (e) {
-    // TODO: should this be a breaking condition? Loosing all the logs, might be a big problem.
-    console.warn("Couldn't setup logger", e);
-}
+    },
+});
