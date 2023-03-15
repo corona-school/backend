731d3ed029d3021801d9376ead920dcae9cb9cfd
Daniel Henkel <daniel@henkel.tech>
2023-03-15 07:51:33 +0100

    implement basic webflow request structure

--- jobs/periodic/sync-to-webflow/index.ts
+++ jobs/periodic/sync-to-webflow/index.ts
@@ -1,8 +1,23 @@
 import { EntityManager } from 'typeorm';
 import { getLogger } from '../../utils/logging';
+import { getCollectionItems, WebflowMetadata } from './webflow-adapter';
 
 const logger = getLogger();
 
+interface Subject extends WebflowMetadata {
+    name: string;
+}
+
+function subjectsFactory(data: any): Subject {
+    // TODO: check if data exist
+    return {
+        _archived: data._archived,
+        name: data.name,
+    };
+}
+
 export default async function execute(_manager: EntityManager): Promise<void> {
-    logger.info('test job');
+    const items = await getCollectionItems<Subject>(process.env.WEBFLOW_COLLECTION_ID, subjectsFactory);
+    // const isso = await manager.find(Course, {});
+    logger.info(items);
 }
---
+++ jobs/periodic/sync-to-webflow/webflow-adapter.ts
@@ -0,0 +1,30 @@
+import { join } from 'path';
+
+export interface WebflowMetadata {
+    _archived: boolean;
+}
+
+export async function getCollectionItems<T>(collectionID: string, factory: (data: any) => T): Promise<T[]> {
+    const data = await request({ path: `collections/${collectionID}/items`, method: 'GET' });
+    return data.map(factory);
+}
+
+interface Request {
+    path: string;
+    method: 'GET';
+}
+
+async function request(req: Request): Promise<any[]> {
+    const url = join(process.env.WEBFLOW_API_BASE_URL, req.path);
+    const token = process.env.WEBFLOW_API_KEY;
+
+    const res = await fetch(url, {
+        method: req.method,
+        headers: {
+            accept: 'application/json',
+            authorization: `Bearer ${token}`,
+        },
+    });
+    const json = await res.json();
+    return json.items || [];
+}
