a30ceac24c164a103c28288f01886949ee662127
Daniel Henkel <daniel@henkel.tech>
2023-03-23 07:37:27 +0100

    remove unnecessary fields from webflow cms database

--- jobs/periodic/sync-to-webflow/sync-courses.ts
+++ jobs/periodic/sync-to-webflow/sync-courses.ts
@@ -4,43 +4,34 @@ import { Logger } from 'log4js';
 import { prisma } from '../../../common/prisma';
 import { course_coursestate_enum as courseState, Subcourse, Subcourse_instructors_student as SubcourseInstructorsStudent } from '../../../graphql/generated';
 import moment, { Moment } from 'moment';
+import { accessURLForKey } from '../../../common/file-bucket';
 
 // This is needed so that the weekday will be translated properly.
 moment.locale('de');
 
 const collectionId = process.env.WEBFLOW_COLLECTION_ID;
+const appBaseUrl = 'https://app.lern-fair.de/single-course';
 
 interface CourseDTO extends WebflowMetadata {
     description: string;
     instructor: string;
-    aboutinstructor: string;
-    subheading: string;
 
     startingdate: string;
     weekday: string;
     courseduration: string; // like "45 min"
-    lessoncount: number;
+    lecturecount: number;
     time: string; // like 16:00 Uhr
     appointments: string;
 
     category: string;
     link: string;
     maxparticipants: number;
-    actualparticipants: number;
+    participantscount: number;
     subject: string;
 
     mingrade: number;
     maxgrade: number;
-    priorknowledge: boolean;
-    agegroup: string;
-    level: string;
 
-    imagecopyrightinfo: string;
-    thumbnail: {
-        fileId: string;
-        url: string;
-        alt: string;
-    };
     image: {
         fileId: string;
         url: string;
@@ -97,37 +88,26 @@ function courseToDTO(subcourse: Subcourse): CourseDTO {
 
         description: subcourse.course.description,
         instructor: generateInstructor(subcourse.subcourse_instructors_student),
-        aboutinstructor: '', // TODO
-        subheading: subcourse.course.outline,
 
         startingdate: startDate.toISOString(),
         weekday: startDate.format('dddd'),
         courseduration: `${getCouseDuration(subcourse)} min`, // TODO: maybe this can be done in a nicer way. Maybe with the help of moment?
-        lessoncount: subcourse.lecture.length,
+        lecturecount: subcourse.lecture.length,
         time: startDate.format('HH:mm'),
         appointments: listAppointments(subcourse),
 
         category: subcourse.course.category,
-        link: '', // TODO Wird noch gebraucht?
+        link: `${appBaseUrl}/${subcourse.id}`,
         maxparticipants: subcourse.maxParticipants,
-        actualparticipants: subcourse.subcourse_participants_pupil.length,
+        participantscount: subcourse.subcourse_participants_pupil.length,
         subject: subcourse.course.subject,
 
         mingrade: subcourse.minGrade,
         maxgrade: subcourse.maxGrade,
-        priorknowledge: false, // TODO No data in DB
-        agegroup: '', // TODO No data in DB
-        level: '', // TODO
 
-        imagecopyrightinfo: subcourse.course.imageKey,
-        thumbnail: {
-            fileId: 'placeholder', // not needed
-            url: subcourse.course.imageKey,
-            alt: '',
-        },
         image: {
             fileId: 'placeholder', // not needed
-            url: subcourse.course.imageKey,
+            url: accessURLForKey(subcourse.course.imageKey),
             alt: '',
         },
     };
