aa67e1528decc1454b0a783b4026fe07927df0f8
Daniel Henkel <daniel@henkel.tech>
2023-03-25 18:55:34 +0100

    move get courses to shared place

---
+++ jobs/periodic/sync-to-webflow/queries.ts
@@ -0,0 +1,23 @@
+import { prisma } from '../../../common/prisma';
+import { IS_PUBLIC_SUBCOURSE } from '../../../graphql/subcourse/fields';
+import { Prisma } from '@prisma/client';
+
+export type WebflowSubcourse = Prisma.subcourseGetPayload<{
+    include: {
+        course: true;
+        subcourse_instructors_student: { include: { student: true } };
+        subcourse_participants_pupil: true;
+        lecture: true;
+    };
+}>;
+
+export const getWebflowSubcourses = (): Promise<WebflowSubcourse[]> =>
+    prisma.subcourse.findMany({
+        where: IS_PUBLIC_SUBCOURSE(),
+        include: {
+            course: true,
+            subcourse_instructors_student: { include: { student: true } },
+            subcourse_participants_pupil: true,
+            lecture: true,
+        },
+    });
