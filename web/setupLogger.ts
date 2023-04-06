b7db96dd8e56449c41c39774cccf928f2c4f6230
Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
2023-04-06 21:41:53 +0200

    write json formatted logs in non dev environments (#565)
    * write json formatted logs in non dev environments
    * move first data element to message field in json logs
    * create LOG_FORMAT environment variable

--- web/setupLogger.ts
+++ web/setupLogger.ts
@@ -1,13 +1,26 @@
 import { isCommandArg } from '../common/util/basic';
-import { configure } from 'log4js';
+import { configure, addLayout } from 'log4js';
+
+addLayout('json', function () {
+    return function (logEvent) {
+        const message = logEvent.data.shift();
+        return JSON.stringify({ ...logEvent, message });
+    };
+});
+
+let appenders = ['out'];
+if (process.env.LOG_FORMAT === 'json') {
+    appenders = ['outJson'];
+}
 
 configure({
     appenders: {
         out: { type: 'stdout', layout: { type: 'coloured' } },
+        outJson: { type: 'stdout', layout: { type: 'json' } },
     },
     categories: {
         default: {
-            appenders: ['out'],
+            appenders,
             level: isCommandArg('--debug') ? 'debug' : 'info',
         },
     },
