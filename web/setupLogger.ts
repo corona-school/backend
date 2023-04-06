13e250e96a8d17fc5db9da1f2d57b43d7064957a
Daniel Henkel <daniel@henkel.tech>
2023-04-06 20:46:03 +0200

    write json formatted logs in non dev environments

--- web/setupLogger.ts
+++ web/setupLogger.ts
@@ -1,13 +1,25 @@
 import { isCommandArg } from '../common/util/basic';
-import { configure } from 'log4js';
+import { configure, addLayout } from 'log4js';
+
+addLayout('json', function () {
+    return function (logEvent) {
+        return JSON.stringify(logEvent);
+    };
+});
+
+let appenders = ['outJson'];
+if (process.env.ENV === 'dev') {
+    appenders = ['out'];
+}
 
 configure({
     appenders: {
         out: { type: 'stdout', layout: { type: 'coloured' } },
+        outJson: { type: 'stdout', layout: { type: 'json' } },
     },
     categories: {
         default: {
-            appenders: ['out'],
+            appenders,
             level: isCommandArg('--debug') ? 'debug' : 'info',
         },
     },
