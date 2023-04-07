8c690b0ce28ddfd9e38d88751a7720fa525d278e
Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
2023-04-07 17:32:27 +0200

    Streamline logging (#568)
    * move job logger config from json file to ts.
    
    * add UID to every graphql request
    
    * use same logging configuration in job & web

--- jobs/utils/logging/index.ts
+++ jobs/utils/logging/index.ts
@@ -1,14 +1,32 @@
-import { configure, getLogger as getLog4jsLogger } from 'log4js';
+import { configure, getLogger as getLog4jsLogger, addLayout } from 'log4js';
+
+addLayout('json', function () {
+    return function (logEvent) {
+        const message = logEvent.data.shift();
+        return JSON.stringify({ ...logEvent, message });
+    };
+});
+
+let appenders = ['out'];
+if (process.env.LOG_FORMAT === 'json') {
+    appenders = ['outJson'];
+}
 
 export function setup() {
-    try {
-        configure('jobs/logconfig.json');
-    } catch (e) {
-        console.warn("Couldn't setup logger", e);
-    }
+    configure({
+        appenders: {
+            out: { type: 'stdout', layout: { type: 'coloured' } },
+            outJson: { type: 'stdout', layout: { type: 'json' } },
+        },
+        categories: {
+            default: {
+                appenders,
+                level: 'all',
+            },
+        },
+    });
 }
 
 export function getLogger(category?: string) {
     return getLog4jsLogger(category);
 }
-
