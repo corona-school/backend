f1059476c3fbe20e1eab2e51682df69162bc3be9
Daniel Henkel <daniel@henkel.tech>
2023-04-07 15:35:07 +0200

    move job logger config from json file to ts.

--- jobs/utils/logging/index.ts
+++ jobs/utils/logging/index.ts
@@ -1,8 +1,54 @@
-import { configure, getLogger as getLog4jsLogger } from 'log4js';
+import { configure, getLogger as getLog4jsLogger, addLayout, Layout } from 'log4js';
+
+addLayout('json', function () {
+    return function (logEvent) {
+        const message = logEvent.data.shift();
+        return JSON.stringify({ ...logEvent, message });
+    };
+});
+
+let stdoutLayout: Layout = { type: 'coloured' };
+if (process.env.LOG_FORMAT === 'json') {
+    stdoutLayout = { type: 'json' };
+}
 
 export function setup() {
     try {
-        configure('jobs/logconfig.json');
+        configure({
+            appenders: {
+                file: {
+                    type: 'dateFile',
+                    filename: 'logs/jobs.log',
+                    keepFileExt: true,
+                },
+                'file-filtered': {
+                    type: 'logLevelFilter',
+                    appender: 'file',
+                    level: 'info',
+                },
+                stdout: {
+                    type: 'stdout',
+                    layout: stdoutLayout,
+                },
+                'stdout-filtered': {
+                    type: 'logLevelFilter',
+                    appender: 'stdout',
+                    level: 'info',
+                },
+                'stderr-filtered': {
+                    type: 'logLevelFilter',
+                    appender: 'stdout',
+                    level: 'all',
+                    maxLevel: 'debug',
+                },
+            },
+            categories: {
+                default: {
+                    appenders: ['stderr-filtered', 'stdout-filtered', 'file-filtered'],
+                    level: 'all',
+                },
+            },
+        });
     } catch (e) {
         console.warn("Couldn't setup logger", e);
     }
@@ -11,4 +57,3 @@ export function setup() {
 export function getLogger(category?: string) {
     return getLog4jsLogger(category);
 }
-
