2ae7c8c506b0d963b1193772d490475c0b3bba68
Jonas <JonasWilms2000@gmail.com>
2023-04-10 14:55:10 +0200

    Add integration test

--- integration-tests/admin.ts
+++ integration-tests/admin.ts
@@ -51,3 +51,91 @@ test("Admin Pupil to Plus", async () => {
       }
     `);
 });
+
+test("Admin Search Users", async () => {
+    const { pupil } = await pupilOne;
+    const { student } = await studentOne;
+
+    const { usersSearch: searchPupilInStudents } = await adminClient.request(`
+        query SearchPupilInStudents {
+            usersSearch(query: "${pupil.firstname} ${pupil.lastname}" only: "student") {
+                email
+            }
+        }
+    `);
+    assert.strictEqual(searchPupilInStudents.length, 0);
+
+
+    const { usersSearch: searchPupilByFirstname } = await adminClient.request(`
+        query SearchPupilByFirstname {
+            usersSearch(query: "${pupil.firstname}" only: "pupil") {
+                email
+            }
+        }
+    `);
+    assert.strictEqual(searchPupilByFirstname.length, 1);
+    assert.strictEqual(searchPupilByFirstname[0].email, pupil.email);
+
+    const { usersSearch: searchPupilByLastname } = await adminClient.request(`
+        query SearchPupilByLastname {
+            usersSearch(query: "${pupil.lastname}" only: "pupil") {
+                email
+            }
+        }
+    `);
+    assert.strictEqual(searchPupilByLastname.length, 1);
+    assert.strictEqual(searchPupilByLastname[0].email, pupil.email);
+
+    const { usersSearch: searchPupilByFullName } = await adminClient.request(`
+        query SearchPupilByFullName {
+            usersSearch(query: "${pupil.firstname} ${pupil.lastname}" only: "pupil") {
+                email
+            }
+        }
+    `);
+    assert.strictEqual(searchPupilByFullName.length, 1);
+    assert.strictEqual(searchPupilByFullName[0].email, pupil.email);
+
+    const { usersSearch: searchPupilByEmail } = await adminClient.request(`
+        query SearchPupilByEmail {
+            usersSearch(query: "${pupil.email}" only: "pupil") {
+                email
+            }
+        }
+    `);
+    assert.strictEqual(searchPupilByEmail.length, 1);
+    assert.strictEqual(searchPupilByEmail[0].email, pupil.email);
+
+    const { usersSearch: searchUsersByPartialEmail } = await adminClient.request(`
+        query SearchUsersByPartialEmail {
+            usersSearch(query: " @lern-fair.de") {
+                email
+            }
+        }
+    `);
+
+    assert(searchUsersByPartialEmail.some(it => it.email === pupil.email));
+    assert(searchUsersByPartialEmail.some(it => it.email === student.email));
+
+    const { usersSearch: searchStudentsByPartialEmail } = await adminClient.request(`
+        query SearchStudentsByPartialEmail {
+            usersSearch(query: "@lern-fair.de " only: "student") {
+                email
+            }
+        }
+    `);
+
+    assert(!searchStudentsByPartialEmail.some(it => it.email === pupil.email));
+    assert(searchStudentsByPartialEmail.some(it => it.email === student.email));
+
+    const { usersSearch: searchUsersByPartialName } = await adminClient.request(`
+        query SearchUsersByPartialName {
+            usersSearch(query: "firstname lastname") {
+                email
+            }
+        }
+    `);
+
+    assert(searchUsersByPartialName.some(it => it.email === pupil.email));
+    assert(searchUsersByPartialName.some(it => it.email === student.email));
+});
