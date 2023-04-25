78ef08a56cdcdf827ded1db5767b8cc6c6b6bbfa
Andreas (AK) <andreas@typedigital.de>
2023-04-25 16:59:50 +0200

    feat: add authorization, role and accessCheck

---
+++ common/appointments/participants.ts
@@ -0,0 +1,14 @@
+import { Lecture } from '../entity/Lecture';
+import { prisma } from '../prisma';
+import { User } from '../user';
+
+export async function isAppointmentParticipant(lecture: Lecture, user: User): Promise<boolean> {
+    return !!(await prisma.lecture.findFirst({
+        where: {
+            id: lecture.id,
+            participants: {
+                has: user.userID,
+            },
+        },
+    }));
+}
