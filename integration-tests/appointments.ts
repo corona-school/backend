e6a97838dd16d4be87e2ebae8e6234e99d90100f
Lomy <salome.wick@typedigital.de>
2023-04-27 16:24:19 +0200

    create more appointments and query my appointments

--- integration-tests/appointments.ts
+++ integration-tests/appointments.ts
@@ -2,16 +2,18 @@ import { test } from './base';
 import { screenedInstructorOne } from './screening';
 import { subcourseOne } from './course';
 import assert from 'assert';
+import { pupilOne, studentOne } from './user';
+import { pupilOneWithPassword } from './auth';
 
 const firstAppointment = test('Create an appointment for a subcourse', async () => {
-    const {subcourseId} = await subcourseOne;
-    const {client} = await screenedInstructorOne;
+    const { subcourseId } = await subcourseOne;
+    const { client } = await screenedInstructorOne;
     const appointmentTitle = 'Group Appointment 1';
     const nextHour = new Date();
     nextHour.setHours(new Date().getHours() + 1);
     const res = await client.request(`
-        mutation creategroupAppointments {
-            appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
+    mutation creategroupAppointments {
+        appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
             {
                 title: "${appointmentTitle}"
                 start: "${nextHour.toISOString()}"
@@ -20,6 +22,75 @@ const firstAppointment = test('Create an appointment for a subcourse', async () 
                 appointmentType: group
             }])
         }
-    `);
+        `);
     assert.ok(res.appointmentsGroupCreate);
+});
+
+const moreAppointments = test('Create more appointments for a subcourse', async () => {
+    const { subcourseId } = await subcourseOne;
+    const { client } = await screenedInstructorOne;
+    const nextDate = new Date();
+    nextDate.setDate(new Date().getDate() + 2);
+    const nextMonth = new Date();
+    nextMonth.setMonth(new Date().getMonth() + 1);
+
+    const res = await client.request(`
+    mutation creategroupAppointments {
+        appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
+            {
+                title: "Group Appointment 2"
+                start: "${nextDate.toISOString()}"
+                duration: 30
+                subcourseId: ${subcourseId}
+                appointmentType: group
+            },
+            {
+                title: "Group Appointment 3"
+                start: "${nextMonth.toISOString()}"
+                duration: 60
+                subcourseId: ${subcourseId}
+                appointmentType: group
+            }
+        ])
+        }
+        `);
+    assert.ok(res.appointmentsGroupCreate);
+});
+
+const myAppointments = test('Get my appointments', async () => {
+    const { client } = await screenedInstructorOne;
+    await firstAppointment;
+    await moreAppointments;
+
+    const {
+        me: { appointments },
+    } = await client.request(`
+        query myAppointments {
+            me {
+                appointments(take: 3) {
+                    id
+                    title
+                    description
+                    start
+                    duration
+                    appointmentType
+                    organizers(skip: 0, take: 5) {
+                        id
+                        firstname
+                        lastname
+                    }
+                    participants(skip: 0, take: 30) {
+                        id
+                        firstname
+                        lastname
+                        isPupil
+                        isStudent
+                    }
+                }
+            }
+        }
+    `);
+
+    console.log('My appointments:', appointments);
+    assert.ok(appointments);
 });
