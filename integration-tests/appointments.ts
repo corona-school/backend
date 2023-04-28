065ac49a2b7b1e7bf48255a877bb559d6466b4fb
Lomy <salome.wick@typedigital.de>
2023-04-28 11:00:17 +0200

    query appointment participants
    Co-authored-by: Andreas (AK) <andreas@typedigital.de>

--- integration-tests/appointments.ts
+++ integration-tests/appointments.ts
@@ -2,13 +2,16 @@ import { test } from './base';
 import { screenedInstructorOne } from './screening';
 import { subcourseOne } from './course';
 import assert from 'assert';
-import { pupilOne, studentOne } from './user';
-import { pupilOneWithPassword } from './auth';
+import { pupilOne, pupilUpdated } from './user';
+
+// TODO define appointment titles here
+const appointmentTitle = 'Group Appointment 1';
+const appointmentTitle2 = 'Group Appointment 2';
+const appointmentTitle3 = 'Group Appointment 3';
 
 const firstAppointment = test('Create an appointment for a subcourse', async () => {
     const { subcourseId } = await subcourseOne;
     const { client } = await screenedInstructorOne;
-    const appointmentTitle = 'Group Appointment 1';
     const nextHour = new Date();
     nextHour.setHours(new Date().getHours() + 1);
     const res = await client.request(`
@@ -35,17 +38,17 @@ const moreAppointments = test('Create more appointments for a subcourse', async 
     nextMonth.setMonth(new Date().getMonth() + 1);
 
     const res = await client.request(`
-    mutation creategroupAppointments {
-        appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
-            {
-                title: "Group Appointment 2"
-                start: "${nextDate.toISOString()}"
-                duration: 30
-                subcourseId: ${subcourseId}
-                appointmentType: group
-            },
-            {
-                title: "Group Appointment 3"
+        mutation creategroupAppointments {
+            appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
+                {
+                    title: "${appointmentTitle2}"
+                    start: "${nextDate.toISOString()}"
+                    duration: 30
+                    subcourseId: ${subcourseId}
+                    appointmentType: group
+                },
+                {
+                title: "${appointmentTitle3}"
                 start: "${nextMonth.toISOString()}"
                 duration: 60
                 subcourseId: ${subcourseId}
@@ -74,6 +77,58 @@ const myAppointments = test('Get my appointments', async () => {
                     start
                     duration
                     appointmentType
+                    displayName
+                    position
+                    total
+                    isCanceled
+                    organizers(skip: 0, take: 1) {
+                        id
+                        firstname
+                        lastname
+                    }
+                    participants(skip: 0, take: 1) {
+                        id
+                        firstname
+                        lastname
+                    }
+                    declinedBy
+                }
+            }
+        }
+    `);
+
+    assert.ok(appointments);
+    assert.ok(appointments.some((a) => (a.title = appointmentTitle)));
+});
+
+const participantAppointments = test('Get participants appointments', async () => {
+    const { client, pupil } = await pupilOne;
+    const { subcourseId } = await subcourseOne;
+
+    await pupilUpdated;
+    await myAppointments;
+
+    await client.request(`
+    mutation joinSubcourse(
+        subcourseJoin(subcourseId: ${parseInt(subcourseId)})
+    )`);
+
+    const {
+        me: { appointments },
+    } = await client.request(`
+        query myAppointments {
+            me {
+                appointments(take: 3) {
+                    id
+                    title
+                    description
+                    start
+                    duration
+                    appointmentType
+                    displayName
+                    position
+                    total
+                    isCanceled
                     organizers(skip: 0, take: 5) {
                         id
                         firstname
@@ -83,14 +138,15 @@ const myAppointments = test('Get my appointments', async () => {
                         id
                         firstname
                         lastname
-                        isPupil
-                        isStudent
                     }
+                    declinedBy
                 }
             }
         }
     `);
-
-    console.log('My appointments:', appointments);
-    assert.ok(appointments);
+    assert.ok(appointments.participants.some((p) => (p.id = pupil.pupil.id)));
 });
+
+// TODO cancel appointment
+
+// TODO declinedBy
