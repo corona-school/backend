5d4b51fcf5418f90fd4e2ff18681c5f5803f44b8
LomyW <115979086+LomyW@users.noreply.github.com>
2023-04-28 10:37:15 +0200

    feat: become organizer or participant on subcourse actions (#610)
    * feat: add methods to create and get appointments
    
    * moved some things and added mutations
    
    * add fieldresolvers for appointments
    
    * add fieldResolvers for appointments
    
    * refactor field auth and appointmentType
    
    * add participant field and ownership
    
    * suffixed participant and organizers column with id
    
    * add and remove group participant
    
    * move util func getUsers to common
    
    * Update graphql/user/fields.ts
    
    Co-authored-by: Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
    
    * add and remove group organizer
    
    * refactorings and optimizations
    function namings, code deduplication
    
    * add orderBy to subcourse and match appointments
    
    * review changes
    
    ---------
    
    Co-authored-by: Andreas (AK) <andreas@typedigital.de>
    Co-authored-by: Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>

--- common/appointment/participants.ts
+++ common/appointment/participants.ts
@@ -15,3 +15,46 @@ export async function isAppointmentParticipant(lecture: Lecture, user: User): Pr
         },
     }));
 }
+
+export async function addGroupAppointmentsParticipant(subcourseId: number, userId: string) {
+    await prisma.lecture.updateMany({
+        where: { subcourseId },
+        data: { participantIds: { push: userId } },
+    });
+}
+
+export async function removeGroupAppointmentsParticipant(subcourseId: number, userId: string) {
+    const appointments = await prisma.lecture.findMany({ where: { subcourseId, participantIds: { hasSome: userId }, start: { gte: new Date() } } });
+
+    await Promise.all(
+        appointments.map(async (a) => {
+            const participants = a.participantIds;
+            const newParticipants = participants.filter((pId) => pId !== userId);
+            await prisma.lecture.update({
+                where: { id: a.id },
+                data: { participantIds: { set: newParticipants } },
+            });
+        })
+    );
+}
+
+export async function addGroupAppointmentsOrganizer(subcourseId: number, organizerId: string) {
+    await prisma.lecture.updateMany({
+        where: { subcourseId },
+        data: { organizerIds: { push: organizerId } },
+    });
+}
+
+export async function removeGroupAppointmentsOrganizer(subcourseId: number, organizerId: string) {
+    const appointments = await prisma.lecture.findMany({ where: { subcourseId } });
+    await Promise.all(
+        appointments.map(async (a) => {
+            const organizers = a.organizerIds;
+            const newOrganizers = organizers.filter((oId) => oId !== organizerId);
+            await prisma.lecture.update({
+                where: { id: a.id },
+                data: { organizerIds: { set: newOrganizers } },
+            });
+        })
+    );
+}
