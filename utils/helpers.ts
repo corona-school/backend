743c6d26a6cc73ec5326359ec8cd360cc3bbac6f
realmayus <realmayus@gmail.com>
2023-05-07 16:39:19 +0200

    Add log message

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -3,7 +3,9 @@ import { Context } from '../common/notification/types';
 import { getLogger } from '../common/logger/logger';
 
 export const renderTemplate = (template: string, context: Partial<Context>, strict: boolean = false) => {
+    const log = getLogger('Template Rendering');
     if (!template) {
+        log.error('Template string undefined');
         return undefined;
     }
     return compile(template, { strict })(context);
