1aa7814a5ae6642be249e4223fad420900ab2277
jenny <jennifer.falkenstein@typedigital.de>
2023-05-09 07:28:57 +0200

    fix: lint error

--- graphql/chat/mutations.ts
+++ graphql/chat/mutations.ts
@@ -1,30 +1,41 @@
-import { Ctx, Mutation, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
 import { Lecture as Appointment } from '../generated';
 import { Role } from '../../common/user/roles';
 import { GraphQLContext } from '../context';
 import { AuthorizedDeferred, hasAccess } from '../authorizations';
 import { getLogger } from '../../common/logger/logger';
+import { prisma } from '../../common/prisma';
 
 const logger = getLogger('MutateChatResolver');
 
 @Resolver(() => Appointment)
 export class MutateChatResolver {
     @Mutation(() => Boolean)
-    async matchChatCreate(@Ctx() context: GraphQLContext) {
+    @AuthorizedDeferred(Role.OWNER)
+    async matchChatCreate(@Ctx() context: GraphQLContext, @Arg('matchId') matchId: number) {
+        const match = await prisma.match.findUnique({ where: { id: matchId }, include: { pupil: true } });
+        await hasAccess(context, 'Match', match);
         return true;
     }
 
     @Mutation(() => Boolean)
-    subcourseChatCreate(@Ctx() context: GraphQLContext) {
+    @AuthorizedDeferred(Role.OWNER)
+    async subcourseChatCreate(@Ctx() context: GraphQLContext, @Arg('subcourseId') subcourseId: number) {
+        const subcourse = await prisma.subcourse.findUnique({ where: { id: subcourseId } });
+        await hasAccess(context, 'Subcourse', subcourse);
         return true;
     }
 
     @Mutation(() => Boolean)
-    async participantChatCreate(@Ctx() context: GraphQLContext) {
+    @AuthorizedDeferred(Role.OWNER)
+    async participantChatCreate(@Ctx() context: GraphQLContext, @Arg('subcourseId') subcourseId: number) {
+        const subcourse = await prisma.subcourse.findUnique({ where: { id: subcourseId } });
+        await hasAccess(context, 'Subcourse', subcourse);
         return true;
     }
 
     @Mutation(() => Boolean)
+    @AuthorizedDeferred(Role.USER)
     async prospectChatCreate(@Ctx() context: GraphQLContext) {
         return true;
     }
