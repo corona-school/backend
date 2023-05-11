8971475289b09f1856f10e10784285a6db2de3da
LomyW <115979086+LomyW@users.noreply.github.com>
2023-05-11 19:40:29 +0200

    test: appointment queries (#612)
    * feat: add methods to create and get appointments
    
    * moved some things and added mutations
    
    * add fieldresolvers for appointments
    
    * add fieldResolvers for appointments
    
    * refactor field auth and appointmentType
    
    * add participant field and ownership
    
    * suffixed participant and organizers column with id
    
    * move util func getUsers to common
    
    * Update graphql/user/fields.ts
    
    Co-authored-by: Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
    
    * refactorings and optimizations
    function namings, code deduplication
    
    * add orderBy to subcourse and match appointments
    
    * add first appointment test
    
    * create more appointments and query my appointments
    
    * prisma create many appointments
    
    * query appointment participants
    
    Co-authored-by: Andreas (AK) <andreas@typedigital.de>
    
    * test to decline and cancel an appointment
    
    * test for updating an appointment
    
    * remove comment
    
    * review changes
    
    * remove decline appointment test
    
    ---------
    
    Co-authored-by: Andreas (AK) <andreas@typedigital.de>
    Co-authored-by: Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>

---
+++ integration-tests/appointments.ts
@@ -0,0 +1,157 @@
+import { test } from './base';
+import { screenedInstructorOne } from './screening';
+import { subcourseOne } from './course';
+import assert from 'assert';
+import { pupilOne } from './user';
+
+const appointmentTitle = 'Group Appointment 1';
+const appointmentTitle2 = 'Group Appointment 2';
+const appointmentTitle3 = 'Group Appointment 3';
+
+const firstAppointment = test('Create an appointment for a subcourse', async () => {
+    const { subcourseId } = await subcourseOne;
+    const { client } = await screenedInstructorOne;
+    const nextHour = new Date();
+    nextHour.setHours(new Date().getHours() + 1);
+    const res = await client.request(`
+    mutation creategroupAppointments {
+        appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
+            {
+                title: "${appointmentTitle}"
+                start: "${nextHour.toISOString()}"
+                duration: 15
+                subcourseId: ${subcourseId}
+                appointmentType: group
+            }])
+        }
+        `);
+    assert.ok(res.appointmentsGroupCreate);
+});
+
+const moreAppointments = test('Create more appointments for a subcourse', async () => {
+    const { subcourseId } = await subcourseOne;
+    const { client } = await screenedInstructorOne;
+    const nextDate = new Date();
+    nextDate.setDate(new Date().getDate() + 2);
+    const nextMonth = new Date();
+    nextMonth.setMonth(new Date().getMonth() + 1);
+
+    const res = await client.request(`
+        mutation creategroupAppointments {
+            appointmentsGroupCreate(subcourseId: ${parseInt(subcourseId)}, appointments: [
+                {
+                    title: "${appointmentTitle2}"
+                    start: "${nextDate.toISOString()}"
+                    duration: 30
+                    subcourseId: ${subcourseId}
+                    appointmentType: group
+                },
+                {
+                title: "${appointmentTitle3}"
+                start: "${nextMonth.toISOString()}"
+                duration: 60
+                subcourseId: ${subcourseId}
+                appointmentType: group
+            }
+        ])
+        }
+        `);
+    assert.ok(res.appointmentsGroupCreate);
+});
+
+const myAppointments = test('Get my appointments', async () => {
+    const { client } = await screenedInstructorOne;
+    await firstAppointment;
+    await moreAppointments;
+
+    const {
+        me: { appointments },
+    } = await client.request(`
+        query myAppointments {
+            me {
+                appointments(take: 3) {
+                    id
+                    title
+                    description
+                    start
+                    duration
+                    appointmentType
+                    displayName
+                    position
+                    total
+                    isCanceled
+                    organizers(skip: 0, take: 1) {
+                        id
+                        firstname
+                        lastname
+                    }
+                    participants(skip: 0, take: 1) {
+                        id
+                        firstname
+                        lastname
+                    }
+                    declinedBy
+                }
+            }
+        }
+    `);
+
+    assert.ok(appointments);
+    assert.ok(appointments.some((a) => a.title === appointmentTitle));
+    return appointments;
+});
+
+void test('Cancel an appointment as a organizer', async () => {
+    const { client } = await screenedInstructorOne;
+    await firstAppointment;
+    const clientAppointments = await myAppointments;
+    const appointmentId = clientAppointments[0].id;
+
+    await client.request(`mutation cancelAppointment {appointmentCancel(appointmentId: ${appointmentId})}`);
+    const isAppointmentCanceled = await client.request(`query appointment {appointment(appointmentId: ${appointmentId}){isCanceled}}`);
+    const {
+        me: { appointments },
+    } = await client.request(`query myAppointments { me { appointments(take: 3) { id }}}`);
+
+    assert.ok(isAppointmentCanceled);
+    assert.ok(appointments.some((a) => a.id != appointmentId));
+});
+
+void test('Update an appointment', async () => {
+    const { client } = await screenedInstructorOne;
+    await firstAppointment;
+    const clientAppointments = await myAppointments;
+    const appointmentId = clientAppointments[1].id;
+    const nextHour = new Date();
+    nextHour.setHours(new Date().getHours() + 1);
+
+    const updateTitle = 'Updated Title';
+    const resp = await client.request(`mutation updateAppointment { appointmentUpdate (
+        appointmentToBeUpdated: {
+            id: ${appointmentId}
+            title: "${updateTitle}"
+            description: "Description"
+            start: "${nextHour}"
+            duration: 120
+        }
+        )
+    }`);
+
+    const {
+        me: { appointments },
+    } = await client.request(`
+        query myAppointments {
+            me {
+                appointments(take: 10) {
+                    id
+                    title
+                    description
+                    start
+                    duration
+                }
+            }
+        }
+    `);
+
+    assert.ok(appointments.some((a) => a.id == appointmentId && a.title == updateTitle));
+});
