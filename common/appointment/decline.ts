6033e9c55827958e5f0e0c41d6f76fba211f784d
Jonas Wilms <unknown@example.org>
2023-05-24 20:27:21 +0200

    Add end time to appointment notifications (and refactor a bit)

---
+++ common/appointment/decline.ts
@@ -0,0 +1,45 @@
+import { lecture as Appointment, lecture_appointmenttype_enum as AppointmentType } from '@prisma/client';
+import { prisma } from '../prisma';
+import { getUser, getStudent, User } from '../user';
+import * as Notification from '../notification';
+import { getLogger } from '../logger/logger';
+import { getAppointmentForNotification } from './util';
+
+const logger = getLogger('Appointment');
+
+export async function declineAppointment(user: User, appointment: Appointment) {
+    await prisma.lecture.update({
+        data: { declinedBy: { push: user.userID } },
+        where: { id: appointment.id },
+    });
+
+    const pupil = await prisma.pupil.findUnique({ where: { id: user.pupilId } });
+
+    switch (appointment.appointmentType) {
+        case AppointmentType.group:
+            const subCourse = await prisma.subcourse.findUniqueOrThrow({ where: { id: appointment.subcourseId }, include: { course: true } });
+            for (const organizerId of appointment.organizerIds) {
+                const user = await getUser(organizerId);
+                const organizer = await getStudent(user);
+                await Notification.actionTaken(organizer, 'pupil_decline_appointment_group', {
+                    appointment: getAppointmentForNotification(appointment),
+                    pupil,
+                    course: subCourse.course,
+                });
+            }
+            break;
+
+        case AppointmentType.match:
+            for (const organizerId of appointment.organizerIds) {
+                const user = await getUser(organizerId);
+                const organizer = await getStudent(user);
+                await Notification.actionTaken(organizer, 'pupil_decline_appointment_match', {
+                    appointment: getAppointmentForNotification(appointment),
+                    pupil,
+                });
+            }
+            break;
+    }
+
+    logger.info(`User(${user.userID}) declined Appointment(${appointment.id})`);
+}
---
+++ common/appointment/util.ts
@@ -0,0 +1,27 @@
+import { lecture as Appointment } from '@prisma/client';
+
+const language = 'de-DE';
+
+export function getAppointmentEnd(appointment: Appointment) {
+    const end = appointment.start;
+    end.setMinutes(end.getMinutes() + appointment.duration);
+
+    return end;
+}
+
+export function getAppointmentForNotification(appointment: Appointment, original?: Appointment) {
+    const end = getAppointmentEnd(appointment);
+
+    return {
+        ...appointment,
+
+        start_day: appointment.start.toLocaleString(language, { weekday: 'long' }),
+        start_date: appointment.start.toLocaleString(language, { day: 'numeric', month: 'long', year: 'numeric' }),
+        start_time: appointment.start.toLocaleString(language, { hour: '2-digit', minute: '2-digit' }),
+
+        end_date: end.toLocaleString(language, { day: 'numeric', month: 'long', year: 'numeric' }),
+        end_time: end.toLocaleString(language, { hour: '2-digit', minute: '2-digit' }),
+
+        original: original ? getAppointmentForNotification(original) : undefined,
+    };
+}
