a7ff11c2451e49ff34e8828acd3ea1b4200b9288
Jonas Wilms <unknown@example.org>
2023-05-24 20:34:09 +0200

    small fixups

--- common/appointment/update.ts
+++ common/appointment/update.ts
@@ -8,7 +8,11 @@ import moment from 'moment';
 
 const logger = getLogger('Appointment');
 
-export async function updateAppointment(user: User, appointment: Appointment, appointmentUpdate: Partial<Appointment>) {
+export async function updateAppointment(
+    user: User,
+    appointment: Appointment,
+    appointmentUpdate: Partial<Pick<Appointment, 'description' | 'duration' | 'start' | 'title'>>
+) {
     const currentDate = moment();
     const isPastAppointment = moment(appointment.start).add(appointment.duration).isBefore(currentDate);
 
@@ -30,24 +34,26 @@ export async function updateAppointment(user: User, appointment: Appointment, ap
 
     // send notification if date has changed
     const student = await getStudent(user);
-    if (updatedAppointment.appointmentType === AppointmentType.group) {
-        const subcourse = await prisma.subcourse.findUniqueOrThrow({ where: { id: updatedAppointment.subcourseId }, include: { course: true } });
-        const participants = await prisma.subcourse_participants_pupil.findMany({ where: { subcourseId: subcourse.id }, include: { pupil: true } });
-        for (const participant of participants) {
-            await Notification.actionTaken(participant.pupil, 'pupil_change_appointment_group', {
+    switch (appointment.appointmentType) {
+        case AppointmentType.group:
+            const subcourse = await prisma.subcourse.findUniqueOrThrow({ where: { id: updatedAppointment.subcourseId }, include: { course: true } });
+            const participants = await prisma.subcourse_participants_pupil.findMany({ where: { subcourseId: subcourse.id }, include: { pupil: true } });
+            for (const participant of participants) {
+                await Notification.actionTaken(participant.pupil, 'pupil_change_appointment_group', {
+                    student: student,
+                    appointment: getAppointmentForNotification(updatedAppointment, /* original: */ appointment),
+                    course: subcourse.course,
+                });
+            }
+            break;
+
+        case AppointmentType.match:
+            const match = await prisma.match.findUnique({ where: { id: updatedAppointment.matchId }, include: { pupil: true } });
+            await Notification.actionTaken(match.pupil, 'pupil_change_appointment_match', {
                 student: student,
-                appointment: getAppointmentForNotification(updatedAppointment, /* original: */ appointment),
-                course: subcourse.course,
+                appointment: getAppointmentForNotification(updatedAppointment, /* original */ appointment),
             });
-        }
-    } else if (updatedAppointment.appointmentType === AppointmentType.match) {
-        const match = await prisma.match.findUnique({ where: { id: updatedAppointment.matchId }, include: { pupil: true } });
-        await Notification.actionTaken(match.pupil, 'pupil_change_appointment_match', {
-            student: student,
-            appointment: getAppointmentForNotification(updatedAppointment, /* original */ appointment),
-        });
-    } else {
-        throw new Error(`Unknown participant type ${updatedAppointment.appointmentType}`);
+            break;
     }
 
     logger.info(`Participants of Appointment(${appointment.id}) were notified of the appointment change`);
