a225760e777edf12a001f8ca2826cc1d9612912d
Jonas Wilms <unknown@example.org>
2023-06-03 18:17:39 +0200

    Move testconfig to file, make mocking more flexible

--- integration-tests/mock.ts
+++ integration-tests/mock.ts
@@ -1,15 +1,16 @@
 import assert from "assert";
-import { red } from "./base";
+import { blue, red } from "./base";
+import wcmatch from "wildcard-match";
 
 interface MockedFetch {
-    url: string;
-    body: string;
+    method: "GET" | "POST" | "PUT" | "PATCH";
+    url: string; // use some/path/* as a wildcard
+    body?: string; // Use * as a wildcard
     responseStatus: number;
-    response: string;
+    response?: string | object;
 }
 
-// A FIFO queue of mocked calls, thus expectFetch(...) must be called in order
-const expectedCalls: MockedFetch[] = [];
+let expectedCalls: MockedFetch[] = [];
 
 export function expectFetch(mocked: MockedFetch) {
     expectedCalls.push(mocked);
@@ -20,14 +21,25 @@ export function setupFetchMock() {
 
     async function mockedFetch(resource: RequestInfo, options: RequestInit) {
         const url = "" + resource;
-        if (expectedCalls.length > 0 && expectedCalls[0].url === url) {
-            const expectedCall = expectedCalls.shift();
-            assert.strictEqual(options.body, expectedCall.body, `Wrong Body of request to ${url}`);
+
+        const mock = expectedCalls.find(it => (it.url === url || wcmatch(it.url)(url)) && it.method === (options.method ?? "GET"));
+
+        if (mock) {
+            expectedCalls = expectedCalls.filter(it => it !== mock);
+            if (mock.body && !wcmatch(mock.body)(options.body.toString())) {
+                // We are inside of the Webserver, throwing an error might raise weird exceptions in the end
+                console.log(red(`Wrong body of mocked call to ${mock.method} ${mock.url}:`));
+                console.log(`expected: ${mock.body}`);
+                console.log(`actual:   ${options.body}`);
+                process.exit(1);
+            }
+
             console.log(`Request to ${url} mocked`);
-            return new Response(expectedCall.response, { status: expectedCall.responseStatus });
+            return new Response(mock.response ? (typeof mock.response === "string" ? mock.response : JSON.stringify(mock.response)) : "mocked response", { status: mock.responseStatus });
         }
 
-        console.log(red(`Request to ${url} not mocked! The integration tests must be self containing `));
+        console.log(red(`Request to ${url} not mocked! The integration tests must be self containing. Mock with:\n`));
+        console.log(blue(`expectFetch(${JSON.stringify({ url, method: options.method, body: options.body, responseStatus: 200, response: "?" }, null, 2)})`));
         process.exit(1);
 
         // return originalFetch(resource, options);
