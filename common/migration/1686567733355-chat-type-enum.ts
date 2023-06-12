a82c8318d99df20e7d392a56ecdec47ea3fb1eb7
Lomy <salome.wick@typedigital.de>
2023-06-12 14:54:33 +0200

    migration for chat type enum

--- common/migration/1686567733355-chat-type-enum.ts
+++ common/migration/1686567733355-chat-type-enum.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class chatTypeEnum1686567733355 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."chat_type" AS ENUM('chat', 'announcement')`);
         await queryRunner.query(`ALTER TABLE "subcourse" ADD "groupChatType" "public"."chat_type" NOT NULL DEFAULT 'course'`);
     }
 
