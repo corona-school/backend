7965950432c27bda1fa5d86ffcb823b1f94f3fef
Lomy <salome.wick@typedigital.de>
2023-06-12 22:56:44 +0200

    migrate new chat types

--- common/migration/1686567733355-chat-type-enum.ts
+++ common/migration/1686567733355-chat-type-enum.ts
@@ -2,11 +2,24 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class chatTypeEnum1686567733355 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TYPE "public"."chat_type" RENAME TO "old_chat_type"`);
         await queryRunner.query(`CREATE TYPE "public"."chat_type" AS ENUM('chat', 'announcement')`);
-        await queryRunner.query(`ALTER TABLE "subcourse" ADD "groupChatType" "public"."chat_type" NOT NULL DEFAULT 'course'`);
+
+        await queryRunner.query(
+            `ALTER TABLE "subcourse" ALTER COLUMN "groupChatType" TYPE "public"."chat_type" USING "groupChatType"::text::"public"."chat_type"`
+        );
+
+        await queryRunner.query(`DROP TYPE "public"."old_chat_type"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TYPE "public"."chat_type"`);
+        await queryRunner.query(`ALTER TYPE "public"."chat_type" RENAME TO "new_chat_type"`);
+        await queryRunner.query(`CREATE TYPE "public"."chat_type" AS ENUM('chat', 'announcement')`);
+
+        await queryRunner.query(
+            `ALTER TABLE "subcourse" ALTER COLUMN "groupChatType" TYPE "public"."chat_type" USING "groupChatType"::text::"public"."chat_type"`
+        );
+
+        await queryRunner.query(`DROP TYPE "public"."new_chat_type"`);
     }
 }
