82ce806c285dcb7ed951a6ce575f0e93b5a1de5d
LomyW <115979086+LomyW@users.noreply.github.com>
2023-06-14 11:17:07 +0200

    feat: announcement group chat (#663)
    * make only pupils readonly for announcement chat
    
    * adjust only allowed contacts
    
    * update conversation on subcourse edit
    
    * fix linter
    
    * create chat as pupil, review changes
    
    * review changes
    
    * quick fix: auth for my contacts

--- common/chat/types.ts
+++ common/chat/types.ts
@@ -2,3 +2,11 @@ export enum ChatType {
     NORMAL = 'NORMAL',
     ANNOUNCEMENT = 'ANNOUNCEMENT',
 }
+
+export enum ContactReason {
+    COURSE = 'course',
+    MATCH = 'match',
+    ANNOUNCEMENT = 'announcement',
+    PARTICIPANT = 'participant',
+    PROSPECT = 'prospect',
+}
