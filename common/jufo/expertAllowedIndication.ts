db53902afb2d8b0e3d6d2a87c4154530e9679f93
Jonas Wilms <unknown@example.org>
2023-07-09 15:31:33 +0200

    Remove old enums

--- common/jufo/expertAllowedIndication.ts
+++
@@ -1,5 +0,0 @@
-export enum ExpertAllowedIndication {
-    PENDING = 'pending', // The permission for expert profile publication is pending
-    YES = 'yes', // The profile is allowed to be public
-    NO = 'no', // The profile is denied to be public
-}
