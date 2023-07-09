5f2d54f2f5b90edc8367957e589b4a00c98b3e95
Jonas Wilms <unknown@example.org>
2023-07-09 15:27:50 +0200

    Remove old jobs

--- common/administration/match-making/tutoring/types/options.ts
+++
@@ -1,8 +0,0 @@
-import { MatchingAlgoSettings } from "../matching/types";
-
-export interface MatchMakingOptions {
-    dryRun: boolean;
-    matchingAlgoSettings?: MatchingAlgoSettings;
-    // If true, no notifications are sent to the matched
-    silent: boolean;
-}
