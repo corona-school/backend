1a3fca86e55ace3bb46ec5b0027e7e56a547c48c
Jonas Wilms <unknown@example.org>
2023-07-09 15:31:15 +0200

    Remove old interest confirmation

--- common/interest-confirmation/tutoring/notify/urls.ts
+++
@@ -1,3 +0,0 @@
-export function generateStatusChangeURLFromToken(token: string, confirmed: boolean) {
-    return `https://my.lern-fair.de/confirm?token=${token}&confirmed=${confirmed ? "true" : "false"}`;
-}
