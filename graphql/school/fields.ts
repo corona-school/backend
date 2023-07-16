78af5e72025e483bf6795f1639ee161adf6d2aab
Jonas Wilms <unknown@example.org>
2023-07-16 17:15:49 +0200

    Remove unecessary email parsing package

--- graphql/school/fields.ts
+++ graphql/school/fields.ts
@@ -1,10 +1,8 @@
 import { School } from '../generated';
-import { Arg, Authorized, FieldResolver, Query, Resolver, Root } from 'type-graphql';
+import { Arg, Authorized, Query, Resolver, Root } from 'type-graphql';
 import { prisma } from '../../common/prisma';
 import { Role } from '../authorizations';
-import { LimitedQuery, LimitEstimated } from '../complexity';
-import { Address } from 'address-rfc2821';
-import { validateEmail } from '../validators';
+import { LimitedQuery } from '../complexity';
 
 @Resolver((of) => School)
 export class ExtendedFieldsSchoolResolver {
@@ -12,11 +10,11 @@ export class ExtendedFieldsSchoolResolver {
     @Authorized(Role.UNAUTHENTICATED)
     @LimitedQuery()
     async schoolForTeacherEmail(@Arg('teacherEmail') teacherEmail: string) {
-        const email = new Address(validateEmail(teacherEmail));
+        const host = teacherEmail.split('@').pop();
 
         const school = await prisma.school.findFirst({
             where: {
-                emailDomain: email.host,
+                emailDomain: host,
             },
         });
 
