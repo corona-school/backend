e4711ec7812bbfaeb4fbe2494f52cce7c35b5053
Lomy <salome.wick@typedigital.de>
2023-07-27 12:11:02 +0200

    draft: authorization

--- web/controllers/chatNotificationController/index.ts
+++ web/controllers/chatNotificationController/index.ts
@@ -6,6 +6,7 @@ import { getPupil, getStudent, getUser } from '../../../common/user';
 import * as Notification from '../../../common/notification';
 import { pupil as Pupil, student as Student } from '@prisma/client';
 import { ChatType, getChatType, getNotificationContext, verifyChatUser } from './util';
+import { createHmac } from 'crypto';
 
 const logger = getLogger('ChatNotification');
 
@@ -13,14 +14,24 @@ export const chatNotificationRouter = Router();
 
 chatNotificationRouter.post('/chat-notification', handleChatNotification);
 
+const secretKey = process.env.TALKJS_API_KEY;
 async function handleChatNotification(req: Request, res: Response): Promise<void> {
     try {
         logger.info('Request at /chat/chat-notification');
-        const userAgent = req.headers['user-agent'];
 
-        if (!userAgent && !userAgent.includes('talkjs/webhook')) {
-            throw new Error('Invalid User-Agent');
-        }
+        const receivedSignature = req.headers['x-talkjs-signature'];
+        const timestamp = req.headers['x-talkjs-timestamp'];
+
+        const payload = timestamp + '.' + req.body;
+        const hash = createHmac('sha256', secretKey).update(payload);
+        const validSignature = hash.digest('hex').toUpperCase();
+
+        // logger.info(`rec sig: ${receivedSignature} : val: ${validSignature}`);
+        // logger.info(`body: ${JSON.stringify(req.body)} timestamp: ${timestamp}`);
+
+        // if (receivedSignature !== validSignature) {
+        //     throw new Error('Invalid Signature');
+        // }
 
         const notificationBody: NotificationTriggered = req.body;
         const { data } = notificationBody;
@@ -47,6 +58,7 @@ async function handleChatNotification(req: Request, res: Response): Promise<void
         }
         res.status(200).send({ status: 'ok' });
     } catch (error) {
+        logger.info(`${error}`);
         logger.error(`Failed to send notification for missed messages`, error);
         res.status(500).send({ error: 'Internal Server Error' });
     }
