08ea049c499bcbc05e177ffe14e82069259f303d
Lomy <salome.wick@typedigital.de>
2023-07-28 13:53:43 +0200

    add function to update zoom meeting

--- common/appointment/update.ts
+++ common/appointment/update.ts
@@ -5,6 +5,7 @@ import * as Notification from '../notification';
 import { getLogger } from '../logger/logger';
 import { getAppointmentForNotification } from './util';
 import moment from 'moment';
+import { getZoomMeeting, updateZoomMeeting } from '../zoom/zoom-scheduled-meeting';
 
 const logger = getLogger('Appointment');
 
@@ -13,31 +14,41 @@ export async function updateAppointment(
     appointment: Appointment,
     appointmentUpdate: Partial<Pick<Appointment, 'description' | 'duration' | 'start' | 'title'>>
 ) {
+    const { id, start, duration, appointmentType } = appointment;
+    const { duration: newDuration, start: newStart } = appointmentUpdate;
+
     const currentDate = moment();
-    const isPastAppointment = moment(appointment.start).add(appointment.duration).isBefore(currentDate);
+    const isPastAppointment = moment(start).add(duration).isBefore(currentDate);
 
     if (isPastAppointment) {
         throw new Error(`Cannot update past appointment.`);
     }
 
     const updatedAppointment = await prisma.lecture.update({
-        where: { id: appointment.id },
+        where: { id: id },
         data: { ...appointmentUpdate },
     });
 
     logger.info(`User(${user.userID}) updated Appointment(${appointment.id})`, { appointment, appointmentUpdate });
 
-    const sameDate = !appointmentUpdate.start || appointment.start.toISOString() === appointmentUpdate.start.toISOString();
+    const sameDate = !newStart || start.toISOString() === newStart.toISOString();
     if (sameDate) {
         return;
     }
 
-    // send notification if date has changed
+    let lastDate: Date;
+
     const student = await getStudent(user);
-    switch (appointment.appointmentType) {
+    switch (appointmentType) {
         case AppointmentType.group:
             const subcourse = await prisma.subcourse.findUniqueOrThrow({ where: { id: updatedAppointment.subcourseId }, include: { course: true } });
             const participants = await prisma.subcourse_participants_pupil.findMany({ where: { subcourseId: subcourse.id }, include: { pupil: true } });
+            const subcourseAppointments = await prisma.lecture.findMany({ where: { subcourseId: appointment.subcourseId } });
+            lastDate = subcourseAppointments[subcourseAppointments.length - 1].start;
+
+            logger.info(`Last Date: ${lastDate}`);
+
+            // send notification if date has changed
             for (const participant of participants) {
                 await Notification.actionTaken(participant.pupil, 'pupil_change_appointment_group', {
                     student: student,
@@ -49,10 +60,17 @@ export async function updateAppointment(
 
         case AppointmentType.match:
             const match = await prisma.match.findUnique({ where: { id: updatedAppointment.matchId }, include: { pupil: true } });
+            const matchAppointments = await prisma.lecture.findMany({ where: { subcourseId: appointment.subcourseId } });
+            lastDate = matchAppointments[matchAppointments.length - 1].start;
+
+            logger.info(`Last Date: ${lastDate}`);
+
+            // send notification if date has changed
             await Notification.actionTaken(match.pupil, 'pupil_change_appointment_match', {
                 student: student,
                 appointment: getAppointmentForNotification(updatedAppointment, /* original */ appointment),
             });
+
             break;
 
         case AppointmentType.internal:
@@ -60,5 +78,11 @@ export async function updateAppointment(
             break;
     }
 
-    logger.info(`Participants of Appointment(${appointment.id}) were notified of the appointment change`);
+    const before = await getZoomMeeting(appointment);
+    await updateZoomMeeting(appointment.zoomMeetingId, newStart, newDuration, lastDate);
+    const after = await getZoomMeeting(appointment);
+    logger.info(`BEFORE: ${JSON.stringify(before)}`);
+    logger.info(`UPDATED: ${JSON.stringify(after)}`);
+
+    logger.info(`Participants of Appointment(${id}) were notified of the appointment change`);
 }
