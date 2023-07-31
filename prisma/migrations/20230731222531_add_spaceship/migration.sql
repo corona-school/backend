3469e27874ebd3d4bf695783a2b0f356217cd516
Jonas Wilms <unknown@example.org>
2023-08-01 00:39:43 +0200

    Revert "TEST: Add migration"
    This reverts commit 1bbd74e66048eb71a5722306771b8ae9b5e6303d.

--- prisma/migrations/20230731222531_add_spaceship/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "spaceship" (
-    "name" TEXT NOT NULL,
-    "planet" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "spaceship_name_key" ON "spaceship"("name");
