fc6aafa6d3aa58564fc3f24e4456163615f46f9f
Jonas Wilms <unknown@example.org>
2023-08-01 15:03:29 +0200

    Move Course Tag creation to Prisma

--- common/courses/util.ts
+++ common/courses/util.ts
@@ -4,9 +4,8 @@ import { join } from 'path';
 
 const courseDefaultImage = process.env.WEBFLOW_COURSE_DEFAULT_IMAGE;
 
-export const COURSE_IMAGES_DEFAULT_PATH = "courses/images";
-export const COURSE_IMAGE_DEFAULT_NAME = "cover";
-
+export const COURSE_IMAGES_DEFAULT_PATH = 'courses/images';
+export const COURSE_IMAGE_DEFAULT_NAME = 'cover';
 
 export function getCourseImageKey(course: { id: number }, fileType: string) {
     return join(COURSE_IMAGES_DEFAULT_PATH, `${course.id}`, `${COURSE_IMAGE_DEFAULT_NAME}.${fileType}`);
