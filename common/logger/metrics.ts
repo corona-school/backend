61f728c3b0c4e08531ded64dbbef39b1342f39e7
Daniel Henkel <9447057+dhenkel92@users.noreply.github.com>
2023-08-11 14:15:28 +0200

    fix: datadog configuration (#791)

--- common/logger/metrics.ts
+++ common/logger/metrics.ts
@@ -1,10 +1,10 @@
 import { StatsD } from 'hot-shots';
-import { getEnvironment, getServiceName, getVersion } from '../../utils/environment';
+import { getDDEnvironment, getServiceName, getVersion } from '../../utils/environment';
 
 export const stats = new StatsD({
     prefix: 'lern_fair.',
     globalTags: {
-        env: getEnvironment(),
+        env: getDDEnvironment(),
         service: getServiceName(),
         version: getVersion(),
     },
