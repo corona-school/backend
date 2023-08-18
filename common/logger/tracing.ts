98d7f85822fd75f9f8531a56d7ae3103b0c6a336
Daniel Henkel <daniel@henkel.tech>
2023-08-18 09:53:22 +0200

    feature: add reminder cnt to job trace

--- common/logger/tracing.ts
+++ common/logger/tracing.ts
@@ -16,3 +16,10 @@ tracer.use('graphql', {});
 tracer.use('pg', { measured: true, service: 'postgres', enabled: true });
 
 export default tracer;
+
+export function addTagsToActiveSpan(tags: { [key: string]: any }): void {
+    const span = tracer.scope().active();
+    if (span) {
+        span.addTags(tags);
+    }
+}
