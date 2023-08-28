4ebdc639e9fe798101a8a38fb272b7a475ee93a7
Jonas Wilms <unknown@example.org>
2023-08-28 17:28:04 +0200

    Ignore if the matching algorithm is missing

--- common/match/pool.ts
+++ common/match/pool.ts
@@ -1,5 +1,6 @@
 import { prisma } from '../prisma';
 import type { Prisma, pupil as Pupil, student as Student } from '@prisma/client';
+// @ts-ignore The matching algorithm is optional, to allow for slim local setups
 import type { Helpee, Helper, Settings, SubjectWithGradeRestriction } from 'corona-school-matching';
 import { createMatch } from './create';
 import { parseSubjectString, Subject } from '../util/subjectsutils';
@@ -360,7 +361,8 @@ export async function runMatching(poolName: string, apply: boolean, _toggles: st
 
     const startMatching = Date.now();
 
-    // To run the matching we need the C++ Part:
+    // To run the matching we need the C++ Part, if it is not installed this will fail at runtime
+    // @ts-ignore
     const { match } = await import('corona-school-matching');
 
     const result = match(helpers, helpees, pool.settings);
