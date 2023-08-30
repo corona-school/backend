5bf3a644c6c668b9f418537127fafce4bcfac33c
Jonas Wilms <jonaswilms2000@gmail.com>
2023-08-30 11:22:20 +0200

    fix: Subcourse Appointment participant Management (#824)

--- common/appointment/participants.ts
+++ common/appointment/participants.ts
@@ -1,6 +1,9 @@
+import { getLogger } from '../logger/logger';
 import { Lecture } from '../entity/Lecture';
 import { prisma } from '../prisma';
 import { User } from '../user';
+
+const logger = getLogger('Appointment Participants');
 
 export async function isAppointmentParticipant(lecture: Lecture, user: User): Promise<boolean> {
     return !!(await prisma.lecture.findFirst({
@@ -17,10 +20,19 @@ export async function isAppointmentParticipant(lecture: Lecture, user: User): Pr
 }
 
 export async function addGroupAppointmentsParticipant(subcourseId: number, userId: string) {
-    await prisma.lecture.updateMany({
-        where: { subcourseId },
-        data: { participantIds: { push: userId } },
-    });
+    for (const lecture of await prisma.lecture.findMany({ where: { subcourseId } })) {
+        if (lecture.participantIds.includes(userId)) {
+            logger.info(`User(${userId}) is already a participant of Appointment(${lecture.id}) of Subcourse(${subcourseId})`);
+            continue;
+        }
+
+        if (lecture.organizerIds.includes(userId)) {
+            throw new Error(`User(${userId}) is already an organizer of Appointment(${lecture.id}) of Subcourse(${subcourseId}), cannot add as participant`);
+        }
+
+        await prisma.lecture.update({ where: { id: lecture.id }, data: { participantIds: { push: userId } } });
+        logger.info(`User(${userId}) added as participant of Appointment(${lecture.id}) of Subcourse(${subcourseId})`);
+    }
 }
 
 export async function removeGroupAppointmentsParticipant(subcourseId: number, userId: string) {
@@ -34,15 +46,28 @@ export async function removeGroupAppointmentsParticipant(subcourseId: number, us
                 where: { id: a.id },
                 data: { participantIds: { set: newParticipants } },
             });
+
+            logger.info(`Removed User(${userId}) from Appointment(${a.id}) of Subcourse(${subcourseId})`);
         })
     );
 }
 
 export async function addGroupAppointmentsOrganizer(subcourseId: number, organizerId: string) {
-    await prisma.lecture.updateMany({
-        where: { subcourseId },
-        data: { organizerIds: { push: organizerId } },
-    });
+    for (const lecture of await prisma.lecture.findMany({ where: { subcourseId } })) {
+        if (lecture.participantIds.includes(organizerId)) {
+            throw new Error(
+                `User(${organizerId}) is already a participant of Appointment(${lecture.id}) of Subcourse(${subcourseId}), cannot add as organizer`
+            );
+        }
+
+        if (lecture.organizerIds.includes(organizerId)) {
+            logger.info(`User(${organizerId}) is already an organizer of Appointment(${lecture.id}) of Subcourse(${subcourseId})`);
+            continue;
+        }
+
+        await prisma.lecture.update({ where: { id: lecture.id }, data: { participantIds: { push: organizerId } } });
+        logger.info(`User(${organizerId}) added as organizer of Appointment(${lecture.id}) of Subcourse(${subcourseId})`);
+    }
 }
 
 export async function removeGroupAppointmentsOrganizer(subcourseId: number, organizerId: string) {
@@ -55,6 +80,7 @@ export async function removeGroupAppointmentsOrganizer(subcourseId: number, orga
                 where: { id: a.id },
                 data: { organizerIds: { set: newOrganizers } },
             });
+            logger.info(`Removed User(${organizerId}) as organizer of Appointment(${a.id}) of Subcourse(${subcourseId})`);
         })
     );
 }
