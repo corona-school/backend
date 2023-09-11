f5213c21dd14b5b0dd19b2dc9f1665d89e42379e
Jonas Wilms <jonas@wilms.ninja>
2023-09-11 20:53:05 +0200

    Allow everyone to query all cooperations (we brag about them anyways), and allow admins to create new ones

---
+++ graphql/cooperation/mutation.ts
@@ -0,0 +1,28 @@
+import { Role } from '../roles';
+import { prisma } from '../../common/prisma';
+import { Cooperation } from '../generated';
+import { Arg, Authorized, Field, InputType, Mutation, Resolver } from 'type-graphql';
+
+@InputType()
+class CreateCooperationInput {
+    @Field()
+    tag: string;
+
+    @Field()
+    welcomeTitle: string;
+    @Field()
+    welcomeMessage: string;
+}
+
+@Resolver((of) => Cooperation)
+export class MutateCooperationResolver {
+    @Mutation((returns) => Boolean)
+    @Authorized(Role.ADMIN)
+    async cooperationCreate(@Arg('data') data: CreateCooperationInput) {
+        await prisma.cooperation.create({
+            data,
+        });
+
+        return true;
+    }
+}
