6c37940016c4a013d4d77bb0a4c5b9efcc7fb452
kristieelim <kristie_lim@hotmail.com>
2023-09-19 20:14:44 +0200

    adds several different statistics

--- jobs/slack-statistics.ts
+++ jobs/slack-statistics.ts
@@ -1,6 +1,6 @@
-import { prisma } from "../common/prisma";
-import { sendToSlack, SLACK_CHANNEL } from "../common/slack";
-import { table } from "../common/slack/blocks";
+import { prisma } from '../common/prisma';
+import { sendToSlack, SLACK_CHANNEL } from '../common/slack';
+import { table } from '../common/slack/blocks';
 
 export async function postStatisticsToSlack() {
     const lastMonthBegin = new Date();
@@ -21,35 +21,165 @@ export async function postStatisticsToSlack() {
     const pupilRegisteredCount = await prisma.pupil.count({ where: { createdAt: { gte: begin, lte: end } } });
     const studentsRegisteredCount = await prisma.student.count({ where: { createdAt: { gte: begin, lte: end } } });
 
-    //Screenings
-    const pupilScreeningCount = await prisma.pupil_screening.count({ where: { createdAt: { gte: begin, lte: end } } });
-    const tutorScreeningCount = await prisma.screening.count({ where: { createdAt: { gte: begin, lte: end } } });
-    const instructorScreeningCount = await prisma.instructor_screening.count({ where: { createdAt: { gte: begin, lte: end } } });
-    const tutorScreeningCountGroupBy = await prisma.screening.groupBy({
-        by: ['knowsCoronaSchoolFrom'],
-        where: { createdAt: { gte: begin, lte: end } },
-        _count: { success: true },
-    });
-    const instructorScreeningCountGroupBy = await prisma.instructor_screening.groupBy({
-        by: ['knowsCoronaSchoolFrom'],
-        where: { createdAt: { gte: begin, lte: end } },
-        _count: { success: true },
+    // Screenings
+    // Pupil screening - total
+    const pupilScreeningCount = await prisma.pupil_screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+        },
     });
 
-    //Matches
-    const matchCount = await prisma.match.count({ where: { createdAt: { gte: begin, lte: end } } });
+    // Pupil screening - success
+    const pupilScreeningSuccessCount = await prisma.pupil_screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+            status: 'success',
+        },
+    });
 
-    //Subcourses
-    const subcourseCreatedCount = await prisma.subcourse.count({ where: { createdAt: { gte: begin, lte: end } } });
-    //TODO: number of free / taken seats of current public subcourses
+    // Tutor screening - total
+    const tutorScreeningCount = await prisma.screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+        },
+    });
 
-    //Match appointments
+    // Tutor screening - success
+    const tutorScreeningSuccessCount = await prisma.screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+            success: true,
+        },
+    });
+
+    // Grouping 'knowsCoronaSchoolFrom' - Tutor
+    const tutorKnowsCoronaSchoolFrom = await prisma.screening.groupBy({
+        by: ['knowsCoronaSchoolFrom'],
+        where: {
+            createdAt: { gte: begin, lte: end },
+            knowsCoronaSchoolFrom: { notIn: [''] },
+        },
+        _count: {
+            createdAt: true,
+        },
+    });
+
+    const tutorKnowsCoronaSchoolFromValue = tutorKnowsCoronaSchoolFrom.map(function (item) {
+        return item['knowsCoronaSchoolFrom'];
+    });
+
+    const tutorKnowsCoronaSchoolFromCount = tutorKnowsCoronaSchoolFrom.map(function (item) {
+        return String(item['_count']['createdAt']);
+    });
+
+    const tutorKnowsCoronaSchoolFromTable: [string, string][] = [];
+    for (let i = 0; i < tutorKnowsCoronaSchoolFromValue.length; i++) {
+        tutorKnowsCoronaSchoolFromTable[i] = [tutorKnowsCoronaSchoolFromValue[i], tutorKnowsCoronaSchoolFromCount[i]];
+    }
+
+    // Instructor screening - total
+    const instructorScreeningCount = await prisma.instructor_screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+        },
+    });
+
+    // Instructor screening - success
+    const instructorScreeningSuccessCount = await prisma.instructor_screening.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+            success: true,
+        },
+    });
+
+    // Grouping 'knowsCoronaSchoolFrom' - Instructor
+    const instructorKnowsCoronaSchoolFrom = await prisma.instructor_screening.groupBy({
+        by: ['knowsCoronaSchoolFrom'],
+        where: {
+            createdAt: { gte: begin, lte: end },
+            knowsCoronaSchoolFrom: { notIn: [''] },
+        },
+        _count: {
+            createdAt: true,
+        },
+    });
+
+    const instructorKnowsCoronaSchoolFromValue = instructorKnowsCoronaSchoolFrom.map(function (item) {
+        return item['knowsCoronaSchoolFrom'];
+    });
+
+    const instructorKnowsCoronaSchoolFromCount = instructorKnowsCoronaSchoolFrom.map(function (item) {
+        return String(item['_count']['createdAt']);
+    });
+
+    const instructorKnowsCoronaSchoolFromTable: [string, string][] = [];
+    for (let i = 0; i < instructorKnowsCoronaSchoolFromValue.length; i++) {
+        instructorKnowsCoronaSchoolFromTable[i] = [instructorKnowsCoronaSchoolFromValue[i], instructorKnowsCoronaSchoolFromCount[i]];
+    }
+
+    // Matches
+    const matchCount = await prisma.match.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+        },
+    });
+
+    // New subcourses created
+    const subcourseCreatedCount = await prisma.subcourse.count({
+        where: {
+            createdAt: { gte: begin, lte: end },
+        },
+    });
+
+    // Number of current subcourses
+    const subcourseCurrentCount = await prisma.subcourse.count();
+
+    // TODO: number of free / taken seats of current public subcourses
+    const subcourseSeats = await prisma.course.findMany({
+        select: {
+            id: true,
+            name: true,
+            subcourse: {
+                select: {
+                    maxParticipants: true,
+                    // eslint-disable-next-line camelcase
+                    subcourse_participants_pupil: {
+                        select: {
+                            pupilId: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    const subcourseSeatsCount = subcourseSeats.map((course) => {
+        const subcourses = course.subcourse;
+        const totalSeats = subcourses.reduce((acc, subcourse) => acc + subcourse.maxParticipants, 0);
+        const takenSeats = subcourses.reduce((acc, subcourse) => acc + subcourse.subcourse_participants_pupil.length, 0);
+        const availableSeats = totalSeats - takenSeats;
+
+        return {
+            courseName: course.name,
+            totalSeats,
+            takenSeats,
+            availableSeats,
+        };
+    });
+
+    const subcourseSeatsTakenTable: [string, string][] = [];
+    for (let i = 0; i < subcourseSeatsCount.length; i++) {
+        subcourseSeatsTakenTable[i] = [
+            subcourseSeatsCount[i].courseName,
+            String(subcourseSeatsCount[i].takenSeats + ' von ' + subcourseSeatsCount[i].totalSeats),
+        ];
+    }
+
+    // Match appointments
     const matchAppointmentCount = await prisma.lecture.count({
         where: {
             createdAt: { gte: begin, lte: end },
-            matchId: {
-                notIn: [],
-            },
+            matchId: { notIn: 0 },
         },
     });
 
@@ -58,15 +188,17 @@ export async function postStatisticsToSlack() {
             table(`Statistiken vom ${begin} zum ${end}`, 'Name', 'Wert', [
                 ['Anzahl registrierter Schüler*innen', '' + pupilRegisteredCount],
                 ['Anzahl registrierter Helfer*innen', '' + studentsRegisteredCount],
-                ['Anzahl Screenings Schüler*innen', '' + pupilScreeningCount],
-                ['Anzahl Screenings Helfer*innen', '' + tutorScreeningCount],
-                ['Anzahl Screenings Lehrer*innen', '' + instructorScreeningCount],
+                ['Anzahl erfolgreicher Screenings Schüler*innen', '' + pupilScreeningSuccessCount + ' von ' + pupilScreeningCount],
+                ['Anzahl erfolgreicher Screenings Helfer*innen', '' + tutorScreeningSuccessCount + ' von ' + tutorScreeningCount],
+                ['Anzahl erfolgreicher Screenings Lehrer*innen', '' + instructorScreeningSuccessCount + ' von ' + instructorScreeningCount],
                 ['Anzahl erstellter Matches', '' + matchCount],
                 ['Anzahl erstellter Kurse', '' + subcourseCreatedCount],
+                ['Anzahl aktueller Kurse', '' + subcourseCurrentCount],
                 ['Anzahl Match-Termine', '' + matchAppointmentCount],
             ]),
-            //TODO: Tables for screenings (knowsCoronaSchoolFrom)
-            //TODO: Table for free/taken seats for subcourses
+            table('Von uns gehört durch... (Helfer*innen)', 'Name', 'Wert', tutorKnowsCoronaSchoolFromTable),
+            table('Von uns gehört durch... (Lehrer*innen)', 'Name', 'Wert', instructorKnowsCoronaSchoolFromTable),
+            table('Anzahl der belegten Plätze in den aktuellen Kursen', 'Name', 'Wert', subcourseSeatsTakenTable),
         ],
     });
 }
