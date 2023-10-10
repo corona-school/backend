992f648654bf8af32d0e240814694a14c71db09d
Daniel Henkel <daniel@henkel.tech>
2023-10-11 00:00:37 +0200

    set service label to all metrics

--- common/logger/metrics.ts
+++ common/logger/metrics.ts
@@ -1,7 +1,9 @@
 import * as promClient from 'prom-client';
 import { Request, Response, Router } from 'express';
+import { getServiceName } from '../../utils/environment';
 
-const regirstry = new promClient.Registry();
+const registry = new promClient.Registry();
+registry.setDefaultLabels({ service: getServiceName() });
 
 export const metricsRouter = Router();
 metricsRouter.get('/', handleMetrics);
@@ -9,31 +11,31 @@ metricsRouter.get('/', handleMetrics);
 export async function handleMetrics(_req: Request, res: Response) {
     res.status(200)
         .header('Content-Type:text')
-        .send(await regirstry.metrics());
+        .send(await registry.metrics());
 }
 
 export const metrics = {
     GraphqlRequests: new promClient.Counter({
         name: 'graphql_requests',
         help: 'Amount of graphql requests',
-        registers: [regirstry],
+        registers: [registry],
         labelNames: ['operation', 'hasErrors'],
     }),
     JobCountExecuted: new promClient.Counter({
         name: 'jobs_executed',
         help: 'Amount of jobs executed',
-        registers: [regirstry],
+        registers: [registry],
         labelNames: ['hasError', 'name'],
     }),
     FileStorageSize: new promClient.Gauge({
         name: 'file_storage_size',
         help: 'Amount of files in storage cache',
-        registers: [regirstry],
+        registers: [registry],
         labelNames: ['type'],
     }),
     FileStorageMaxSize: new promClient.Gauge({
         name: 'file_storage_max_size',
         help: 'Max amount of files in storage cache',
-        registers: [regirstry],
+        registers: [registry],
     }),
 };
