c389b5d01fe0ca2e2285b20743c24855aba4613a
Daniel Henkel <daniel@henkel.tech>
2023-10-10 22:21:23 +0200

    try to set service name

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -5,4 +5,4 @@ DD_SERVICE=${SERVICE_NAME:-n/a}
 DD_VERSION=${HEROKU_SLUG_COMMIT:-latest}
 GIT_COMMIT=${HEROKU_SLUG_COMMIT:-main}
 
-DD_TAGS="git.commit.sha:${GIT_COMMIT} git.repository_url:github.com/corona-school/backend"
+DD_TAGS="git.commit.sha:${GIT_COMMIT} git.repository_url:github.com/corona-school/backend service:${DD_SERVICE}"
