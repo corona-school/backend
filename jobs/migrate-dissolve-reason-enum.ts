9a52fa113b38e46749dbd8de1093a25d711637a3
realmayus <realmayus@gmail.com>
2023-10-30 10:59:34 +0100

    interim commit

--- jobs/migrate-dissolve-reason-enum.ts
+++ jobs/migrate-dissolve-reason-enum.ts
@@ -2,10 +2,11 @@ import { getLogger } from '../common/logger/logger';
 import { prisma } from '../common/prisma';
 import { dissolve_reason as DissolveReason, dissolved_by_enum as DissolvedBy } from '@prisma/client';
 import moment from 'moment-timezone';
+import { getUserTypeAndIdForUserId } from '../common/user';
 
 const logger = getLogger();
 
-const dissolveReasonByIndex = (index: number): DissolveReason => {
+const dissolveReasonByIndex = (index: number, dissolvedBy: DissolvedBy): DissolveReason => {
     switch (index) {
         case 0:
             return DissolveReason.accountDeactivated;
@@ -21,8 +22,28 @@ const dissolveReasonByIndex = (index: number): DissolveReason => {
             return DissolveReason.other;
         case 9:
             return DissolveReason.languageIssues;
-        default:
+        default: {
+            if (dissolvedBy == DissolvedBy.pupil) {
+                switch (index) {
+                    case 2:
+                        return DissolveReason.isOfNoHelp;
+                    case 3:
+                        return DissolveReason.noMoreTime;
+                    case 4:
+                        return DissolveReason.noMoreHelpNeeded;
+                }
+            } else if (dissolvedBy == DissolvedBy.student) {
+                switch (index) {
+                    case 2:
+                        return DissolveReason.noMoreHelpNeeded;
+                    case 3:
+                        return DissolveReason.isOfNoHelp;
+                    case 4:
+                        return DissolveReason.noMoreTime;
+                }
+            }
             return DissolveReason.unknown;
+        }
     }
 };
 
@@ -33,59 +54,72 @@ export default async function execute() {
     const matches = await prisma.match.findMany({
         where: {
             dissolved: true,
+            OR: [
+                // let's find the dissolved matches for which we haven't been able to find a dissolveReason or dissolver yet.
+                {
+                    dissolveReasonEnum: DissolveReason.unknown,
+                },
+                {
+                    dissolvedBy: DissolvedBy.unknown,
+                },
+            ],
         },
     });
+    // Gather all matchDissolve logs where a user was specified.
+    const dissolveLogs = await prisma.log.findMany({
+        where: {
+            logtype: 'matchDissolve',
+            NOT: {
+                OR: [
+                    {
+                        user: null,
+                    },
+                    {
+                        user: 'unknown', // logTransaction sets this if wix id is undefined
+                    },
+                ],
+            },
+        },
+        orderBy: {
+            createdAt: 'asc', // in case there are multiple dissolve logs for a match, we'd put the last one in the map
+        },
+    });
+
+    const dissolvers = new Map<number, DissolvedBy>(); // matchId -> DissolvedBy
+    for (const log of dissolveLogs) {
+        const data = JSON.parse(log.data);
+        if (data) {
+            const matchId = data.matchId;
+            if (matchId) {
+                try {
+                    const [type, _] = getUserTypeAndIdForUserId(log.user);
+                    if (type == 'pupil') {
+                        dissolvers[matchId] = DissolvedBy.pupil;
+                    } else if (type == 'student') {
+                        dissolvers[matchId] = DissolvedBy.student;
+                    } else if (type == 'screener') {
+                        dissolvers[matchId] = DissolvedBy.admin;
+                    }
+                } catch {
+                    continue;
+                }
+            }
+        }
+    }
+
     let knownReason = 0;
     let knownDissolvedBy = 0;
     let unknownReason = 0;
     let unknownDissolvedBy = 0;
     for (const match of matches) {
-        const reason = match.dissolveReasonEnum ?? dissolveReasonByIndex(match.dissolveReason);
-        logger.info(`Match(${match.id}) has dissolveReason ${reason}`);
-        let dissolver: DissolvedBy = DissolvedBy.unknown;
-        if (reason == DissolveReason.accountDeactivated && match.dissolvedAt) {
-            logger.info(`Match(${match.id}) was dissolved due to account deactivation, figuring out who deactivated their account...`);
-            const pupil = await prisma.pupil.findUnique({ where: { id: match.pupilId }, select: { active: true, wix_id: true } });
-            if (pupil && !pupil.active) {
-                const pupilDeactivatedCausingDissolve = await prisma.log.findFirst({
-                    where: {
-                        logtype: 'deActivate',
-                        user: pupil.wix_id,
-                        createdAt: {
-                            gte: match.dissolvedAt, // match gets dissolved before account deactivation
-                            lte: moment(match.dissolvedAt).add(2, 'seconds').toDate(), // add some buffer
-                        },
-                    },
-                });
-                if (pupilDeactivatedCausingDissolve) {
-                    dissolver = DissolvedBy.pupil;
-                    logger.info(`...Pupil(${pupilDeactivatedCausingDissolve.id}) was deactivated, causing the dissolve`);
-                }
-            }
-            if (dissolver === DissolvedBy.unknown) {
-                const student = await prisma.student.findUnique({ where: { id: match.studentId }, select: { active: true, wix_id: true } });
-                if (student && !student.active) {
-                    const studentDeactivatedCausingDissolve = await prisma.log.findFirst({
-                        where: {
-                            logtype: 'deActivate',
-                            user: student.wix_id,
-                            createdAt: {
-                                gte: match.dissolvedAt, // match gets dissolved before account deactivation
-                                lte: moment(match.dissolvedAt).add(2, 'seconds').toDate(), // add some buffer
-                            },
-                        },
-                    });
-                    if (studentDeactivatedCausingDissolve) {
-                        dissolver = DissolvedBy.student;
-                        logger.info(`...Student(${studentDeactivatedCausingDissolve.id}) was deactivated, causing the dissolve`);
-                    }
-                }
-            }
-            if (dissolver === DissolvedBy.unknown) {
-                logger.info("Couldn't find anyone who deactivated their account...");
-            }
+        const dissolver: DissolvedBy = dissolvers[match.id] ?? DissolvedBy.unknown;
+        const reason = dissolveReasonByIndex(match.dissolveReason, dissolver) ?? DissolveReason.unknown;
+        if (dissolver === DissolvedBy.unknown && reason === DissolveReason.unknown) {
+            // continue, can't update anyways
+            unknownDissolvedBy++;
+            unknownReason++;
+            continue;
         }
-        // Only if account deactivation caused match dissolve, we know who the dissolver is
 
         if (reason === DissolveReason.unknown) {
             unknownReason++;
@@ -98,6 +132,7 @@ export default async function execute() {
         } else {
             knownDissolvedBy++;
         }
+
         logger.info(`Match(${match.id}): Setting reason=${reason}, dissolver=${dissolver}`);
         await prisma.match.update({
             where: {
@@ -109,10 +144,10 @@ export default async function execute() {
             },
         });
     }
-    logger.info(`Updated ${knownReason + unknownReason} matches in total, of which:
-        - ${knownReason} have an unambiguous dissolveReason
-        - ${unknownReason} have an ambiguous dissolveReason (set to 'unknown')
-        - ${knownDissolvedBy} have a known dissolver
-        - ${unknownDissolvedBy} have an unknown dissolver
+    logger.info(`Found ${matches.length} matches with unknown dissolver/dissolveReasonEnum. Map of matchId -> dissolvedBy has ${dissolvers.size} entries.
+        - ${knownReason} now have a known dissolvedReason.
+        - We now know the dissolver for ${knownDissolvedBy} matches.
+        - ${unknownDissolvedBy} matches still have an unknown dissolver.
+        - ${unknownReason} matches still have an unknown reason.
     `);
 }
