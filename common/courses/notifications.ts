3f8e9a1ffefe95bfa2872548f78156472af85633
Jonas Wilms <jonas@wilms.ninja>
2023-11-01 17:39:41 +0100

    ignore dep cycle

--- common/courses/notifications.ts
+++ common/courses/notifications.ts
@@ -5,6 +5,7 @@ import { getFullName, userForPupil, userForStudent } from '../user';
 import * as Prisma from '@prisma/client';
 import { getFirstLecture } from './lectures';
 import { parseSubjectString } from '../util/subjectsutils';
+// eslint-disable-next-line import/no-cycle
 import { getCourseCapacity, getCourseFreePlaces, isParticipant } from './participants';
 import { getCourseImageURL } from './util';
 import { getCourse } from '../../graphql/util';
