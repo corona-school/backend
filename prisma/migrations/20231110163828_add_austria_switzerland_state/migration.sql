5ad5271d2fc1fc82f9c5775f77ef9bdb8c908397
realmayus <realmayus@gmail.com>
2023-11-10 19:45:09 +0100

    Add Switzerland and Austria as states

---
+++ prisma/migrations/20231110163828_add_austria_switzerland_state/migration.sql
@@ -0,0 +1,32 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "pupil_state_enum" ADD VALUE 'at';
+ALTER TYPE "pupil_state_enum" ADD VALUE 'ch';
+
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "school_state_enum" ADD VALUE 'at';
+ALTER TYPE "school_state_enum" ADD VALUE 'ch';
+
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "student_state_enum" ADD VALUE 'at';
+ALTER TYPE "student_state_enum" ADD VALUE 'ch';
