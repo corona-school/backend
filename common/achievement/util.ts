c477405c2d31460dba34f9c21ab29901127ef67d
LomyW <salome.wick@typedigital.de>
2023-11-15 14:35:17 +0100

    add relation resolution

--- common/achievement/util.ts
+++ common/achievement/util.ts
@@ -1,6 +1,7 @@
 import { ActionID, NotificationContext } from '../notification/types';
 import { metricsByAction } from './metrics';
 import { Metric } from './types';
+import { prisma } from '../prisma';
 
 export function getRelationByContext(context: NotificationContext): string {
     const { appointment, match, subcourse } = context;
@@ -13,9 +14,6 @@ export function getRelationByContext(context: NotificationContext): string {
         case context.subcourse:
             relation = `subcourse/${subcourse.id}`;
             break;
-        case context.appointment:
-            relation = `appointment/${appointment.id}`;
-            break;
         default:
             relation = '';
     }
@@ -25,4 +23,32 @@ export function getRelationByContext(context: NotificationContext): string {
 
 export function getMetricsByAction<ID extends ActionID>(actionId: ID): Metric[] {
     return metricsByAction.get(actionId) || [];
+}
+
+type RelationTypes = 'match' | 'subcourse';
+
+export function getRelationTypeAndId(relation: string): [relationType: RelationTypes, id: number] {
+    const validRelationTypes = ['match', 'subcourse'];
+    const [relationType, relationId] = relation.split('/');
+    if (!validRelationTypes.includes(relationType)) {
+        throw Error('No valid relation found in relation: ' + relationType);
+    }
+
+    const parsedRelationId = parseInt(relationId, 10);
+    return [relationType as RelationTypes, parsedRelationId];
+}
+
+export async function getRelationContext(relation: string) {
+    const [type, id] = getRelationTypeAndId(relation);
+
+    if (type == 'match') {
+        const match = await prisma.match.findFirst({ where: { id }, select: { id: true, lecture: { select: { start: true, duration: true } } } });
+        return match;
+    }
+    if (type == 'subcourse') {
+        const subcourse = await prisma.subcourse.findFirst({ where: { id }, select: { id: true, lecture: { select: { start: true, duration: true } } } });
+        return subcourse;
+    }
+
+    throw new Error(`Unknown Relation(${relation})`);
 }
