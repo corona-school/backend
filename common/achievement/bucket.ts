c87442a335dd712770c2babd22d54de4216f81f6
LomyW <salome.wick@typedigital.de>
2023-11-15 14:31:25 +0100

    add first bucket creators

--- common/achievement/bucket.ts
+++ common/achievement/bucket.ts
@@ -1,6 +1,31 @@
-import { BucketFormula } from './types';
+import moment from 'moment';
+import { Bucket, BucketFormula } from './types';
+import { getRelationContext } from './util';
 
 type BucketCreatorDefs = Record<string, BucketFormula>;
 
 // Buckets are needed to pre-sort and aggregate certain events by types / a certain time window (e.g. weekly) etc.
-export const bucketCreatorDefs: BucketCreatorDefs = {};
+export const bucketCreatorDefs: BucketCreatorDefs = {
+    default: {
+        function: (): Bucket[] => {
+            return [];
+        },
+    },
+    by_lecture_start: {
+        function: async (relation): Promise<Bucket[]> => {
+            if (!relation) {
+                return [];
+            }
+            const context = await getRelationContext(relation);
+            return context.lecture.map((lecture) => ({
+                startTime: moment(lecture.start).subtract(10, 'minutes').toDate(),
+                endTime: moment(lecture.start).add(lecture.duration, 'minutes').add(10, 'minutes').toDate(),
+            }));
+        },
+    },
+    by_weeks: {
+        function: (relation) => {
+            return [];
+        },
+    },
+};
