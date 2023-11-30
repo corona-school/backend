1c69c6e328bd30858913b3457ce4b5479094c6c7
Lucas <lucas.falkowsky@typedigital.de>
2023-11-30 13:55:52 +0100

    fix: delete unnecessary data

--- jobs/periodic/revalidate-streaks/index.ts
+++ jobs/periodic/revalidate-streaks/index.ts
@@ -1,38 +1,6 @@
 import { checkUserAchievement } from '../../../common/achievement';
 import { UserAchievementTemplate } from '../../../common/achievement/types';
 import { prisma } from '../../../common/prisma';
-import { CSJob } from '../../types';
-
-const userAchievement = {
-    userId: 'student/1',
-    id: 67,
-    achievedAt: '2023-11-29T12:15:25.611Z',
-    context: '{}',
-    template: {
-        id: 8,
-        name: 'Anwesenheits-Rekord',
-        metrics: ['weekly_presence'],
-        templateFor: 'Global',
-        group: 'weekly_presence',
-        groupOrder: 1,
-        stepName: '',
-        type: 'STREAK',
-        subtitle: '',
-        description: 'Dieses Achievement zeichnet kontinuierliche Teilnahme auf einer Lernplattform aus, insbesondere im Hinblick auf die Anwesenheit.',
-        image: 'https://img.freepik.com/vektoren-kostenlos/flache-landschaft-von-bergen-am-meer_23-2147587827.jpg?size=626&ext=jpg&ga=GA1.1.153302910.1700229246&semt=ais',
-        achievedImage:
-            'https://img.freepik.com/vektoren-kostenlos/flache-landschaft-von-bergen-am-meer_23-2147587827.jpg?size=626&ext=jpg&ga=GA1.1.153302910.1700229246&semt=ais',
-        actionName: null,
-        actionRedirectLink: '/matching',
-        actionType: null,
-        achievedText:
-            'Herzlichen Glückwunsch! Du hast deinen Anwesenheits-Rekord gebrochen und beweist damit deine außergewöhnliche Engagement und Hingabe zum Lernen. Mach weiter so und lass dich von deinem Erfolg zu noch größeren Leistungen motivieren!',
-        condition: 'weekly_presence_count > recordValue',
-        conditionDataAggregations: { weekly_presence_count: { metric: 'weekly_presence', aggregator: 'count_weeks', createBuckets: 'by_weeks' } },
-        isActive: true,
-    },
-    recordValue: 1,
-};
 
 export enum StreakTimeFrame {
     WEEK = 'by_weeks',
