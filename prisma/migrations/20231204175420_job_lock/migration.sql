64e0adeaecf3af15f507300dba4b5408863e0f3e
Jonas Wilms <jonas@wilms.ninja>
2023-12-04 18:54:49 +0100

    Consistently use Timestamps with milliseconds

--- prisma/migrations/20231204175420_job_lock/migration.sql
+++ prisma/migrations/20231204175420_job_lock/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "job_run" (
     "job_name" TEXT NOT NULL,
     "startedAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "endedAt" TIMESTAMP(3),
+    "endedAt" TIMESTAMP(6),
     "worker" TEXT NOT NULL,
 
     CONSTRAINT "job_run_pkey" PRIMARY KEY ("job_name","startedAt")
