fbd23dc07551e087adeabfbf0acb716ae95187e7
Marius <realmayus@gmail.com>
2023-12-07 17:01:13 +0100

    Split up migrations (as multiple enum additions aren't supported by older postgres versions)

---
+++ prisma/migrations/20231207155807_add_austria/migration.sql
@@ -0,0 +1,8 @@
+-- AlterEnum
+ALTER TYPE "pupil_state_enum" ADD VALUE 'at';
+
+-- AlterEnum
+ALTER TYPE "school_state_enum" ADD VALUE 'at';
+
+-- AlterEnum
+ALTER TYPE "student_state_enum" ADD VALUE 'at';
