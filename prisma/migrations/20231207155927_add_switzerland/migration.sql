2a3623a98322d969fda4569dc5c5479cd493540f
Marius <realmayus@gmail.com>
2023-12-07 17:09:05 +0100

    Split up migrations even more!

--- prisma/migrations/20231207155927_add_switzerland/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "pupil_state_enum" ADD VALUE 'ch';
-
--- AlterEnum
-ALTER TYPE "school_state_enum" ADD VALUE 'ch';
-
--- AlterEnum
-ALTER TYPE "student_state_enum" ADD VALUE 'ch';
