9a3626c31032f471f06a71899e7387e6e32d870f
Jonas Wilms <jonas@wilms.ninja>
2023-12-19 16:11:17 +0100

    Expose the conversation object

--- graphql/chat/fields.ts
+++ graphql/chat/fields.ts
@@ -3,6 +3,7 @@ import { isConversationReadOnly, shouldMarkChatAsReadonly } from '../../common/c
 import { TJConversation } from '../../common/chat/types';
 import { Role } from '../roles';
 import { getConversation } from '../../common/chat';
+import { GraphQLJSON } from 'graphql-scalars';
 
 @ObjectType()
 export class Chat {
@@ -19,6 +20,30 @@ export async function getChat(conversationID: string): Promise<Chat | null> {
 
 @Resolver((of) => Chat)
 export class FieldsChatResolver {
+    @FieldResolver((returns) => String)
+    @Authorized(Role.ADMIN)
+    conversationId(@Root() chat: Chat) {
+        return chat.conversation.id;
+    }
+
+    @FieldResolver((returns) => Date)
+    @Authorized(Role.ADMIN)
+    createdAt(@Root() chat: Chat) {
+        return chat.conversation.createdAt;
+    }
+
+    @FieldResolver((returns) => String)
+    @Authorized(Role.ADMIN)
+    subject(@Root() chat: Chat) {
+        return chat.conversation.subject;
+    }
+
+    @FieldResolver((returns) => GraphQLJSON, { nullable: true })
+    @Authorized(Role.ADMIN)
+    customData(@Root() chat: Chat) {
+        return chat.conversation.custom;
+    }
+
     @FieldResolver((returns) => Boolean)
     @Authorized(Role.ADMIN)
     async shouldMarkAsReadonly(@Root() chat: Chat) {
