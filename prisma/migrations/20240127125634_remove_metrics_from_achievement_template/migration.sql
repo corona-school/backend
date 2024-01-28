7d7ec22c723a2ce757dc9981f6c6c70cb31f946b
Daniel Henkel <daniel@henkel.tech>
2024-01-28 14:42:52 +0100

    chore: squash achievement migrations
    During the implementation, we had updated the DB structure many times.
    This PR is squashing all migrations into one, as separate migrations don't provide a lot of value here.

--- prisma/migrations/20240127125634_remove_metrics_from_achievement_template/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `metrics` on the `achievement_template` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "achievement_template" DROP COLUMN "metrics";
