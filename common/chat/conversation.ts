589da9e23a437397d6b7dcaf27cfa1122bc558b6
LomyW <salome.wick@typedigital.de>
2024-02-09 08:24:32 +0100

    fix build

--- common/chat/conversation.ts
+++ common/chat/conversation.ts
@@ -159,7 +159,7 @@ async function updateConversation(conversationToBeUpdated: { id: string } & Conv
     assert(TALKJS_SECRET_KEY, `No TalkJS secret key found to update conversation ${conversationToBeUpdated.id}.`);
     assureChatFeatureActive();
 
-    logger.info('Update chat conversation, {conversationToBeUpdated});
+    logger.info(`Update chat conversation, ${conversationToBeUpdated.id}`);
 
     try {
         if (!(await getConversation(conversationToBeUpdated.id))) {
