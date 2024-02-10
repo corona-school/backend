8a61256d36498d2a5b02b57e89d3ddb4a4b65d61
Jonas Wilms <jonaswilms2000@gmail.com>
2024-02-10 11:46:22 +0100

    feat: Also allow screeners to revoke match requests (#988)
    Co-authored-by: Jonas Wilms <jonas@wilms.ninja>

--- graphql/pupil/mutations.ts
+++ graphql/pupil/mutations.ts
@@ -284,7 +284,7 @@ export class MutatePupilResolver {
     }
 
     @Mutation((returns) => Boolean)
-    @Authorized(Role.ADMIN, Role.TUTEE)
+    @Authorized(Role.ADMIN, Role.TUTEE, Role.SCREENER)
     async pupilDeleteMatchRequest(@Ctx() context: GraphQLContext, @Arg('pupilId', { nullable: true }) pupilId?: number): Promise<boolean> {
         const pupil = await getSessionPupil(context, /* elevated override */ pupilId);
         await deletePupilMatchRequest(pupil);
