bebadb6f5ef2db728858e3cabc2ba8c241aca120
Daniel Henkel <daniel@henkel.tech>
2024-03-04 11:22:11 +0100

    feature: create achievemenet template uid

--- common/logger/metrics.ts
+++ common/logger/metrics.ts
@@ -49,12 +49,12 @@ export const metrics = {
         name: 'achievements_created',
         help: 'Amount of created achievements',
         registers: [registry],
-        labelNames: ['id', 'title', 'type'],
+        labelNames: ['id', 'uid', 'type'],
     }),
     AchievementsAchieved: new promClient.Counter({
         name: 'achievements_achieved',
         help: 'Amount of achieved achievements',
         registers: [registry],
-        labelNames: ['id', 'title', 'type'],
+        labelNames: ['id', 'uid', 'type'],
     }),
 };
