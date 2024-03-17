bfa1d8908b6ec140ff56ac4fe9b21f532e61b974
Jonas Wilms <jonaswilms2000@gmail.com>
2024-03-17 14:53:36 +0100

    fix: Cancel reminders when cancelling courses (#1033)

--- common/appointment/cancel.ts
+++ common/appointment/cancel.ts
@@ -39,54 +39,61 @@ export async function cancelAppointment(user: User, appointment: Appointment, si
 
     const student = await getStudent(user);
 
-    // Notifications are sent only when an appointment is cancelled, not when a subcourse is cancelled
-    if (!silent) {
-        switch (appointment.appointmentType) {
-            case AppointmentType.group: {
-                const subcourse = await prisma.subcourse.findFirst({ where: { id: appointment.subcourseId }, include: { course: true } });
-                const participants = await prisma.subcourse_participants_pupil.findMany({ where: { subcourseId: subcourse.id }, include: { pupil: true } });
-                const instructors = await prisma.subcourse_instructors_student.findMany({ where: { subcourseId: subcourse.id }, include: { student: true } });
+    switch (appointment.appointmentType) {
+        case AppointmentType.group: {
+            const subcourse = await prisma.subcourse.findFirst({ where: { id: appointment.subcourseId }, include: { course: true } });
+            const participants = await prisma.subcourse_participants_pupil.findMany({ where: { subcourseId: subcourse.id }, include: { pupil: true } });
+            const instructors = await prisma.subcourse_instructors_student.findMany({ where: { subcourseId: subcourse.id }, include: { student: true } });
 
-                for (const participant of participants) {
+            for (const participant of participants) {
+                // Notifications are sent only when an appointment is cancelled, not when a subcourse is cancelled
+                if (!silent) {
                     await Notification.actionTaken(userForPupil(participant.pupil), 'student_cancel_appointment_group', {
                         appointment: getAppointmentForNotification(appointment),
                         student,
                         ...(await getNotificationContextForSubcourse(subcourse.course, subcourse)),
                     });
-                    await Notification.actionTaken(userForPupil(participant.pupil), 'cancel_group_appointment_reminder', {
-                        appointment: getAppointmentForNotification(appointment),
-                        uniqueId: appointment.id.toString(),
-                    });
                 }
-                for (const instructor of instructors) {
-                    await Notification.actionTaken(userForStudent(instructor.student), 'cancel_group_appointment_reminder', {
-                        appointment: getAppointmentForNotification(appointment),
-                        uniqueId: appointment.id.toString(),
-                    });
-                }
-                break;
+
+                // Cancellation actions are triggered regardless
+                await Notification.actionTaken(userForPupil(participant.pupil), 'cancel_group_appointment_reminder', {
+                    appointment: getAppointmentForNotification(appointment),
+                    uniqueId: appointment.id.toString(),
+                });
             }
-            case AppointmentType.match: {
-                const match = await prisma.match.findUnique({ where: { id: appointment.matchId }, include: { pupil: true, student: true } });
+
+            for (const instructor of instructors) {
+                await Notification.actionTaken(userForStudent(instructor.student), 'cancel_group_appointment_reminder', {
+                    appointment: getAppointmentForNotification(appointment),
+                    uniqueId: appointment.id.toString(),
+                });
+            }
+            break;
+        }
+        case AppointmentType.match: {
+            const match = await prisma.match.findUnique({ where: { id: appointment.matchId }, include: { pupil: true, student: true } });
+
+            if (!silent) {
                 await Notification.actionTaken(userForPupil(match.pupil), 'student_cancel_appointment_match', {
                     appointment: getAppointmentForNotification(appointment),
                     student,
                 });
-                await Notification.actionTaken(userForPupil(match.pupil), 'cancel_match_appointment_reminder', {
-                    appointment: getAppointmentForNotification(appointment),
-                    uniqueId: appointment.id.toString(),
-                });
-                await Notification.actionTaken(userForStudent(match.student), 'cancel_match_appointment_reminder', {
-                    appointment: getAppointmentForNotification(appointment),
-                    uniqueId: appointment.id.toString(),
-                });
-
-                break;
             }
-            case AppointmentType.internal:
-            case AppointmentType.legacy:
-                break;
+
+            await Notification.actionTaken(userForPupil(match.pupil), 'cancel_match_appointment_reminder', {
+                appointment: getAppointmentForNotification(appointment),
+                uniqueId: appointment.id.toString(),
+            });
+            await Notification.actionTaken(userForStudent(match.student), 'cancel_match_appointment_reminder', {
+                appointment: getAppointmentForNotification(appointment),
+                uniqueId: appointment.id.toString(),
+            });
+
+            break;
         }
+        case AppointmentType.internal:
+        case AppointmentType.legacy:
+            break;
     }
 
     if (appointment.zoomMeetingId) {
