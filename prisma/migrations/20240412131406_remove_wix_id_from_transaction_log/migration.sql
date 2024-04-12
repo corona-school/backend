a47aa33873cd15d8ac95ac9ed4a08a514a1ce690
John Angel <john.angel@lern-fair.de>
2024-04-12 15:29:31 +0200

    refactor: Add migration to remove user (wix_id) column from transaction log

---
+++ prisma/migrations/20240412131406_remove_wix_id_from_transaction_log/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user` on the `log` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "log" DROP COLUMN "user";
