d28303faa7394262068d98fd25b45fc5b5957fa8
Jonas Wilms <jonas@wilms.ninja>
2024-07-18 17:09:12 +0200

    feat: Queries for learning assignments and topics

--- graphql/learning/topic/fields.ts
+++ graphql/learning/topic/fields.ts
@@ -1,10 +1,21 @@
-import { Authorized, FieldResolver, Resolver, Root } from 'type-graphql';
+import { Arg, Authorized, Ctx, FieldResolver, Query, Resolver, Root } from 'type-graphql';
 import { Learning_assignment as LearningAssignment, Learning_note as LearningNote, Learning_topic as LearningTopic } from '../../generated';
-import { ImpliesRoleOnResult, Role } from '../../authorizations';
+import { AuthorizedDeferred, ImpliesRoleOnResult, Role, hasAccess } from '../../authorizations';
 import { prisma } from '../../../common/prisma';
+import { GraphQLContext } from '../../context';
+import { GraphQLInt } from 'graphql';
 
 @Resolver((of) => LearningTopic)
 export class LearningTopicFieldResolver {
+    @Query((returns) => LearningTopic)
+    @AuthorizedDeferred(Role.ADMIN, Role.OWNER)
+    @ImpliesRoleOnResult(Role.OWNER, Role.OWNER)
+    async learningTopic(@Ctx() context: GraphQLContext, @Arg('id', () => GraphQLInt) id: number) {
+        const result = await prisma.learning_topic.findUniqueOrThrow({ where: { id } });
+        await hasAccess(context, 'Learning_topic', result);
+        return result;
+    }
+
     @FieldResolver((returns) => [LearningNote])
     @Authorized(Role.OWNER, Role.ADMIN)
     @ImpliesRoleOnResult(Role.OWNER, Role.OWNER)
