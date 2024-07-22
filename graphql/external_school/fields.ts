562b8ed5e9846398511c22fc23c8154a039a7273
John Angel <john.angel@lern-fair.de>
2024-07-22 18:04:25 +0200

    feat: Add rate limit

--- graphql/external_school/fields.ts
+++ graphql/external_school/fields.ts
@@ -2,6 +2,7 @@ import { Arg, Authorized, Field, InputType, Int, ObjectType, Query, Resolver } f
 import { SchoolResult, searchExternalSchools } from '../../common/school/externalSearch';
 import { pupil_state_enum as State, pupil_schooltype_enum as SchoolType } from '@prisma/client';
 import { Role } from '../authorizations';
+import { RateLimit } from '../rate-limit';
 
 @ObjectType()
 class ExternalSchoolSearch implements SchoolResult {
@@ -37,6 +38,7 @@ class ExternalSchoolSearchOptions {
 export class ExternalSchoolResolver {
     @Query((returns) => [ExternalSchoolSearch])
     @Authorized(Role.UNAUTHENTICATED)
+    @RateLimit('ExternalSchoolSearch', 100 /* requests per */, 5 * 60 * 60 * 1000 /* 5 hours */)
     externalSchoolSearch(@Arg('filters') filters: ExternalSchoolSearchFilters, @Arg('options') options: ExternalSchoolSearchOptions) {
         return searchExternalSchools({ filters, options });
     }
