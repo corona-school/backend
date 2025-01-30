ff7ae4f245cb364ea79f7b397fbccd0cfc89ff30
John Angel <john.angel@lern-fair.de>
2025-01-30 17:31:44 +0100

    feat: Add Google authentication function

---
+++ common/idp/clients/google.ts
@@ -0,0 +1,26 @@
+import { OAuth2Client } from 'google-auth-library';
+import { USER_APP_DOMAIN } from '../../util/environment';
+
+const oAuth2Client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_KEY, `http://${USER_APP_DOMAIN}/login-with`);
+
+interface AuthenticateResponse {
+    email: string;
+    firstname: string;
+    clientId: string;
+    lastname?: string;
+}
+
+export const authenticate = async (code: string): Promise<AuthenticateResponse> => {
+    const { tokens } = await oAuth2Client.getToken(code);
+    const loginTicket = await oAuth2Client.verifyIdToken({
+        idToken: tokens.id_token,
+        audience: oAuth2Client._clientId,
+    });
+    const payload = loginTicket.getPayload();
+    return {
+        email: payload.email,
+        firstname: payload.given_name,
+        clientId: oAuth2Client._clientId,
+        lastname: payload.family_name,
+    };
+};
