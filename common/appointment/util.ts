61c3961fc20699e4f8b83ce3fe7574861543ded1
realmayus <realmayus@gmail.com>
2025-04-16 14:03:59 +0200

    add url

--- common/appointment/util.ts
+++ common/appointment/util.ts
@@ -92,6 +92,7 @@ export async function getIcsFile(appointments: Appointment[], isOrganizer: boole
             duration: { hours: durationHours, minutes: durationMinutes },
             title: displayName,
             description: appointment.description ?? undefined,
+            url: `https://app.lern-fair.de/appointment/${appointment.id}`,
             location: `https://app.lern-fair.de/appointment/${appointment.id}`,
             alarms: [
                 {
