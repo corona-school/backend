6ee6cd65f44d1de589f6b3d507db5b6fd56a6d4b
John Angel <john.angel@lern-fair.de>
2025-05-21 13:34:19 +0200

    fix: Prohibit edit courses to be homework help

--- graphql/course/mutations.ts
+++ graphql/course/mutations.ts
@@ -121,6 +121,12 @@ export class MutateCourseResolver {
     ): Promise<GraphQLModel.Course> {
         const course = await getCourse(courseId);
         await hasAccess(context, 'Course', course);
+        const mayCreateHomeworkHelp = context.user.roles.includes(Role.ADMIN) || context.user.roles.includes(Role.COURSE_SCREENER);
+        const isTryingToCreateHomeworkHelp = course.category !== data.category && data.category === course_category_enum.homework_help;
+        if (!mayCreateHomeworkHelp && isTryingToCreateHomeworkHelp) {
+            throw new ForbiddenError('Only authorized users can create homework help courses');
+        }
+
         if (course.courseState === 'allowed') {
             let editableSubcourse = false;
             for (const subcourse of await getSubcoursesForCourse(courseId, true)) {
