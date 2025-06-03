2646575e09dd916269a38f91ffe87f6a52efef3c
John Angel <john.angel@lern-fair.de>
2025-06-03 15:10:10 +0200

    feat: Update foreign key constraints

--- prisma/migrations/20250603130559_add_screening_appointment_fields_in_lecture_table/migration.sql
+++ prisma/migrations/20250603130559_add_screening_appointment_fields_in_lecture_table/migration.sql
@@ -11,7 +11,7 @@ ADD COLUMN     "tutorScreeningId" INTEGER;
 ALTER TABLE "lecture" ADD CONSTRAINT "lecture_pupilScreeningId_fkey" FOREIGN KEY ("pupilScreeningId") REFERENCES "pupil_screening"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "lecture" ADD CONSTRAINT "lecture_tutorScreeningId_fkey" FOREIGN KEY ("tutorScreeningId") REFERENCES "screening"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "lecture" ADD CONSTRAINT "lecture_tutorScreeningId_fkey" FOREIGN KEY ("tutorScreeningId") REFERENCES "screening"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "lecture" ADD CONSTRAINT "lecture_instructorScreeningId_fkey" FOREIGN KEY ("instructorScreeningId") REFERENCES "instructor_screening"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "lecture" ADD CONSTRAINT "lecture_instructorScreeningId_fkey" FOREIGN KEY ("instructorScreeningId") REFERENCES "instructor_screening"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
