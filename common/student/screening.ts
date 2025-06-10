c30f86e05a1e783f59d7da985ff0a3038697a807
John Angel <john.angel@lern-fair.de>
2025-06-10 12:26:29 +0200

    fix: Adjust when to require for ethics onboarding

--- common/student/screening.ts
+++ common/student/screening.ts
@@ -142,11 +142,13 @@ export async function updateStudentScreening(type: StudentScreeningType, screeni
         },
     };
 
+    if (data.status === ScreeningStatus.success) {
+        await requireStudentOnboarding(screening.studentId);
+    }
     // @ts-expect-error For some reason Typescript doesn't treat the update the same way it does with the find. This should work fine
     await screeningModel.update(update);
     logger.info(`Screener(${screenerId}) updated ${screeningModelLabel} of Student(${screening.studentId})`, data);
     if (data.status === ScreeningStatus.success) {
-        await requireStudentOnboarding(screening.studentId);
         const roleUpdate = type === StudentScreeningType.tutor ? { isStudent: true } : { isInstructor: true };
         await prisma.student.update({ data: roleUpdate, where: { id: screening.studentId } });
         const asUser = userForStudent(screening.student);
