8c75dfdb47fb9665c347ca5229b1b93c558f2972
John Angel <john.angel@lern-fair.de>
2025-09-03 11:18:50 +0200

    feat: Invalidate screenings when pupils request a second match / add admin exception

--- common/match/request.ts
+++ common/match/request.ts
@@ -70,30 +70,27 @@ export async function createPupilMatchRequest(pupil: Pupil, adminOverride = fals
 
     await Notification.actionTaken(userForPupil(pupil), 'tutee_match_requested', {});
 
-    const isFirstScreening =
-        (await prisma.pupil_screening.count({
-            where: {
-                pupilId: pupil.id,
-            },
-        })) === 1;
+    const hasActiveMatch = (await prisma.match.count({ where: { pupilId: pupil.id, dissolved: false } })) > 0;
 
-    // Invalidation for match request doesn't apply when there is only one screening.
-    if (!isFirstScreening) {
-        // If the last successful screening, wasn't in the last four months, then invalidate all the screenings.
-        const screening = await prisma.pupil_screening.findFirst({
-            where: {
-                pupilId: pupil.id,
-                status: 'success',
-                invalidated: false,
-                createdAt: {
-                    gte: moment().subtract(4, 'months').toDate(),
-                },
+    const screeningInTheLastFourMonths = await prisma.pupil_screening.findFirst({
+        where: {
+            pupilId: pupil.id,
+            status: 'success',
+            invalidated: false,
+            createdAt: {
+                gte: moment().subtract(4, 'months').toDate(),
             },
-            orderBy: {
-                createdAt: 'desc',
-            },
-        });
-        if (!screening) {
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    });
+
+    // Invalidation doesn't apply when admins/screeners are creating the match request
+    if (!adminOverride) {
+        // If the last successful screening, wasn't in the last four months.
+        // OR if the user is requesting a second match, then invalidate all the screenings.
+        if (!screeningInTheLastFourMonths || hasActiveMatch) {
             await invalidateAllScreeningsOfPupil(pupil.id);
         }
     }
