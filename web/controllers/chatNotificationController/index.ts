156d27367ed06449edb437370c8e00feaea5fe4d
John Angel <john.angel@lern-fair.de>
2025-11-26 12:38:45 +0100

    feat: Update webhook to use message.sent event

--- web/controllers/chatNotificationController/index.ts
+++ web/controllers/chatNotificationController/index.ts
@@ -1,6 +1,6 @@
 import { getLogger } from '../../../common/logger/logger';
 import { Request, Response, Router } from 'express';
-import { NotificationTriggered, WithRawBody } from './types';
+import { NotificationTriggered, MessageSentEvent, WithRawBody } from './types';
 import { talkJsIdToUserId } from '../../../common/chat/helper';
 import { getUser } from '../../../common/user';
 import * as Notification from '../../../common/notification';
@@ -12,12 +12,11 @@ const logger = getLogger('ChatNotification');
 
 export const chatNotificationRouter = Router();
 
-chatNotificationRouter.post('/chat-notification', handleChatNotification);
+chatNotificationRouter.post('/chat-event', handleChatEvent);
 
-const secretKey = process.env.TALKJS_API_KEY;
-async function handleChatNotification(req: WithRawBody<Request>, res: Response): Promise<void> {
+async function handleChatEvent(req: WithRawBody<Request>, res: Response) {
     try {
-        logger.info('Request at /chat/chat-notification');
+        logger.info('Request at /chat/chat-event');
 
         const receivedSignature = req.headers['x-talkjs-signature'];
         const timestamp = req.headers['x-talkjs-timestamp'];
@@ -29,7 +28,25 @@ async function handleChatNotification(req: WithRawBody<Request>, res: Response):
         if (receivedSignature !== validSignature) {
             throw new InvalidSignatureError('Invalid Signature');
         }
+        const notificationBody: NotificationTriggered | MessageSentEvent = req.body;
+        if (notificationBody.type === 'notification.triggered') {
+            await handleChatNotification(req, res);
+        } else if (notificationBody.type === 'message.sent') {
+            await handleMessageSentEvent(req, res);
+        }
+    } catch (error) {
+        if (error instanceof InvalidSignatureError) {
+            logger.info('Invalid Signature');
+            res.status(401).send({ error: 'Unauthorized' });
+            return;
+        }
+    }
+}
 
+const secretKey = process.env.TALKJS_API_KEY;
+async function handleChatNotification(req: WithRawBody<Request>, res: Response): Promise<void> {
+    try {
+        logger.info('Handling chat notification for missed messages');
         const notificationBody: NotificationTriggered = req.body;
         const { data } = notificationBody;
         const { recipient, sender, conversation } = data;
@@ -49,41 +66,52 @@ async function handleChatNotification(req: WithRawBody<Request>, res: Response):
 
             const notificationAction = chatType === ChatType.ONE_ON_ONE ? 'missed_one_on_one_chat_message' : 'missed_course_chat_message';
             await Notification.actionTaken(recipientUser, notificationAction, notificationContext);
-            const match = conversation.custom.match ? JSON.parse(conversation.custom.match) : undefined;
-            const message = data.messages[0];
-            const userType = senderUser.pupilId ? 'pupil' : 'student';
-            if (chatType === ChatType.ONE_ON_ONE && !!match?.matchId && (senderUser?.pupilId || senderUser?.studentId)) {
-                const matchRecord = await prisma.match.findFirst({
-                    where: { id: match.matchId },
-                });
-                const messageDate = new Date(message.createdAt).toISOString();
-                if (userType === 'student') {
-                    await prisma.match.update({
-                        where: { id: match.matchId },
-                        data: {
-                            studentLastMessageSentAt: messageDate,
-                            studentFirstMessageSentAt: matchRecord?.studentFirstMessageSentAt ? matchRecord.studentFirstMessageSentAt : messageDate,
-                        },
-                    });
-                } else if (userType === 'pupil') {
-                    await prisma.match.update({
-                        where: { id: match.matchId },
-                        data: {
-                            pupilLastMessageSentAt: messageDate,
-                            pupilFirstMessageSentAt: matchRecord?.pupilFirstMessageSentAt ? matchRecord.pupilFirstMessageSentAt : messageDate,
-                        },
-                    });
-                }
-            }
         }
         res.status(200).send({ status: 'ok' });
     } catch (error) {
-        if (error instanceof InvalidSignatureError) {
-            logger.info('Invalid Signature');
-            res.status(401).send({ error: 'Unauthorized' });
-            return;
-        }
         logger.error(`Failed to send notification for missed messages`, error);
         res.status(500).send({ error: 'Internal Server Error' });
+    }
+}
+
+async function handleMessageSentEvent(req: WithRawBody<Request>, res: Response): Promise<void> {
+    try {
+        logger.info('Handling message.sent event');
+        res.status(200).send({ status: 'ok' });
+        const notificationBody: MessageSentEvent = req.body;
+        const { data } = notificationBody;
+        const { sender, conversation, message } = data;
+        const senderUserId = talkJsIdToUserId(sender.id);
+        const match = conversation.custom.match ? JSON.parse(conversation.custom.match) : undefined;
+        const userType = senderUserId.startsWith('pupil') ? 'pupil' : 'student';
+        const conversationParticipants = Object.keys(data.conversation.participants);
+        const chatType = getChatType(conversationParticipants);
+        if (chatType === ChatType.ONE_ON_ONE && !!match?.matchId) {
+            const matchRecord = await prisma.match.findFirst({
+                where: { id: match.matchId },
+            });
+            const messageDate = new Date(message.createdAt).toISOString();
+            if (userType === 'student') {
+                await prisma.match.update({
+                    where: { id: match.matchId },
+                    data: {
+                        studentLastMessageSentAt: messageDate,
+                        studentFirstMessageSentAt: matchRecord?.studentFirstMessageSentAt ? matchRecord.studentFirstMessageSentAt : messageDate,
+                        studentMessageCount: { increment: 1 },
+                    },
+                });
+            } else if (userType === 'pupil') {
+                await prisma.match.update({
+                    where: { id: match.matchId },
+                    data: {
+                        pupilLastMessageSentAt: messageDate,
+                        pupilFirstMessageSentAt: matchRecord?.pupilFirstMessageSentAt ? matchRecord.pupilFirstMessageSentAt : messageDate,
+                        pupilMessageCount: { increment: 1 },
+                    },
+                });
+            }
+        }
+    } catch (error) {
+        logger.error(`Failed to process message.sent event (${error.message})`, error);
     }
 }
