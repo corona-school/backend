edf273c871ba3348f7debba3f77961dccd996561
John Angel <john.angel@lern-fair.de>
2026-06-01 14:24:36 +0200

    feat: Deactivate autoInviteForScreening

--- common/match/pool.ts
+++ common/match/pool.ts
@@ -212,7 +212,7 @@ export const TEST_POOL = {
 const _pools = [
     {
         name: 'lern-fair-now',
-        autoInviteForScreening: true,
+        autoInviteForScreening: false,
         toggles: [...INTEREST_CONFIRMATION_TOGGLES, ...PUPIL_SCREENING_TOGGLES],
 
         pupilsToMatch: (toggles: (InterestConfirmationToggle | PupilScreeningToggle)[]): Prisma.pupilWhereInput => {
