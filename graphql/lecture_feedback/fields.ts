ca4128e1e71d19f162a688d8494c167e9921e201
John Angel <john.angel@lern-fair.de>
2026-09-09 10:53:02 +0200

    feat: Add lectureFeedbacks query

--- graphql/lecture_feedback/fields.ts
+++ graphql/lecture_feedback/fields.ts
@@ -1,11 +1,111 @@
-import { Resolver, Query, Authorized, Ctx, FieldResolver, Root } from 'type-graphql';
+import { Resolver, Query, Authorized, Ctx, FieldResolver, Root, Arg, InputType, Field, ObjectType } from 'type-graphql';
 import { prisma } from '../../common/prisma';
-import { Lecture_feedback as LectureFeedback } from '../generated';
+import { Lecture_feedback as LectureFeedback, Lecture } from '../generated';
 import { Role } from '../../common/user/roles';
 import { GraphQLContext } from '../context';
+import { Prisma } from '@prisma/client';
+
+@InputType()
+export class AppointmentStartRange {
+    @Field((type) => Date, { nullable: true })
+    from?: Date;
+
+    @Field((type) => Date, { nullable: true })
+    to?: Date;
+}
 
 @Resolver((of) => LectureFeedback)
 export class LectureFeedbackFieldsResolver {
+    @FieldResolver((returns) => Lecture, { nullable: true })
+    @Authorized(Role.OWNER, Role.APPOINTMENT_PARTICIPANT, Role.ADMIN)
+    async lecture(@Root() appointment: LectureFeedback) {
+        if (!appointment.lectureId) {
+            return null;
+        }
+
+        return await prisma.lecture.findUnique({
+            where: { id: appointment.lectureId },
+        });
+    }
+
+    @Query((returns) => [LectureFeedback])
+    @Authorized(Role.ADMIN)
+    async lectureFeedbacks(
+        @Ctx() context: GraphQLContext,
+        @Arg('userEmail', { nullable: true }) userEmail?: string,
+        @Arg('appointmentStart', (type) => AppointmentStartRange, { nullable: true })
+        appointmentStart?: AppointmentStartRange,
+        @Arg('ratingContains', (type) => [Number], { nullable: true })
+        ratingContains?: number[],
+        @Arg('onlyWithComments', { nullable: true }) onlyWithComments?: boolean
+    ) {
+        const conditions: Prisma.Sql[] = [];
+
+        // Filter by student/pupil email
+        if (userEmail) {
+            conditions.push(Prisma.sql`
+            (
+                (
+                    lf."userId" LIKE 'student/%'
+                    AND s."email" = ${userEmail}
+                )
+                OR
+                (
+                    lf."userId" LIKE 'pupil/%'
+                    AND p."email" = ${userEmail}
+                )
+            )
+        `);
+        }
+
+        // Filter by lecture start date
+        if (appointmentStart?.from) {
+            conditions.push(Prisma.sql`l."start" >= ${appointmentStart.from}`);
+        }
+
+        if (appointmentStart?.to) {
+            conditions.push(Prisma.sql`l."start" <= ${appointmentStart.to}`);
+        }
+
+        // Filter by ratings
+        if (ratingContains?.length) {
+            conditions.push(Prisma.sql`lf."rating" IN (${Prisma.join(ratingContains)})`);
+        }
+
+        // "Comments" are basically tags startng with "Sonstiges:"
+        if (onlyWithComments) {
+            conditions.push(Prisma.sql`
+            EXISTS (
+                SELECT 1
+                FROM unnest(lf."tags") AS tag
+                WHERE tag LIKE 'Sonstiges:%'
+            )
+        `);
+        }
+
+        const where = conditions.length > 0 ? Prisma.sql`WHERE ${Prisma.join(conditions, ' AND ')}` : Prisma.empty;
+
+        const rows = await prisma.$queryRaw<{ id: number }[]>(Prisma.sql`
+            SELECT lf.*
+            FROM "lecture_feedback" lf
+
+            LEFT JOIN "lecture" l
+                ON l."id" = lf."lectureId"
+
+            LEFT JOIN "student" s
+                ON lf."userId" = 'student/' || s."id"::text
+
+            LEFT JOIN "pupil" p
+                ON lf."userId" = 'pupil/' || p."id"::text
+
+            ${where}
+
+            ORDER BY lf."createdAt" DESC
+        `);
+
+        return rows;
+    }
+
     @Query((returns) => [LectureFeedback])
     @Authorized(Role.STUDENT, Role.PUPIL)
     async pendingLectureFeedbacks(@Ctx() context: GraphQLContext) {
